957189f1716884922a27a275c16b7c13cc72d9c9
Casimir666 <casimir666@users.sourceforge.net>
2007-05-26 11:13:23 +0000

    Removed file/folder
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@114 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/ShuttlePN31/TestPipe/ShuttlePN31.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CShuttlePN31
-{
-public:
-	CShuttlePN31(void);
-	~CShuttlePN31(void);
-};
--- src/apps/ShuttlePN31/filter/stddcls.c
+++
@@ -1,4 +0,0 @@
-// stddcls.cpp -- Precompiled header stub for WDM driver
-// Produced by Walt Oney's driver wizard
-
-#include "stddcls.h"
