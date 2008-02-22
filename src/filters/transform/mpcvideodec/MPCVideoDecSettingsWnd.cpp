b3d49ed6fb480beade5e03906e228e7f8ed3c940
alexwild <alexwild@users.sourceforge.net>
2008-02-22 20:28:40 +0000

    language fixes; german updated
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@418 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/mpcvideodec/MPCVideoDecSettingsWnd.cpp
+++ src/filters/transform/mpcvideodec/MPCVideoDecSettingsWnd.cpp
@@ -139,7 +139,7 @@ bool CMPCVideoDecSettingsWnd::OnActivate()
 	nPosY += VERTICAL_SPACING;
 //	m_txtDXVAMode.Create (ResStr (IDS_VDF_DXVA_MODE), WS_VISIBLE|WS_CHILD|BS_AUTOCHECKBOX|WS_DISABLED, CRect (LEFT_SPACING,  nPosY, 190, nPosY+15), this, IDC_PP_ENABLE_DEBLOCKING);
 	m_txtDXVAMode.Create (ResStr (IDS_VDF_DXVA_MODE), WS_VISIBLE|WS_CHILD, CRect (LEFT_SPACING,  nPosY, 190, nPosY+15), this, IDC_STATIC);
-	m_edtDXVAMode.Create (WS_CHILD|WS_VISIBLE|WS_DISABLED|WS_BORDER, CRect (140,  nPosY, 315, nPosY+20), this, 0);
+	m_edtDXVAMode.Create (WS_CHILD|WS_VISIBLE|WS_DISABLED, CRect (140,  nPosY, 315, nPosY+20), this, 0);
 	m_edtDXVAMode.SetWindowText (GetDXVAMode (m_pMDF->GetDXVADecoderGuid()));
 
 
