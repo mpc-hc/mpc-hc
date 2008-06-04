10549244b638902f8ebe87d34db830efbc896674
drevil_xxl <drevil_xxl@users.sourceforge.net>
2008-06-04 20:16:48 +0000

    libavcodec x64 msvc compile fix?
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@550 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- include/stdint.h
+++ include/stdint.h
@@ -77,15 +77,11 @@ typedef uint64_t  uint_least64_t;
 
 // 7.18.1.3 Fastest minimum-width integer types
 typedef int8_t    int_fast8_t;
-#ifndef _WIN64
 typedef int16_t   int_fast16_t;
-#endif
 typedef int32_t   int_fast32_t;
 typedef int64_t   int_fast64_t;
 typedef uint8_t   uint_fast8_t;
-#ifndef _WIN64
 typedef uint16_t  uint_fast16_t;
-#endif
 typedef uint32_t  uint_fast32_t;
 typedef uint64_t  uint_fast64_t;
 
