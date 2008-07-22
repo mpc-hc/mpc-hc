1ec609c06024974f6bd953a88ec5e9ecd0b3f24f
Casimir666 <casimir666@users.sourceforge.net>
2008-07-22 21:26:27 +0000

    NEW    : Pixel shader to convert BT601 -> BT701
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@680 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/mplayerc.cpp
+++ src/apps/mplayerc/mplayerc.cpp
@@ -2028,6 +2028,7 @@ void CMPlayerCApp::Settings::UpdateData(bool fSave)
 		CAtlStringMap<UINT> shaders;
 
 		shaders[_T("16-235 -> 0-255")] = IDF_SHADER_LEVELS;
+		shaders[_T("BT601 -> BT701")] = IDF_SHADER_BT601_BT701;
 		shaders[_T("contour")] = IDF_SHADER_CONTOUR;
 		shaders[_T("deinterlace (blend)")] = IDF_SHADER_DEINTERLACE;
 		shaders[_T("edge sharpen")] = IDF_SHADER_EDGE_SHARPEN;
