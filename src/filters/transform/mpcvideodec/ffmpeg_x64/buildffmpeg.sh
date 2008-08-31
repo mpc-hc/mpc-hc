7fc2f01414ae6bbc11971dc05898b75fba024b2a
Casimir666 <casimir666@users.sourceforge.net>
2008-08-31 13:09:41 +0000

    Change : MPCVideodec build using MingW64 for x64 edition (using buildffmpeg.sh script)
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@769 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/mpcvideodec/ffmpeg_x64/buildffmpeg.sh
+++ src/filters/transform/mpcvideodec/ffmpeg_x64/buildffmpeg.sh
@@ -14,19 +14,23 @@ while opt=$1 && shift; do
   case "$opt" in
     "--help" )
       cat << EOF
-    This build script will setup an entire buildroot environment consisting Ming64 and ffmpeg
-    to build Mpc-hc x64 edition. Source code will be downloaded from SourceForge projects :
-	- Crt and Headers from MingW x64 (http://sourceforge.net/project/showfiles.php?group_id=202880)
-	- Ffmpeg from Mpc-hc (http://sourceforge.net/projects/mpc-hc/)
+    This build script will setup an entire buildroot environment consisting 
+    Ming64 and ffmpeg to build Mpc-hc x64 edition. Source code will be 
+    downloaded from SourceForge projects :
+	- Crt and Headers from MingW x64 
+	(http://sourceforge.net/project/showfiles.php?group_id=202880)
+	- Ffmpeg from Mpc-hc
+	(http://sourceforge.net/projects/mpc-hc/)
 
   To use this script, you should have :
 	- Download MingW64 chaintool for i686-mingw platform 
 	 (http://sourceforge.net/project/showfiles.php?group_id=202880&package_id=245516&release_id=546049)
 	- Download MSys, and configure it with MingW64 with postinstall script 
 	(http://sourceforge.net/project/showfiles.php?group_id=2435)
+	- Patch MSys with latest release of CoreUtils and Bash
+	-  Install Subversion command line client from http://subversion.tigris.org, and add path in msys.bat
 	- Start MSys and run this script
-  link above).
-	
+  	
       $0 [ --help ] [ --updatemingw ] 
       
     --help	Causes all other arguments to be ignored and results in the display of
@@ -46,19 +50,21 @@ EOF
 done
 
 for i in $DIRS; do
-[ -d $i ]  || mkdir $i && updatemingw="true"
+[ -d $i ]  || mkdir $i || updatemingw="true"
 done
 
 if [[ $updatemingw == "true" ]]; then
 	echo "Downloading Ming64 crt and headers.." && cd $BD/mingw
 	svn -q co https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk .
+fi
 
-	echo "Compiling crt.." && cd $BD/mingw/build-$HST
+if [[ $updatemingw == "true" ]]; then
+	echo "Compiling Ming64 crt and headers.." && cd $BD/mingw/build-$HST
 	../mingw-w64-crt/configure --prefix=$PF --with-sysroot=$PF --host=$TGT || exit 1
+	# export CFLAGS="-fno-leading-underscore -mno-cygwin"
 	make CFLAGS="-fno-leading-underscore -mno-cygwin" -s && make install || exit 1
-	
-	cp /mingw/lib/gcc/x86_64-pc-mingw32/ ../../../../../lib64
-	cp $PF/x86_64-pc-mingw32/lib/libmingwex.a ../../../../../lib64
+	cp /mingw/lib/gcc/x86_64-pc-mingw32/4.4.0/libgcc.a $BD/../../../../../../lib64/libgcc.a
+	cp $PF/x86_64-pc-mingw32/lib/libmingwex.a $BD/../../../../../../lib64/libmingwex.a
 fi
 
 
