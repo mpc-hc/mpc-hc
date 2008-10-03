3c2a834c7dea751053bc3cf6f70264397c152ef5
Aleksoid <aleksoid@users.sourceforge.net>
2008-10-03 23:43:59 +0000

    ADD : mpciconlib.dll - icons library(compile mpciconlib.vcproj)
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@817 10f7b99b-c216-0410-bff0-8a66a9350fd8

---
+++ src/apps/mplayerc/mpciconlib.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by mpciconlib.rc
+//
+#define IDI_WMV_ICON                       	101
+#define IDI_WMA_ICON                       	102
+#define IDI_AVI_ICON                       	103
+#define IDI_WAV_ICON                       	104
+#define IDI_MPG_ICON                       	105
+#define IDI_MPA_ICON                       	106
+#define IDI_DVDF_ICON                       107
+#define IDI_DVDA_ICON                       108
+#define IDI_MP3_ICON                       	109
+#define IDI_MID_ICON                       	110
+#define IDI_IVF_ICON                       	111
+#define IDI_AIFF_ICON                       112
+#define IDI_AU_ICON                       	113
+#define IDI_OGM_ICON                       	114
+#define IDI_OGG_ICON                       	115
+#define IDI_CDA_ICON                       	116
+#define IDI_FLIC_ICON                       117
+#define IDI_D2V_ICON                       	118
+#define IDI_MP4_ICON                       	119
+#define IDI_AAC_ICON                       	120
+#define IDI_MKV_ICON                       	121
+#define IDI_MKA_ICON                       	122
+#define IDI_BIK_ICON                       	123
+#define IDI_RATDVD_ICON                     124
+#define IDI_ROQ_ICON                       	125
+#define IDI_RM_ICON                       	126
+#define IDI_RA_ICON                       	127
+#define IDI_RT_ICON                       	128
+#define IDI_DRC_ICON                       	129
+#define IDI_DSM_ICON                       	130
+#define IDI_MPC_ICON                       	131
+#define IDI_FLAC_ICON                       132
+#define IDI_ALAC_ICON                       133
+#define IDI_FLV_ICON                       	134
+#define IDI_SWF_ICON                       	135
+#define IDI_MOV_ICON                       	136
+#define IDI_JPEG_ICON                       137
+#define IDI_PLS_ICON                       	138
+#define IDI_OTHER_ICON                      139
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
