8d7680fce206fbeb0b00f47d33aa68d07cbcfa9e
Casimir666 <casimir666@users.sourceforge.net>
2009-03-31 19:01:02 +0000

    CHANGED : support for MLP/True HD in MKV
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1029 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/parser/matroskasplitter/MatroskaSplitter.cpp
+++ src/filters/parser/matroskasplitter/MatroskaSplitter.cpp
@@ -420,6 +420,8 @@ avcsuccess:
 					id2ft["A_FLAC"] = -WAVE_FORMAT_FLAC;
 					id2ft["A_WAVPACK4"] = -WAVE_FORMAT_WAVPACK4;
 					id2ft["A_TTA1"] = WAVE_FORMAT_TTA1;
+					id2ft["A_TRUEHD"] = WAVE_FORMAT_DOLBY_AC3;
+					id2ft["A_MLP"] = WAVE_FORMAT_DOLBY_AC3;
 				}
 
 				int wFormatTag;
