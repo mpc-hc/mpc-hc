ae07c5bc39bd9c4c35718201d15d4f1b62e41aa5
Aleksoid <aleksoid@users.sourceforge.net>
2009-07-26 22:10:20 +0000

    Change : build.bat
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1192 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/build.bat
+++ src/apps/mplayerc/build.bat
@@ -48,7 +48,7 @@ devenv mpcresources.sln %BUILDTYPE% "Release Unicode Korean|Win32"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Unicode Polish|Win32"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
-devenv mpcresources.sln %BUILDTYPE% "Release Unicode Portuguese|Win32"
+devenv mpcresources.sln %BUILDTYPE% "Release Portuguese|Win32"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Unicode Russian|Win32"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
@@ -65,13 +65,15 @@ IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Swedish|Win32"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 
+IF "%1"=="clean" goto x64
 mkdir Build_x86
 xcopy "Release Unicode\*.dll" ".\Build_x86\" /y 
 xcopy "Release Unicode\*.exe" ".\Build_x86\" /y
 xcopy AUTHORS ".\Build_x86\" /y
 xcopy ChangeLog ".\Build_x86\" /y
-xcopy Homepage.url ".\Build_x86\" /y
+rem xcopy Homepage.url ".\Build_x86\" /y
 xcopy ..\..\..\COPYING ".\Build_x86\" /y
+:x64
 
 rem goto Nox64
 devenv ..\..\..\mpc-hc.sln %BUILDTYPE% "Release Unicode|x64"
@@ -96,7 +98,7 @@ devenv mpcresources.sln %BUILDTYPE% "Release Unicode Korean|x64"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Unicode Polish|x64"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
-devenv mpcresources.sln %BUILDTYPE% "Release Unicode Portuguese|x64"
+devenv mpcresources.sln %BUILDTYPE% "Release Portuguese|x64"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Unicode Russian|x64"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
@@ -113,12 +115,13 @@ IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 devenv mpcresources.sln %BUILDTYPE% "Release Swedish|x64"
 IF %ERRORLEVEL% NEQ 0 GOTO EndBad
 
+IF "%1"=="clean" goto Nox64
 mkdir Build_x64
 xcopy "x64\Release Unicode\*.dll" ".\Build_x64\" /y 
 xcopy "x64\Release Unicode\*.exe" ".\Build_x64\" /y
 xcopy AUTHORS ".\Build_x64\" /y
 xcopy ChangeLog ".\Build_x64\" /y
-xcopy Homepage.url ".\Build_x64\" /y
+rem xcopy Homepage.url ".\Build_x64\" /y
 xcopy ..\..\..\COPYING ".\Build_x64\" /y
 
 :Nox64
