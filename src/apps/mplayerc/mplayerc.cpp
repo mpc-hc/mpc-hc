ff12e46e1934ae46148c28760a4024edd474f9ad
clsid2 <clsid2@users.sourceforge.net>
2009-10-14 20:18:43 +0000

    Prevent loading incompatible versions of d3dx9_xx.dll
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1301 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/mplayerc.cpp
+++ src/apps/mplayerc/mplayerc.cpp
@@ -2998,19 +2998,38 @@ HINSTANCE CMPlayerCApp::GetD3X9Dll()
 {
 	if (m_hD3DX9Dll == NULL)
 	{
+		int min_ver = D3DX_SDK_VERSION;
+		int max_ver = D3DX_SDK_VERSION;
+		
 		m_nDXSdkRelease = 0;
-		// Try to load latest DX9 available
-		for (int i=D3DX_SDK_VERSION; i>23; i--)
+
+		if(D3DX_SDK_VERSION >= 42) {
+			// August 2009 SDK (v42) is not compatible with older versions
+			min_ver = 42;			
+		} else {
+			if(D3DX_SDK_VERSION > 33) {
+				// versions between 34 and 41 have no known compatibility issues
+				min_ver = 34;
+			}	else {		
+				// The minimum version that supports the functionality required by MPC is 24
+				min_ver = 24;
+				
+				if(D3DX_SDK_VERSION == 33) {
+					// The April 2007 SDK (v33) should not be used (crash sometimes during shader compilation)
+					max_ver = 32;		
+				}				
+			}
+		}
+		
+		// load latest compatible version of the DLL that is available
+		for (int i=max_ver; i>=min_ver; i--)
 		{
-			if (i != 33)	// Prevent using DXSDK April 2007 (crash sometimes during shader compilation)
+			m_strD3DX9Version.Format(_T("d3dx9_%d.dll"), i);
+			m_hD3DX9Dll = LoadLibrary (m_strD3DX9Version);
+			if (m_hD3DX9Dll) 
 			{
-				m_strD3DX9Version.Format(_T("d3dx9_%d.dll"), i);
-				m_hD3DX9Dll = LoadLibrary (m_strD3DX9Version);
-				if (m_hD3DX9Dll) 
-				{
-					m_nDXSdkRelease = i;
-					break;
-				}
+				m_nDXSdkRelease = i;
+				break;
 			}
 		}
 	}
