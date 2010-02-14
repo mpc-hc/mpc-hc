7e956bb14bc7ee5499727fea101f91a3085d19ea
XhmikosR <xhmikosr@users.sourceforge.net>
2010-02-14 12:03:03 +0000

    -DSUtil.h: Removed linker pragma inclusion of fixed-name DSUtil.lib files to allow for more flexible project file management -Rasterizer.cpp: Don't use MMX intrinsics in 64 bit builds -SeparableFilter.h: Fix line order for OpenMP building
    Patch by nielsm
    
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1658 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/DSUtil/DSUtil.h
+++ src/DSUtil/DSUtil.h
@@ -21,20 +21,6 @@
 
 #pragma once
 
-#ifdef UNICODE
-#ifdef DEBUG
-#pragma comment(lib, "dsutilDU")
-#else
-#pragma comment(lib, "dsutilRU")
-#endif
-#else
-#ifdef DEBUG
-#pragma comment(lib, "dsutilD")
-#else
-#pragma comment(lib, "dsutilR")
-#endif
-#endif
-
 #include "NullRenderers.h"
 #include "HdmvClipInfo.h"
 #include "H264Nalu.h"
