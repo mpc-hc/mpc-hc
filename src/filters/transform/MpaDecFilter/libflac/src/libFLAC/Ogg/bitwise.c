72a0f6f3db09997117b9637c5e5fa646ab88cbe5
XhmikosR <xhmikosr@users.sourceforge.net>
2010-04-29 16:45:01 +0000

    updated Ogg for libFLAC
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1822 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MpaDecFilter/libflac/src/libFLAC/Ogg/bitwise.c
+++ src/filters/transform/MpaDecFilter/libflac/src/libFLAC/Ogg/bitwise.c
@@ -1,17 +1,17 @@
 /********************************************************************
  *                                                                  *
- * THIS FILE IS PART OF THE OggVorbis SOFTWARE CODEC SOURCE CODE.   *
+ * THIS FILE IS PART OF THE Ogg CONTAINER SOURCE CODE.              *
  * USE, DISTRIBUTION AND REPRODUCTION OF THIS LIBRARY SOURCE IS     *
  * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
  * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
  *                                                                  *
- * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2009             *
+ * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2010             *
  * by the Xiph.Org Foundation http://www.xiph.org/                  *
  *                                                                  *
  ********************************************************************
 
   function: packing variable sized words into an octet stream
-  last mod: $Id: bitwise.c 16051 2009-05-27 05:00:06Z xiphmont $
+  last mod: $Id: bitwise.c 16993 2010-03-21 23:15:46Z xiphmont $
 
  ********************************************************************/
 
