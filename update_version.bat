8ee09c5a2c60571a9b94edb07a6f480b2a820c7e
XhmikosR <xhmikosr@users.sourceforge.net>
2010-05-13 12:21:21 +0000

    Updated "update_version.bat". TortoiseSVN adds itself in PATH so there's no need to use a hardcoded path for SubWCRev.
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@1882 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- update_version.bat
+++ update_version.bat
@@ -1,24 +1,11 @@
-@if not exist "%programfiles%\TortoiseSVN\bin\SubWCRev.exe" goto :x64
-
-@"%programfiles%\TortoiseSVN\bin\SubWCRev.exe" .\ include\SubWCRev.conf include\version.h
-@"%programfiles%\TortoiseSVN\bin\SubWCRev.exe" .\ src\apps\mplayerc\res\mpc-hc.exe.manifest.conf src\apps\mplayerc\res\mpc-hc.exe.manifest
-@if %ERRORLEVEL%==6 goto :NoSubWCRev
-@if %ERRORLEVEL%==10 goto :NoSubWCRev
-@goto :eof
-
-:x64
-@if not exist "%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" goto :NoSubWCRev
-
-@"%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" .\ include\SubWCRev.conf include\version.h
-@"%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" .\ src\apps\mplayerc\res\mpc-hc.exe.manifest.conf src\apps\mplayerc\res\mpc-hc.exe.manifest
-@if %ERRORLEVEL%==6 goto :NoSubWCRev
-@if %ERRORLEVEL%==10 goto :NoSubWCRev
+@SubWCRev .\ include\SubWCRev.conf include\version.h
+@SubWCRev .\ src\apps\mplayerc\res\mpc-hc.exe.manifest.conf src\apps\mplayerc\res\mpc-hc.exe.manifest
+@if %ERRORLEVEL% NEQ 0 goto :NoSubWCRev
 @goto :eof
 
 :NoSubWCRev
 @echo NoSubWCRev
-@if exist include\version.h del include\version.h
-echo #pragma once >> include\version.h
+echo #pragma once > include\version.h
 echo. >> include\Version.h
 echo #define DO_MAKE_STR(x) #x >> include\Version.h
 echo #define MAKE_STR(x) DO_MAKE_STR(x) >> include\Version.h
@@ -28,4 +15,4 @@ echo. >> include\Version.h
 @echo #define VERSION_REV 0 >> include\version.h
 @echo #define VERSION_PATCH 0 >> include\version.h
 
-@copy src\apps\mplayerc\res\mpc-hc.exe.manifest.template src\apps\mplayerc\res\mpc-hc.exe.manifest
+@copy src\apps\mplayerc\res\mpc-hc.exe.manifest.template src\apps\mplayerc\res\mpc-hc.exe.manifest /Y
