101d0a4c237246cb1c0f961d7a9d32465651624f
XhmikosR <xhmikosr@users.sourceforge.net>
2010-07-27 01:07:52 +0000

    put manifest file in the project files
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2159 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/stdafx.h
+++ src/apps/mplayerc/stdafx.h
@@ -65,8 +65,6 @@
 
 #include <gdiplus.h>
 
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 template <class T = CString, class S = CString>
 class CAtlStringMap : public CAtlMap<S, T, CStringElementTraits<S> > {};
 
