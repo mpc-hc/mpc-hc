0fc70a844a5929f87aff978bfbeaf93ceb49a2a5
XhmikosR <xhmikosr@users.sourceforge.net>
2010-08-22 12:55:46 +0000

    fix previous commit
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2297 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MPCVideoDec/ffmpeg/gccbuild.bat
+++ src/filters/transform/MPCVideoDec/ffmpeg/gccbuild.bat
@@ -1,19 +1,19 @@
 @ECHO OFF
-IF NOT "x%MINGW32%" == "x" GOTO :VarOk
-ECHO: "ERROR : Please define MINGW32 (and/or MSYS) environment variable(s)"
+IF NOT "x%MINGW32%" == "x" GOTO VarOk
+ECHO "ERROR : Please define MINGW32 (and/or MSYS) environment variable(s)"
 EXIT 1005
 
 :VarOk
 SET CC=gcc.exe
 SET PATH=%MSYS%\bin;%MINGW32%\bin;%YASM%;%PATH%
 
-IF "%1%"=="rebuild" GOTO :DoClean
-IF "%1%"=="clean" GOTO :OnlyClean
-GOTO :NoArchClean
+IF "%1%"=="rebuild" GOTO DoClean
+IF "%1%"=="clean" GOTO OnlyClean
+GOTO NoArchClean
 
 :OnlyClean
 make.exe clean
-GOTO :End
+GOTO End
 
 :DoClean
 make.exe clean
