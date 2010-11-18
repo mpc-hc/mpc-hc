840acceb358ecb8270cd06ee9836827e9cd26fc1
sansnom05 <sansnom05@users.sourceforge.net>
2010-11-18 21:33:21 +0000

    UI fixes based on patch from khagaroth, translations needed This commit fixes ticket #926
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2733 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/mpcresources/patch.bat
+++ src/apps/mplayerc/mpcresources/patch.bat
@@ -1,8 +1,9 @@
 @echo off
 
 rem A simple script which demonstrates how to apply translated text to locale rc file.
-rem This is only an example, to use it you will have to change the filenames.
+rem This is only an example.
 
-echo Generating new rc files and string files...
-perl patch.pl -i text\mplayerc.fr.rc.txt mplayerc.fr.rc
+for %%i in (*.rc) do echo Patching file %%i & perl patch.pl -i text\%%i.txt %%i & echo ----------------------
+echo ----------------------
+
 pause
