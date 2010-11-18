78849547972b5311388545327e97e0fb359f15ac
XhmikosR <xhmikosr@users.sourceforge.net>
2010-11-18 19:04:27 +0000

    update docs
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2731 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- update_version.bat
+++ update_version.bat
@@ -5,6 +5,6 @@ IF %ERRORLEVEL% NEQ 0 GOTO :NoSubWCRev
 GOTO :EOF
 
 :NoSubWCRev
-ECHO:NoSubWCRev
-ECHO:#define VERSION_REV 0 > include\Version_rev.h
+ECHO:NoSubWCRev, will use VERSION_REV=0
+ECHO:#define VERSION_REV 0 >include\Version_rev.h
 COPY /Y src\apps\mplayerc\res\mpc-hc.exe.manifest.template src\apps\mplayerc\res\mpc-hc.exe.manifest
