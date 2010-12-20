9f2a2c2c803fc4ddfdcef3f242d038f5e5f3e670
sansnom05 <sansnom05@users.sourceforge.net>
2010-12-20 04:07:21 +0000

    -updated korean translation by xneokr -revert accidental commit for GotoDlg
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2788 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/GoToDlg.cpp
+++ src/apps/mplayerc/GoToDlg.cpp
@@ -87,7 +87,7 @@ BOOL CGoToDlg::OnInitDialog()
 
 	}
 
-	return TRUE;
+	return FALSE;
 
 	//	return TRUE;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
