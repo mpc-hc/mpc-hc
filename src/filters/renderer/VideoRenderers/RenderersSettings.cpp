9aec83a58de569bb4646acbe9213137c29abcaf7
clsid2 <clsid2@users.sourceforge.net>
2010-12-21 00:32:34 +0000

    Shader model 1.x is not supported anymore, so remove them.
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@2794 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/renderer/VideoRenderers/RenderersSettings.cpp
+++ src/filters/renderer/VideoRenderers/RenderersSettings.cpp
@@ -130,32 +130,15 @@ LONGLONG CRenderersData::GetPerfCounter()
 
 HINSTANCE CRenderersData::GetD3X9Dll()
 {
-	if (m_hD3DX9Dll == NULL) {
-		int min_ver = D3DX_SDK_VERSION;
-		int max_ver = D3DX_SDK_VERSION;
+#if D3DX_SDK_VERSION < 42
+#error DirectX SDK August 2009 (v42) or newer is required to build MPC
+#endif
 
+	if (m_hD3DX9Dll == NULL) {
 		m_nDXSdkRelease = 0;
 
-		if(D3DX_SDK_VERSION >= 42) {
-			// August 2009 SDK (v42) is not compatible with older versions
-			min_ver = 42;
-		} else {
-			if(D3DX_SDK_VERSION > 33) {
-				// versions between 34 and 41 have no known compatibility issues
-				min_ver = 34;
-			} else {
-				// The minimum version that supports the functionality required by MPC is 24
-				min_ver = 24;
-
-				if(D3DX_SDK_VERSION == 33) {
-					// The April 2007 SDK (v33) should not be used (crash sometimes during shader compilation)
-					max_ver = 32;
-				}
-			}
-		}
-
 		// load latest compatible version of the DLL that is available
-		for (int i=max_ver; i>=min_ver; i--) {
+		for (int i=D3DX_SDK_VERSION; i>=42; i--) {
 			m_strD3DX9Version.Format(_T("d3dx9_%d.dll"), i);
 			m_hD3DX9Dll = LoadLibrary (m_strD3DX9Version);
 			if (m_hD3DX9Dll) {
