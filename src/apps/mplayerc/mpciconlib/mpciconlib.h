9a509f581c3ed7aaaaa17d8c26b913b528e51d9a
v0lt <v0lt@users.sourceforge.net>
2011-05-07 08:02:01 +0000

    Updated mpciconlib.dll. Redraw all the icons, change the order. Need to reassociate extensions with icons. The easiest way to do this: 1. Go to "Options ...-> Formats", uncheck "With icons" and click "Apply" 2. Checkbox "With icons" back and click "Apply" again 3. Close the "Options".
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3089 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/mpciconlib/mpciconlib.h
+++ src/apps/mplayerc/mpciconlib/mpciconlib.h
@@ -4,42 +4,53 @@
  * when adding/removing icons.
  */
 
-#define IDI_AVI_ICON                    0
-#define IDI_MPG_ICON                    1
-#define IDI_MP4_ICON                    2
-#define IDI_MKV_ICON                    3
-#define IDI_OGM_ICON                    4
-#define IDI_FLV_ICON                    5
-#define IDI_WMV_ICON                    6
-#define IDI_MOV_ICON                    7
-#define IDI_RM_ICON                     8
-#define IDI_MP3_ICON                    9
-#define IDI_OGG_ICON                    10
-#define IDI_AAC_ICON                    11
-#define IDI_MKA_ICON                    12
-#define IDI_WMA_ICON                    13
-#define IDI_RA_ICON                     14
-#define IDI_WAV_ICON                    15
-#define IDI_MPA_ICON                    16
-#define IDI_CDA_ICON                    17
-#define IDI_MPC_ICON                    18
-#define IDI_FLAC_ICON                   19
-#define IDI_ALAC_ICON                   20
-#define IDI_PLC_ICON                    21
-#define IDI_D2V_ICON                    22
-#define IDI_MID_ICON                    23
-#define IDI_AIFF_ICON                   24
-#define IDI_AU_ICON                     25
-#define IDI_IVF_ICON                    26
-#define IDI_FLIC_ICON                   27
-#define IDI_BIK_ICON                    28
-#define IDI_RATDVD_ICON                 29
-#define IDI_RT_ICON                     30
-#define IDI_DSM_ICON                    31
-#define IDI_SWF_ICON                    32
-#define IDI_OTHER_ICON                  33
-#define IDI_DVDA_ICON                   34
-#define IDI_DVDF_ICON                   35
+#define IDI_OTHER_ICON                  0 
+#define IDI_AAC_ICON                    1
+#define IDI_AC3_ICON                    2
+#define IDI_AIFF_ICON                   3
+#define IDI_ALAC_ICON                   4
+#define IDI_AMR_ICON                    5
+#define IDI_APE_ICON                    6
+#define IDI_AU_ICON                     7
+#define IDI_AVI_ICON                    8
+#define IDI_BINK_ICON                   9
+#define IDI_CDA_ICON                    10
+#define IDI_D2V_ICON                    11
+#define IDI_DAT_ICON                    12
+#define IDI_DSM_ICON                    13
+#define IDI_DTS_ICON                    14
+#define IDI_FLAC_ICON                   15
+#define IDI_FLIC_ICON                   16
+#define IDI_FLV_ICON                    17
+#define IDI_IFO_ICON                    18
+#define IDI_IVF_ICON                    19
+#define IDI_MIDI_ICON                   20
+#define IDI_MKA_ICON                    21
+#define IDI_MKV_ICON                    22
+#define IDI_MOV_ICON                    23
+#define IDI_MP3_ICON                    24
+#define IDI_MP4_ICON                    25
+#define IDI_MPA_ICON                    26
+#define IDI_MPC_ICON                    27
+#define IDI_MPEG_ICON                   28
+#define IDI_OFR_ICON                    29
+#define IDI_OGG_ICON                    30
+#define IDI_OGM_ICON                    31
+#define IDI_PLAYLIST_ICON               32
+#define IDI_RA_ICON                     33
+#define IDI_RATDVD_ICON                 34
+#define IDI_RM_ICON                     35
+#define IDI_RT_ICON                     36
+#define IDI_SMK_ICON                    37
+#define IDI_SWF_ICON                    38
+#define IDI_TS_ICON                     39
+#define IDI_TTA_ICON                    40
+#define IDI_VOB_ICON                    41
+#define IDI_WAV_ICON                    42
+#define IDI_WEBM_ICON                   43
+#define IDI_WMA_ICON                    44
+#define IDI_WMV_ICON                    45
+#define IDI_WV_ICON                     46
 #define IDI_NONE                        -1
 
 
@@ -47,7 +58,7 @@
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        39
+#define _APS_NEXT_RESOURCE_VALUE        46
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
