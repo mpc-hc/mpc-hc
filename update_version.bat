3bdcdde6b0ff78b19245fab61c7a471c856b500e
XhmikosR <xhmikosr@users.sourceforge.net>
2011-07-22 19:32:00 +0000

    show a more useful message when SubWCRev isn't found
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3454 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- update_version.bat
+++ update_version.bat
@@ -1,17 +1,25 @@
 @ECHO OFF
 SETLOCAL
-SubWCRev .\ include\Version_rev.h.in include\Version_rev.h -f
+
+SET "SUBWCREV=SubWCRev.exe"
+
+"%SUBWCREV%" . "include\Version_rev.h.in" "include\Version_rev.h" -f
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
 
-SubWCRev .\ src\apps\mplayerc\res\mpc-hc.exe.manifest.conf src\apps\mplayerc\res\mpc-hc.exe.manifest -f >NUL
+"%SUBWCREV%" . "src\apps\mplayerc\res\mpc-hc.exe.manifest.conf" "src\apps\mplayerc\res\mpc-hc.exe.manifest" -f >NUL
 IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
 
+ENDLOCAL
 EXIT /B
 
+
 :NoSubWCRev
-ECHO NoSubWCRev, will use MPC_VERSION_REV=0
+ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
+ECHO You should (re)install TortoiseSVN.
+ECHO I'll use MPC_VERSION_REV=0 for now.
 
-ECHO #define MPC_VERSION_REV 0 >include\Version_rev.h
-COPY /Y /V src\apps\mplayerc\res\mpc-hc.exe.manifest.template src\apps\mplayerc\res\mpc-hc.exe.manifest
+ECHO #define MPC_VERSION_REV 0 > "include\Version_rev.h"
+COPY /Y /V "src\apps\mplayerc\res\mpc-hc.exe.manifest.template" "src\apps\mplayerc\res\mpc-hc.exe.manifest" >NUL
 
+ENDLOCAL
 EXIT /B
