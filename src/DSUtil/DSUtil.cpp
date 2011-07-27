d72ad681c2b0619de89897a2d70781c810c86f5d
Aleksoid <aleksoid@users.sourceforge.net>
2011-07-27 21:49:38 +0000

    Fix : Improve detect Aspect Rate from splitter, ticket #1566.
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3491 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/DSUtil/DSUtil.cpp
+++ src/DSUtil/DSUtil.cpp
@@ -1140,47 +1140,45 @@ bool ExtractDim(const AM_MEDIA_TYPE* pmt, int& w, int& h, int& arx, int& ary)
 	}
 
 	if(!arx || !ary) {
+		BYTE* ptr = NULL;
+		DWORD len = 0;
+
+		if(pmt->formattype == FORMAT_MPEGVideo) {
+			ptr = ((MPEG1VIDEOINFO*)pmt->pbFormat)->bSequenceHeader;
+			len = ((MPEG1VIDEOINFO*)pmt->pbFormat)->cbSequenceHeader;
+
+			if(ptr && len >= 8 && *(DWORD*)ptr == 0xb3010000) {
+				w = (ptr[4]<<4)|(ptr[5]>>4);
+				h = ((ptr[5]&0xf)<<8)|ptr[6];
+				float ar[] = {
+					1.0000f,1.0000f,0.6735f,0.7031f,
+					0.7615f,0.8055f,0.8437f,0.8935f,
+					0.9157f,0.9815f,1.0255f,1.0695f,
+					1.0950f,1.1575f,1.2015f,1.0000f,
+				};
+				arx = (int)((float)w / ar[ptr[7]>>4] + 0.5);
+				ary = h;
+			}
+		} else if(pmt->formattype == FORMAT_MPEG2_VIDEO) {
+			ptr = (BYTE*)((MPEG2VIDEOINFO*)pmt->pbFormat)->dwSequenceHeader;
+			len = ((MPEG2VIDEOINFO*)pmt->pbFormat)->cbSequenceHeader;
+
+			if(ptr && len >= 8 && *(DWORD*)ptr == 0xb3010000) {
+				w = (ptr[4]<<4)|(ptr[5]>>4);
+				h = ((ptr[5]&0xf)<<8)|ptr[6];
+				struct {
+					int x, y;
+				} ar[] = {{w,h},{4,3},{16,9},{221,100},{w,h}};
+				int i = min(max(ptr[7]>>4, 1), 5)-1;
+				arx = ar[i].x;
+				ary = ar[i].y;
+			}
+		}
+	}
+
+	if(!arx || !ary) {
 		arx = w;
 		ary = h;
-	}
-
-	BYTE* ptr = NULL;
-	DWORD len = 0;
-
-	if(pmt->formattype == FORMAT_MPEGVideo) {
-		ptr = ((MPEG1VIDEOINFO*)pmt->pbFormat)->bSequenceHeader;
-		len = ((MPEG1VIDEOINFO*)pmt->pbFormat)->cbSequenceHeader;
-
-		if(ptr && len >= 8 && *(DWORD*)ptr == 0xb3010000) {
-			w = (ptr[4]<<4)|(ptr[5]>>4);
-			h = ((ptr[5]&0xf)<<8)|ptr[6];
-			float ar[] = {
-				1.0000f,1.0000f,0.6735f,0.7031f,
-				0.7615f,0.8055f,0.8437f,0.8935f,
-				0.9157f,0.9815f,1.0255f,1.0695f,
-				1.0950f,1.1575f,1.2015f,1.0000f,
-			};
-			arx = (int)((float)w / ar[ptr[7]>>4] + 0.5);
-			ary = h;
-		}
-	} else if(pmt->formattype == FORMAT_MPEG2_VIDEO) {
-		ptr = (BYTE*)((MPEG2VIDEOINFO*)pmt->pbFormat)->dwSequenceHeader;
-		len = ((MPEG2VIDEOINFO*)pmt->pbFormat)->cbSequenceHeader;
-
-		if(ptr && len >= 8 && *(DWORD*)ptr == 0xb3010000) {
-			w = (ptr[4]<<4)|(ptr[5]>>4);
-			h = ((ptr[5]&0xf)<<8)|ptr[6];
-			struct {
-				int x, y;
-			} ar[] = {{w,h},{4,3},{16,9},{221,100},{w,h}};
-			int i = min(max(ptr[7]>>4, 1), 5)-1;
-			arx = ar[i].x;
-			ary = ar[i].y;
-		}
-	}
-
-	if(ptr && len >= 8) {
-
 	}
 
 	DWORD a = arx, b = ary;
