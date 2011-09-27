7ae3a08f07710cc926379d2a86b701a5427e51ab
XhmikosR <xhmikosr@users.sourceforge.net>
2011-09-27 06:31:27 +0000

    * remove unneeded file * gccbuild batch files: merge from the ffmpeg branch and also use the NUMBER_OF_PROCESSORS environment variable (if defined) to get the number of jobs make will use (the default is still 4 if the previous variable is not defined)
    TODO: check if the variable has valid numbers only
    
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3745 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MPCVideoDec/ffmpeg/gccbuild64_2010.bat
+++ src/filters/transform/MPCVideoDec/ffmpeg/gccbuild64_2010.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+SETLOCAL
 
 rem Check for the help switches
 IF /I "%~1"=="help"   GOTO SHOWHELP
@@ -9,51 +10,75 @@ IF /I "%~1"=="/?"     GOTO SHOWHELP
 
 IF DEFINED MINGW64 GOTO VarOk
 ECHO ERROR: Please define MINGW64 (and/or MSYS) environment variable(s)
+ENDLOCAL
 EXIT /B
 
 :VarOk
 SET PATH=%MSYS%\bin;%MINGW64%\bin;%PATH%
 
+IF /I "%~2" == "Debug" SET "DEBUG=DEBUG=yes"
+
 IF "%~1" == "" (
   SET "BUILDTYPE=build"
+  CALL :SubMake
+  EXIT /B
 ) ELSE (
-  IF /I "%~1" == "Build"     SET "BUILDTYPE=build"   & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "/Build"    SET "BUILDTYPE=build"   & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "-Build"    SET "BUILDTYPE=build"   & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "--Build"   SET "BUILDTYPE=build"   & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "Clean"     SET "BUILDTYPE=clean"   & CALL :SubMake clean & EXIT /B
-  IF /I "%~1" == "/Clean"    SET "BUILDTYPE=clean"   & CALL :SubMake clean & EXIT /B
-  IF /I "%~1" == "-Clean"    SET "BUILDTYPE=clean"   & CALL :SubMake clean & EXIT /B
-  IF /I "%~1" == "--Clean"   SET "BUILDTYPE=clean"   & CALL :SubMake clean & EXIT /B
-  IF /I "%~1" == "Rebuild"   SET "BUILDTYPE=rebuild" & CALL :SubMake clean & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "/Rebuild"  SET "BUILDTYPE=rebuild" & CALL :SubMake clean & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "-Rebuild"  SET "BUILDTYPE=rebuild" & CALL :SubMake clean & CALL :SubMake & EXIT /B
-  IF /I "%~1" == "--Rebuild" SET "BUILDTYPE=rebuild" & CALL :SubMake clean & CALL :SubMake & EXIT /B
+  IF /I "%~1" == "Build" (
+    SET "BUILDTYPE=build"
+    CALL :SubMake
+    EXIT /B
+  )
+
+  IF /I "%~1" == "Clean" (
+    SET "BUILDTYPE=clean"
+    CALL :SubMake clean
+    EXIT /B
+  )
+
+  IF /I "%~1" == "Rebuild" (
+    SET "BUILDTYPE=clean"
+    CALL :SubMake clean
+    SET "BUILDTYPE=build"
+    CALL :SubMake
+    EXIT /B
+  )
 
   ECHO.
   ECHO Unsupported commandline switch!
   ECHO Run "%~nx0 help" for details about the commandline switches.
+  ENDLOCAL
   EXIT /B
 )
 
 
 :SubMake
-SET "make_args=-j4"
-IF /I "%BUILDTYPE%"=="clean" SET "make_args="
+IF "%BUILDTYPE%" == "clean" (
+  SET JOBS=1
+) ELSE (
+  IF DEFINED NUMBER_OF_PROCESSORS (
+    SET JOBS=%NUMBER_OF_PROCESSORS%
+  ) ELSE (
+    REM Default number of jobs
+    SET JOBS=4
+  )
+)
 
-TITLE "make.exe 64BIT=yes %make_args% %*"
-ECHO make.exe 64BIT=yes %make_args% %*
-make.exe 64BIT=yes %make_args% %*
+SET "JOBS=-j%JOBS%"
+
+TITLE "make %JOBS% 64BIT=yes %DEBUG% %*"
+ECHO make %JOBS% 64BIT=yes %DEBUG% %*
+make.exe %JOBS% 64BIT=yes %DEBUG% %*
+
+ENDLOCAL
 EXIT /B
 
 
 :SHOWHELP
 TITLE "%~nx0 %1"
 ECHO. & ECHO.
-ECHO Usage:   %~nx0 [Clean^|Build^|Rebuild]
+ECHO Usage:   %~nx0 [Clean^|Build^|Rebuild] [Debug]
 ECHO.
-ECHO Notes:   You can also prefix the commands with "-", "--" or "/".
-ECHO          The arguments are case insesitive.
+ECHO Notes:   The arguments are not case sensitive.
 ECHO. & ECHO.
 ECHO Executing "%~nx0" will use the defaults: "%~nx0 build"
 ECHO.
