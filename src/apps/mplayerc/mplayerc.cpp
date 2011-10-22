mpc-hc/mpc-hc

commit cce5660aae9668a4f1535ab6ac38ed9101535aa3

fixed bug after closing the player and fast start a new file
git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3789 10f7b99b-c216-0410-bff0-8a66a9350fd8

src/apps/mplayerc/mplayerc.cpp
@@ -1156,22 +1156,23 @@ BOOL CMPlayerCApp::InitInstance()
 	if (GetLastError() == ERROR_ALREADY_EXISTS
 			&& (!(m_s.fAllowMultipleInst || (m_s.nCLSwitches&CLSW_NEW) || m_cmdln.IsEmpty())
 				|| (m_s.nCLSwitches&CLSW_ADD))) {
-		int wait_count = 0;
 		HWND hWnd = ::FindWindow(MPC_WND_CLASS_NAME, NULL);
-		while (!hWnd && (wait_count++<200)) {
-			Sleep(100);
-			hWnd = ::FindWindow(MPC_WND_CLASS_NAME, NULL);
-		}
-		if (hWnd && (wait_count<200)) {
-			SetForegroundWindow(hWnd);
+		if (hWnd) {
+			DWORD Result = 0;
+			LRESULT Return = 0;
+			Return = ::SendMessageTimeout(hWnd, WM_NULL, 0, 0, SMTO_ABORTIFHUNG, 400, &Result );
+			Sleep(200);
+			Return = ::SendMessageTimeout(hWnd, WM_NULL, 0, 0, SMTO_ABORTIFHUNG, 400, &Result );
 
-			if (!(m_s.nCLSwitches&CLSW_MINIMIZED) && IsIconic(hWnd)) {
-				ShowWindow(hWnd, SW_RESTORE);
+			if (Return) {
+				SetForegroundWindow(hWnd);
+				if (!(m_s.nCLSwitches&CLSW_MINIMIZED) && IsIconic(hWnd)) {
+					ShowWindow(hWnd, SW_RESTORE);
+				}
+				SendCommandLine(hWnd);
+
+				return FALSE;
 			}
-
-			SendCommandLine(hWnd);
-
-			return FALSE;
 		}
 	}
 

commit 824140fc35daef97afe7196f96f9c1eecaf718a6

Fix x64 compilation broken by r3789.
git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3790 10f7b99b-c216-0410-bff0-8a66a9350fd8

src/apps/mplayerc/mplayerc.cpp
@@ -1158,7 +1158,7 @@ BOOL CMPlayerCApp::InitInstance()
 				|| (m_s.nCLSwitches&CLSW_ADD))) {
 		HWND hWnd = ::FindWindow(MPC_WND_CLASS_NAME, NULL);
 		if (hWnd) {
-			DWORD Result = 0;
+			DWORD_PTR Result = 0;
 			LRESULT Return = 0;
 			Return = ::SendMessageTimeout(hWnd, WM_NULL, 0, 0, SMTO_ABORTIFHUNG, 400, &Result );
 			Sleep(200);