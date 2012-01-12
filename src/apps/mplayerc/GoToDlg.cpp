9ca7e4ae0ea8cb8719a1537f242bd61c6a2067d5
v0lt <v0lt@users.sourceforge.net>
2012-01-12 18:26:39 +0000

    GoTo dialog: more user-friendly and correct time input (hhh:mm:ss[.ms], mmm:ss[.ms] sss[.ms])
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@3965 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/GoToDlg.cpp
+++ src/apps/mplayerc/GoToDlg.cpp
@@ -2,7 +2,7 @@
  * $Id$
  *
  * (C) 2003-2006 Gabest
- * (C) 2006-2011 see AUTHORS
+ * (C) 2006-2012 see AUTHORS
  *
  * This file is part of mplayerc.
  *
@@ -107,50 +107,34 @@ void CGoToDlg::OnBnClickedOk1()
 {
 	UpdateData();
 
-	int hh, mm, ss, ms;
-	hh = mm = ss = ms = 0;
+	unsigned int hh = 0;
+	unsigned int mm = 0;
+	float ss = 0.0;
+	wchar_t c1 = L':'; // delimiter character
+	wchar_t c2 = L':'; // delimiter character
+	wchar_t c3[2]; // unnecessary character
 
-	CAtlRegExp<> re;
-
-	REParseError status = re.Parse(_T("{\\z}"), FALSE);
-	if (REPARSE_ERROR_OK == status) {
-		m_timestr += 'A';	// HACK:  Without this the while loop below would keep going on X64 on release builds...
-		CAtlREMatchContext<> mc;
-		const CAtlREMatchContext<>::RECHAR* s = m_timestr.GetBuffer();
-		const CAtlREMatchContext<>::RECHAR* e = NULL;
-		while (s && re.Match(s, &mc, &e)) {
-			const CAtlREMatchContext<>::RECHAR* szStart = 0;
-			const CAtlREMatchContext<>::RECHAR* szEnd = 0;
-			mc.GetMatch(0, &szStart, &szEnd);
-
-			if (hh != 0 || hh > 59 || mm > 59 || ss > 59) {
-				AfxMessageBox(ResStr(IDS_GOTO_ERROR_PARSING_TIME), MB_ICONEXCLAMATION | MB_OK);
-				return;
-			}
-
-			hh = mm;
-			mm = ss;
-			ss = ms;
-			ms = _tcstol(szStart, (TCHAR**)&szStart, 10);
-
-			s = e;
-		}
-
-		m_time = ((hh*60+mm)*60+ss)*1000+ms;
-
-		AfxGetApp()->WriteProfileInt(IDS_R_SETTINGS, _T("gotoluf"), 0);
-
+	if ((swscanf_s(m_timestr, L"%f%1s", &ss, &c3, 2) == 1 || // sss[.ms]
+		 swscanf_s(m_timestr, L"%u%c%f%1s", &mm, &c2, 1, &ss, &c3, 2) == 3 && ss < 60 || // mmm:ss[.ms]
+		 swscanf_s(m_timestr, L"%u%c%u%c%f%1s", &hh, &c1, 1, &mm, &c2, 1, &ss, &c3, 2) == 5 && mm < 60  && ss < 60) && // hhh:mm:ss[.ms]
+		 c1 == L':' && c2 == L':' && ss >=0) {
+		
+		m_time = (int)(1000*((hh*60+mm)*60+ss)+0.5);
 		OnOK();
+	} else {
+		AfxMessageBox(ResStr(IDS_GOTO_ERROR_PARSING_TIME), MB_ICONEXCLAMATION | MB_OK);
 	}
 }
 
 void CGoToDlg::OnBnClickedOk2()
 {
 	UpdateData();
+
 	unsigned int frame;
 	float fps;
 	wchar_t c1[2]; // delimiter character
 	wchar_t c2[2]; // unnecessary character
+
 	int result = swscanf_s(m_framestr, L"%u%1s%f%1s", &frame, &c1, 2, &fps, &c2, 2);
 	if (result == 1) {
 		m_time = (int)(1000.0*frame/m_fps+0.5);
