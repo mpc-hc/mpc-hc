mpc-hc/mpc-hc

commit a1f4c706a7811c07e250698891651fe2202a8c1d

UI improvements for the properties dialog:  - the filename is not highlighted anymore when opening the dialog  - double-clicking on the file location will now open the containing folder and select the file.
git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4181 10f7b99b-c216-0410-bff0-8a66a9350fd8

src/apps/mplayerc/PPageFileInfoClip.cpp
@@ -55,8 +55,20 @@ CPPageFileInfoClip::~CPPageFileInfoClip()
 BOOL CPPageFileInfoClip::PreTranslateMessage(MSG* pMsg)
 {
 	if (pMsg->message == WM_LBUTTONDBLCLK && pMsg->hwnd == m_location.m_hWnd && !m_location_str.IsEmpty()) {
-		ShellExecute(NULL, _T("open"), m_location_str, NULL, NULL, SW_SHOWDEFAULT);
-		return TRUE;
+		HRESULT res = CoInitialize(NULL);
+
+		if (res == S_OK || res == S_FALSE) {
+			PIDLIST_ABSOLUTE pidl = ILCreateFromPath(m_location_str+_T("\\")+m_fn);
+
+			if (pidl) {
+				SHOpenFolderAndSelectItems(pidl, 0, NULL, 0);
+				ILFree(pidl);
+			}
+
+			CoUninitialize();
+
+			return TRUE;
+		}
 	}
 
 	return __super::PreTranslateMessage(pMsg);

src/apps/mplayerc/PPageFileInfoSheet.cpp
@@ -70,7 +70,7 @@ END_MESSAGE_MAP()
 
 BOOL CPPageFileInfoSheet::OnInitDialog()
 {
-	BOOL fRet = __super::OnInitDialog();
+	__super::OnInitDialog();
 
 	m_fn.TrimRight('/');
 	int i = max(m_fn.ReverseFind('\\'), m_fn.ReverseFind('/'));
@@ -94,7 +94,9 @@ BOOL CPPageFileInfoSheet::OnInitDialog()
 	m_Button_MI.SetFont(GetFont());
 	m_Button_MI.ShowWindow(SW_HIDE);
 
-	return fRet;
+	GetTabControl()->SetFocus();
+
+	return FALSE;  // return TRUE unless you set the focus to a control
 }
 
 void CPPageFileInfoSheet::OnSaveAs()

commit 1620e191e08ed80310ea7b40543e44cd4e8edfdb

Fix a regression in r4181: double-clicking on the location field in the properties dialog was not opening the folder when the file was directly at the root of a drive.
git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4187 10f7b99b-c216-0410-bff0-8a66a9350fd8

src/apps/mplayerc/PPageFileInfoClip.cpp
@@ -58,11 +58,17 @@ BOOL CPPageFileInfoClip::PreTranslateMessage(MSG* pMsg)
 		HRESULT res = CoInitialize(NULL);
 
 		if (res == S_OK || res == S_FALSE) {
-			PIDLIST_ABSOLUTE pidl = ILCreateFromPath(m_location_str+_T("\\")+m_fn);
+			PIDLIST_ABSOLUTE pidl;
+			
+			CString path = m_location_str;
+			if (path[path.GetLength() - 1] != '\\') {
+				path += _T("\\");
+			}
+			path += m_fn;
 
-			if (pidl) {
+			if (SHParseDisplayName(path, NULL, &pidl, 0, NULL) == S_OK) {
 				SHOpenFolderAndSelectItems(pidl, 0, NULL, 0);
-				ILFree(pidl);
+				CoTaskMemFree(pidl);
 			}
 
 			CoUninitialize();