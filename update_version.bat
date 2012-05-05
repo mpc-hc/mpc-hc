31e4959ca2d87dc8593f1c41c4f7cf37e552f4f4
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-05 15:03:55 +0000

    update_version.bat: minor improvements
    patch by kasper93
    
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4597 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- update_version.bat
+++ update_version.bat
@@ -1,28 +1,35 @@
 @ECHO OFF
 SETLOCAL
 
-SET "SUBWCREV=SubWCRev.exe"
-
 PUSHD %~dp0
 
+IF EXIST "SubWCRev.exe" SET "SUBWCREV=SubWCRev.exe"
+FOR %%A IN (SubWCRev.exe) DO (SET SUBWCREV=%%~$PATH:A)
+IF NOT DEFINED SUBWCREV GOTO SubNoSubWCRev
+
 "%SUBWCREV%" . "include\Version_rev.h.in" "include\Version_rev.h" -f
-IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
+IF %ERRORLEVEL% NEQ 0 GOTO SubError
 
 "%SUBWCREV%" . "src\apps\mplayerc\res\mpc-hc.exe.manifest.conf" "src\apps\mplayerc\res\mpc-hc.exe.manifest" -f >NUL
-IF %ERRORLEVEL% NEQ 0 GOTO NoSubWCRev
-
-GOTO END
-
-
-:NoSubWCRev
-ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
-ECHO You should (re)install TortoiseSVN.
-ECHO I'll use MPC_VERSION_REV=0 for now.
-
-ECHO #define MPC_VERSION_REV 0 > "include\Version_rev.h"
-TYPE "src\apps\mplayerc\res\mpc-hc.exe.manifest.template" > "src\apps\mplayerc\res\mpc-hc.exe.manifest"
+IF %ERRORLEVEL% NEQ 0 GOTO SubError
 
 :END
 POPD
 ENDLOCAL
+EXIT /B
+
+
+:SubNoSubWCRev
+ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
+ECHO You should (re)install TortoiseSVN.
+GOTO SubCommon
+
+:SubError
+ECHO Something went wrong when generating the revision number.
+
+:SubCommon
+ECHO I'll use MPC_VERSION_REV=0 for now.
+
+ECHO #define MPC_VERSION_REV 0 > "include\Version_rev.h"
+TYPE "src\apps\mplayerc\res\mpc-hc.exe.manifest.template" > "src\apps\mplayerc\res\mpc-hc.exe.manifest"
 EXIT /B
