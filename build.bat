b9b302971d616b94381a059ef1e1666c4034f99e
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-05 16:17:29 +0000

    build.bat: fix r4599. We need to use %PATH% first otherwise FIND.exe from MSYS is used.
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4601 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- build.bat
+++ build.bat
@@ -41,7 +41,7 @@ IF EXIST "build.user.bat" (
   IF DEFINED MSYS    (SET MPCHC_MSYS=%MSYS%)       ELSE (GOTO MissingVar)
 )
 
-SET "PATH=%MPCHC_MSYS%\bin;%MPCHC_MINGW32%\bin;%PATH%"
+SET "PATH=%PATH%;%MPCHC_MSYS%\bin;%MPCHC_MINGW32%\bin"
 FOR %%X IN (gcc.exe) DO (SET FOUND=%%~$PATH:X)
 IF NOT DEFINED FOUND GOTO MissingVar
 
