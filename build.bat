76f2b8349b2e1aa2ef0542b960487f110b7323ea
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-05 10:48:18 +0000

    build.bat: * add support for creating the 7z packages with 7zip if it's installed * the installer is now built only when the packages switch is used.
    The 7zip detection routine contributed by kasper93
    
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4592 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- build.bat
+++ build.bat
@@ -42,7 +42,8 @@ SET ARG=%ARG:-=%
 SET ARGB=0
 SET ARGBC=0
 SET ARGC=0
-SET ARGP=0
+SET ARGPL=0
+SET ARGPA=0
 SET INPUT=0
 
 IF /I "%ARG%" == "?"         GOTO ShowHelp
@@ -52,10 +53,10 @@ FOR %%A IN (%ARG%) DO (
   IF /I "%%A" == "Build"     SET "BUILDTYPE=Build"   & Set /A ARGB+=1
   IF /I "%%A" == "Clean"     SET "BUILDTYPE=Clean"   & Set /A ARGB+=1
   IF /I "%%A" == "Rebuild"   SET "BUILDTYPE=Rebuild" & Set /A ARGB+=1
-  IF /I "%%A" == "Both"      SET "PLATFORM=Both"     & Set /A ARGP+=1
-  IF /I "%%A" == "Win32"     SET "PLATFORM=Win32"    & Set /A ARGP+=1
-  IF /I "%%A" == "x86"       SET "PLATFORM=Win32"    & Set /A ARGP+=1
-  IF /I "%%A" == "x64"       SET "PLATFORM=x64"      & Set /A ARGP+=1
+  IF /I "%%A" == "Both"      SET "PLATFORM=Both"     & Set /A ARGPL+=1
+  IF /I "%%A" == "Win32"     SET "PLATFORM=Win32"    & Set /A ARGPL+=1
+  IF /I "%%A" == "x86"       SET "PLATFORM=Win32"    & Set /A ARGPL+=1
+  IF /I "%%A" == "x64"       SET "PLATFORM=x64"      & Set /A ARGPL+=1
   IF /I "%%A" == "All"       SET "CONFIG=All"        & Set /A ARGC+=1
   IF /I "%%A" == "Main"      SET "CONFIG=Main"       & Set /A ARGC+=1
   IF /I "%%A" == "Filters"   SET "CONFIG=Filters"    & Set /A ARGC+=1
@@ -63,17 +64,19 @@ FOR %%A IN (%ARG%) DO (
   IF /I "%%A" == "Resources" SET "CONFIG=Resources"  & Set /A ARGC+=1
   IF /I "%%A" == "Debug"     SET "BUILDCFG=Debug"    & Set /A ARGBC+=1
   IF /I "%%A" == "Release"   SET "BUILDCFG=Release"  & Set /A ARGBC+=1
+  IF /I "%%A" == "Packages"  SET "PACKAGES=True"     & Set /A ARGPA+=1
 )
 
 FOR %%X IN (%*) DO SET /A INPUT+=1
-SET /A VALID=%ARGB%+%ARGP%+%ARGC%+%ARGBC%
+SET /A VALID=%ARGB%+%ARGPL%+%ARGC%+%ARGBC%+%ARGPA%
 
 IF %VALID% NEQ %INPUT% GOTO UnsupportedSwitch
 
 IF %ARGB%  GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGB% == 0  (SET "BUILDTYPE=Build")
-IF %ARGP%  GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGP% == 0  (SET "PLATFORM=Both")
+IF %ARGPL% GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGPL% == 0 (SET "PLATFORM=Both")
 IF %ARGC%  GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGC% == 0  (SET "CONFIG=MPCHC")
 IF %ARGBC% GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGBC% == 0 (SET "BUILDCFG=Release")
+IF %ARGPA% GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGPA% == 0 (SET "PACKAGES=False")
 
 
 :Start
@@ -86,14 +89,29 @@ IF /I "%PLATFORM%" == "x64"   GOTO x64
 :Win32
 CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
-IF /I "%CONFIG%" == "Filters"   CALL :SubFilters Win32 && GOTO x64
+IF /I "%CONFIG%" == "Filters" (
+  CALL :SubFilters Win32
+  IF /I "%PACKAGES%" == "True" CALL :SubCreatePackages Filters Win32
+  GOTO x64
+)
+
 IF /I "%CONFIG%" == "Resources" CALL :SubResources Win32 && GOTO x64
+
 CALL :SubMPCHC Win32
+
 IF /I "%CONFIG%" == "Main" GOTO x64
 
 CALL :SubResources Win32
-CALL :SubCreatePackages Win32
-IF /I "%CONFIG%" == "All"  CALL :SubFilters Win32
+
+IF /I "%PACKAGES%" == "True" (
+  CALL :SubCreateInstaller Win32
+  CALL :SubCreatePackages MPC-HC Win32
+)
+
+IF /I "%CONFIG%" == "All" (
+  CALL :SubFilters Win32
+  IF /I "%PACKAGES%" == "True" CALL :SubCreatePackages Filters Win32
+)
 
 
 :x64
@@ -102,14 +120,29 @@ IF /I "%PLATFORM%" == "Win32" GOTO End
 IF DEFINED PROGRAMFILES(x86) (SET x64_type=amd64) ELSE (SET x64_type=x86_amd64)
 CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" %x64_type%
 
-IF /I "%CONFIG%" == "Filters"   CALL :SubFilters x64 && GOTO END
+IF /I "%CONFIG%" == "Filters" (
+  CALL :SubFilters x64
+  IF /I "%PACKAGES%" == "True" CALL :SubCreatePackages Filters x64
+  GOTO END
+)
+
 IF /I "%CONFIG%" == "Resources" CALL :SubResources x64 && GOTO END
+
 CALL :SubMPCHC x64
+
 IF /I "%CONFIG%" == "Main" GOTO End
 
 CALL :SubResources x64
-CALL :SubCreatePackages x64
-IF /I "%CONFIG%" == "All"  CALL :SubFilters x64
+
+IF /I "%PACKAGES%" == "True" (
+  CALL :SubCreateInstaller x64
+  CALL :SubCreatePackages MPC-HC x64
+)
+
+IF /I "%CONFIG%" == "All" (
+  CALL :SubFilters x64
+  IF /I "%PACKAGES%" == "True" CALL :SubCreatePackages Filters x64
+)
 
 
 :End
@@ -174,7 +207,7 @@ FOR %%A IN ("Armenian" "Basque" "Belarusian" "Catalan" "Chinese Simplified"
 EXIT /B
 
 
-:SubCreatePackages
+:SubCreateInstaller
 IF /I "%BUILDTYPE%" == "Clean" EXIT /B
 IF /I "%BUILDCFG%" == "Debug"  EXIT /B
 IF /I "%CONFIG%" == "Filters"  EXIT /B
@@ -183,39 +216,152 @@ IF /I "%~1" == "x64" SET ISDefs=/Dx64Build
 
 CALL :SubDetectInnoSetup
 
-IF DEFINED InnoSetupPath (
-  TITLE Compiling %1 installer...
-  "%InnoSetupPath%\iscc.exe" /Q /O"bin" "distrib\mpc-hc_setup.iss" %ISDefs%
-  IF %ERRORLEVEL% NEQ 0 CALL :SubMsg "ERROR" "Compilation failed!"
-  CALL :SubMsg "INFO" "%1 installer successfully built"
-) ELSE (
+IF NOT DEFINED InnoSetupPath (
   CALL :SubMsg "WARNING" "Inno Setup wasn't found, the %1 installer wasn't built"
+  EXIT /B
 )
+
+TITLE Compiling %1 installer...
+"%InnoSetupPath%\ISCC.exe" /Q /O"bin" "distrib\mpc-hc_setup.iss" %ISDefs%
+IF %ERRORLEVEL% NEQ 0 CALL :SubMsg "ERROR" "Compilation failed!"
+CALL :SubMsg "INFO" "%1 installer successfully built"
+
+EXIT /B
+
+
+:SubCreatePackages
+CALL :SubDetectSevenzipPath
+CALL :SubGetVersion
+
+IF NOT DEFINED SEVENZIP (
+  CALL :SubMsg "WARNING" "7-Zip wasn't found, the %1 %2 package wasn't built"
+  EXIT /B
+)
+
+IF /I "%~1" == "Filters" (SET "NAME=MPC-HC_standalone_filters") ELSE (SET "NAME=MPC-HC")
+IF /I "%~2" == "Win32"   (SET ARCH=x86) ELSE (SET ARCH=x64)
+
+PUSHD "bin"
+
+IF EXIST "%NAME%.%MPCHC_VER%.%ARCH%.7z"     DEL "%NAME%.%MPCHC_VER%.%ARCH%.7z"
+IF EXIST "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z" DEL "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z"
+IF EXIST "%NAME%.%MPCHC_VER%.%ARCH%"        RD /Q /S "%NAME%.%MPCHC_VER%.%ARCH%"
+
+REM Compress the pdb file for mpc-hc only
+IF /I "%NAME%" == "MPC-HC" (
+  PUSHD "%~1_%ARCH%"
+  TITLE Creating archive %NAME%.%MPCHC_VER%.%ARCH%.pdb.7z...
+  START "7z" /B /WAIT "%SEVENZIP%" a -t7z "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z" "*.pdb" -m0=LZMA -mx9 -ms=on
+  IF %ERRORLEVEL% NEQ 0 CALL :SubMsg "ERROR" "Unable to create %NAME%.%MPCHC_VER%.%ARCH%.pdb.7z!"
+  CALL :SubMsg "INFO" "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z successfully created"
+  IF EXIST "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z" MOVE /Y "%NAME%.%MPCHC_VER%.%ARCH%.pdb.7z" ".." >NUL
+  POPD
+)
+
+REM Create a text file containing the files to be excluded from the archives
+ECHO .bmp >    ExcludeList.txt
+ECHO .dmp >>   ExcludeList.txt
+ECHO .exp >>   ExcludeList.txt
+ECHO .ilk >>   ExcludeList.txt
+ECHO .ini >>   ExcludeList.txt
+ECHO .lib >>   ExcludeList.txt
+ECHO .mpcpl >> ExcludeList.txt
+ECHO .pdb >>   ExcludeList.txt
+ECHO .png >>   ExcludeList.txt
+
+TITLE Copying %NAME%.%MPCHC_VER%.%ARCH%...
+XCOPY "%~1_%ARCH%" "%NAME%.%MPCHC_VER%.%ARCH%" /EXCLUDE:ExcludeList.txt /I /S /H /K /Q /R /Y /V >NUL
+
+COPY /Y /V "..\COPYING.txt"        "%NAME%.%MPCHC_VER%.%ARCH%" >NUL
+COPY /Y /V "..\docs\Authors.txt"   "%NAME%.%MPCHC_VER%.%ARCH%" >NUL
+COPY /Y /V "..\docs\Changelog.txt" "%NAME%.%MPCHC_VER%.%ARCH%" >NUL
+COPY /Y /V "..\docs\Readme.txt"    "%NAME%.%MPCHC_VER%.%ARCH%" >NUL
+
+TITLE Creating archive %NAME%.%MPCHC_VER%.%ARCH%.7z...
+START "7z" /B /WAIT "%SEVENZIP%" a -t7z "%NAME%.%MPCHC_VER%.%ARCH%.7z" "%NAME%.%MPCHC_VER%.%ARCH%"^
+ -m0=LZMA -mx9 -ms=on -x@ExcludeList.txt
+IF %ERRORLEVEL% NEQ 0 CALL :SubMsg "ERROR" "Unable to create %NAME%.%MPCHC_VER%.%ARCH%.7z!"
+CALL :SubMsg "INFO" "%NAME%.%MPCHC_VER%.%ARCH%.7z successfully created"
+
+IF EXIST "ExcludeList.txt"           DEL "ExcludeList.txt"
+IF EXIST "%NAME%.%MPCHC_VER%.%ARCH%" RD /Q /S "%NAME%.%MPCHC_VER%.%ARCH%"
+
+POPD
+EXIT /B
+
+
+:SubGetVersion
+REM Get the version
+FOR /F "tokens=3,4 delims= " %%K IN (
+  'FINDSTR /I /L /C:"define MPC_VERSION_MAJOR" "include\Version.h"') DO (SET "VerMajor=%%K")
+FOR /F "tokens=3,4 delims= " %%K IN (
+  'FINDSTR /I /L /C:"define MPC_VERSION_MINOR" "include\Version.h"') DO (SET "VerMinor=%%K")
+FOR /F "tokens=3,4 delims= " %%K IN (
+  'FINDSTR /I /L /C:"define MPC_VERSION_PATCH" "include\Version.h"') DO (SET "VerPatch=%%K")
+FOR /F "tokens=3,4 delims= " %%K IN (
+  'FINDSTR /I /L /C:"define MPC_VERSION_REV" "include\Version_rev.h"') DO (SET "VerRev=%%K")
+
+SET MPCHC_VER=%VerMajor%.%VerMinor%.%VerPatch%.%VerRev%
+EXIT /B
+
+
+:SubDetectInnoSetup
+REM Detect if we are running on 64bit WIN and use Wow6432Node, and set the path
+REM of Inno Setup accordingly
+IF DEFINED PROGRAMFILES(x86) (
+  SET "U_=HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+) ELSE (
+  SET "U_=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+)
+
+FOR /F "delims=" %%A IN (
+  'REG QUERY "%U_%\Inno Setup 5_is1" /v "Inno Setup: App Path" 2^>Nul ^|FIND "REG_SZ"') DO (
+  SET "InnoSetupPath=%%A" & CALL :SubInnoSetupPath %%InnoSetupPath:*Z=%%)
+EXIT /B
+
+
+:SubDetectSevenzipPath
+IF EXIST "%PROGRAMFILES%\7za.exe" SET "SEVENZIP=%PROGRAMFILES%\7za.exe" & EXIT /B
+IF EXIST "7za.exe"                SET "SEVENZIP=7za.exe" & EXIT /B
+
+FOR %%A IN (7z.exe)  DO (SET SEVENZIP=%%~$PATH:A & EXIT /B)
+FOR %%A IN (7za.exe) DO (SET SEVENZIP=%%~$PATH:A & EXIT /B)
+
+FOR /F "tokens=3" %%A IN (
+  'REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\7-Zip" /v "Path" 2^>Nul ^| FIND "REG_SZ"') DO (
+  SET "SEVENZIP_REG=%%A"
+)
+
+FOR /F "delims=" %%A IN (
+  'REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\7-Zip" /v "Path" 2^>Nul ^| FIND "REG_SZ"') DO (
+  SET "SEVENZIP_REG=%%A"
+)
+
+IF EXIST "%SEVENZIP_REG:*REG_SZ    =%\7z.exe" SET SEVENZIP="%SEVENZIP_REG:*REG_SZ    =%\7z.exe"
 EXIT /B
 
 
 :ShowHelp
-TITLE %~nx0 %1
-ECHO. & ECHO.
+TITLE %~nx0 Help
+ECHO.
 ECHO Usage:
-ECHO %~nx0 [Clean^|Build^|Rebuild] [x86^|x64^|Both] [Main^|Resources^|MPCHC^|Filters^|All] [Debug^|Release]
+ECHO %~nx0 [Clean^|Build^|Rebuild] [x86^|x64^|Both] [Main^|Resources^|MPCHC^|Filters^|All] [Debug^|Release] [Packages]
 ECHO.
 ECHO Notes: You can also prefix the commands with "-", "--" or "/".
-ECHO        The arguments are not case sensitive.
+ECHO        Debug only applies to mpc-hc.sln.
+ECHO        The arguments are not case sensitive and can be ommitted.
 ECHO. & ECHO.
 ECHO Executing %~nx0 without any arguments will use the default ones:
 ECHO "%~nx0 Build Both MPCHC Release"
-ECHO.
+ECHO. & ECHO.
 ECHO Examples:
-ECHO %~nx0 x86 Resources  -Builds the x86 resources
-ECHO %~nx0 Resources      -Builds both x86 and x64 resources
-ECHO %~nx0 x86            -Builds x86 Main exe and the x86 resources
-ECHO %~nx0 x86 Debug      -Builds x86 Main Debug exe and x86 resources
-ECHO %~nx0 x86 Filters    -Builds x86 Filters
-ECHO %~nx0 x86 All        -Builds x86 Main exe, x86 Filters and the x86 resources
-ECHO.
-ECHO NOTES:
-ECHO Debug only applies to mpc-hc.sln
+ECHO %~nx0 x86 Resources -Builds the x86 resources
+ECHO %~nx0 Resources     -Builds both x86 and x64 resources
+ECHO %~nx0 x86           -Builds x86 Main exe and the x86 resources
+ECHO %~nx0 x86 Debug     -Builds x86 Main Debug exe and x86 resources
+ECHO %~nx0 x86 Filters   -Builds x86 Filters
+ECHO %~nx0 x86 All       -Builds x86 Main exe, x86 Filters and the x86 resources
+ECHO %~nx0 x86 Packages  -Builds x86 Main exe, x86 resources and creates the .7z package
 ECHO.
 ENDLOCAL
 EXIT /B
@@ -242,21 +388,6 @@ ECHO.
 ECHO Unsupported commandline switch!
 ECHO Run "%~nx0 help" for details about the commandline switches.
 CALL :SubMsg "ERROR" "Compilation failed!"
-
-
-:SubDetectInnoSetup
-REM Detect if we are running on 64bit WIN and use Wow6432Node, and set the path
-REM of Inno Setup accordingly
-IF DEFINED PROGRAMFILES(x86) (
-  SET "U_=HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-) ELSE (
-  SET "U_=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-)
-
-FOR /F "delims=" %%A IN (
-  'REG QUERY "%U_%\Inno Setup 5_is1" /v "Inno Setup: App Path"2^>Nul^|FIND "REG_"') DO (
-  SET "InnoSetupPath=%%A" & CALL :SubInnoSetupPath %%InnoSetupPath:*Z=%%)
-EXIT /B
 
 
 :SubInnoSetupPath
