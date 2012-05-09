1000fcee2c3609880f4734f7034d9fa1fa107722
clsid2 <clsid2@users.sourceforge.net>
2012-05-09 15:27:16 +0000

    Created three build profiles for the internal filters. It is now possible to build without internal filters using a single define.
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4658 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/InternalFiltersConfig.h
+++ src/apps/mplayerc/InternalFiltersConfig.h
@@ -26,73 +26,19 @@
 
 #pragma once
 
-// Internal source filters
-#define INTERNAL_SOURCEFILTER_AVI       1
-#define INTERNAL_SOURCEFILTER_CDDA      1
-#define INTERNAL_SOURCEFILTER_CDXA      1
-#define INTERNAL_SOURCEFILTER_DSM       1
-#define INTERNAL_SOURCEFILTER_DTSAC3    1
-#define INTERNAL_SOURCEFILTER_VTS       1
-#define INTERNAL_SOURCEFILTER_DVSOURCE  1
-#define INTERNAL_SOURCEFILTER_FLIC      1
-#define INTERNAL_SOURCEFILTER_FLAC      1
-#define INTERNAL_SOURCEFILTER_FLV       1
-#define INTERNAL_SOURCEFILTER_MATROSKA  1
-#define INTERNAL_SOURCEFILTER_MP4       1
-#define INTERNAL_SOURCEFILTER_OGG       1
-#define INTERNAL_SOURCEFILTER_MPEGAUDIO 1
-#define INTERNAL_SOURCEFILTER_MPEG      1
-#define INTERNAL_SOURCEFILTER_REALMEDIA 1
-#define INTERNAL_SOURCEFILTER_SHOUTCAST 1
-#define INTERNAL_SOURCEFILTER_UDP       1
-#define INTERNAL_SOURCEFILTER_AVI2AC3   1
+/* Uncomment to build without internal filters. */
+//#define INTERNAL_FILTERS_NONE
 
-// Internal audio decoders
-#define INTERNAL_DECODER_DTS            1
-#define INTERNAL_DECODER_LPCM           1
-#define INTERNAL_DECODER_PS2AUDIO       1
-#define INTERNAL_DECODER_REALAUDIO      1
-#define INTERNAL_DECODER_FLAC           1
-#define INTERNAL_DECODER_PCM            1
+/* Uncomment to build with a custom subset of filters. Included by default are filters that are considered useful even when primarily using external filters. */
+//#define INTERNAL_FILTERS_CUSTOM
 
-// Internal audio decoders (FFmpeg)
-#define INTERNAL_DECODER_AC3            1 /* also E-AC3,TrueHD,MLP */
-#define INTERNAL_DECODER_AAC            1
-#define INTERNAL_DECODER_ALAC           1
-#define INTERNAL_DECODER_ALS            1
-#define INTERNAL_DECODER_MPEGAUDIO      1
-#define INTERNAL_DECODER_VORBIS         1
-#define INTERNAL_DECODER_NELLYMOSER     1
-#define INTERNAL_DECODER_AMR            1
-#define INTERNAL_DECODER_ADPCM          1
-
-// Internal video decoders
-#define INTERNAL_DECODER_MPEG1          1
-#define INTERNAL_DECODER_MPEG2          1
-#define INTERNAL_DECODER_REALVIDEO      1
-
-// Internal video decoders (FFmpeg)
-#define INTERNAL_DECODER_H264           1
-#define INTERNAL_DECODER_VC1            1
-#define INTERNAL_DECODER_FLV            1
-#define INTERNAL_DECODER_VP356          1
-#define INTERNAL_DECODER_DIVX           1
-#define INTERNAL_DECODER_XVID           1
-#define INTERNAL_DECODER_WMV            1
-#define INTERNAL_DECODER_MSMPEG4        1
-#define INTERNAL_DECODER_SVQ            1
-#define INTERNAL_DECODER_H263           1
-#define INTERNAL_DECODER_THEORA         1
-#define INTERNAL_DECODER_AMVV           1
-#define INTERNAL_DECODER_VP8            1
-#define INTERNAL_DECODER_MJPEG          1
-#define INTERNAL_DECODER_INDEO          1
-
-// DXVA decoders
-#define INTERNAL_DECODER_H264_DXVA      1
-#define INTERNAL_DECODER_VC1_DXVA       1
-#define INTERNAL_DECODER_MPEG2_DXVA     1
-#define INTERNAL_DECODER_WMV3_DXVA      1
+#if defined(INTERNAL_FILTERS_NONE)
+	#include "InternalFiltersConfig_none.h"
+#elif defined(INTERNAL_FILTERS_CUSTOM)
+	#include "InternalFiltersConfig_custom.h"
+#else
+	#include "InternalFiltersConfig_standard.h"
+#endif
 
 // Groups
 #define HAS_SOURCEFILTERS \
