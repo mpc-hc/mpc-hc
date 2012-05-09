648340f0859ebe73365e2e6c1d9b3b1a859bb0d5
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-09 16:16:11 +0000

    merge gccbuild64.bat to gccbuild.bat.  Also make the order of the switches not matter.
    partial patch by kasper93
    
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4662 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MPCVideoDec/ffmpeg/gccbuild.bat
+++ src/filters/transform/MPCVideoDec/ffmpeg/gccbuild.bat
@@ -1,17 +1,31 @@
 @ECHO OFF
-SETLOCAL
+REM $Id$
+REM
+REM (C) 2009-2012 see Authors.txt
+REM
+REM This file is part of MPC-HC.
+REM
+REM MPC-HC is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation; either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM MPC-HC is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-rem Check for the help switches
-IF /I "%~1"=="help"   GOTO SHOWHELP
-IF /I "%~1"=="/help"  GOTO SHOWHELP
-IF /I "%~1"=="-help"  GOTO SHOWHELP
-IF /I "%~1"=="--help" GOTO SHOWHELP
-IF /I "%~1"=="/?"     GOTO SHOWHELP
+
+SETLOCAL
 
 IF EXIST "..\..\..\..\..\build.user.bat" (
   CALL "..\..\..\..\..\build.user.bat"
 ) ELSE (
   IF DEFINED MINGW32 (SET MPCHC_MINGW32=%MINGW32%) ELSE (GOTO MissingVar)
+  IF DEFINED MINGW64 (SET MPCHC_MINGW64=%MINGW64%) ELSE (GOTO MissingVar)
   IF DEFINED MSYS    (SET MPCHC_MSYS=%MSYS%)       ELSE (GOTO MissingVar)
 )
 
@@ -19,62 +33,77 @@ SET PATH=%MPCHC_MSYS%\bin;%MPCHC_MINGW32%\bin;%PATH%
 FOR %%X IN (gcc.exe) DO (SET FOUND=%%~$PATH:X)
 IF NOT DEFINED FOUND GOTO MissingVar
 
+PUSHD %~dp0
 
-:VarOk
-IF /I "%~2" == "Debug" SET "DEBUG=DEBUG=yes"
+SET ARG=%*
+SET ARG=%ARG:/=%
+SET ARG=%ARG:-=%
+SET ARGB=0
+SET ARGBC=0
+SET ARGPL=0
+SET INPUT=0
 
-IF "%~1" == "" (
-  SET "BUILDTYPE=build"
-  CALL :SubMake
-  EXIT /B
-) ELSE (
-  IF /I "%~1" == "Build" (
-    SET "BUILDTYPE=build"
-    CALL :SubMake
-    EXIT /B
-  )
+IF /I "%ARG%" == "?"          GOTO ShowHelp
 
-  IF /I "%~1" == "Clean" (
-    SET "BUILDTYPE=clean"
-    CALL :SubMake clean
-    EXIT /B
-  )
+FOR %%A IN (%ARG%) DO (
+  IF /I "%%A" == "help"       GOTO ShowHelp
+  IF /I "%%A" == "Build"      SET "BUILDTYPE=Build"   & SET /A ARGB+=1
+  IF /I "%%A" == "Clean"      SET "BUILDTYPE=Clean"   & SET /A ARGB+=1
+  IF /I "%%A" == "Rebuild"    SET "BUILDTYPE=Rebuild" & SET /A ARGB+=1
+  IF /I "%%A" == "Both"       SET "ARCH=Both"         & SET /A ARGPL+=1
+  IF /I "%%A" == "Win32"      SET "ARCH=x86"          & SET /A ARGPL+=1
+  IF /I "%%A" == "x86"        SET "ARCH=x86"          & SET /A ARGPL+=1
+  IF /I "%%A" == "x64"        SET "ARCH=x64"          & SET /A ARGPL+=1
+  IF /I "%%A" == "Debug"      SET "DEBUG=DEBUG=yes"   & SET /A ARGBC+=1
+  IF /I "%%A" == "Release"    SET "DEBUG="            & SET /A ARGBC+=1
+)
 
-  IF /I "%~1" == "Rebuild" (
-    SET "BUILDTYPE=clean"
-    CALL :SubMake clean
-    SET "BUILDTYPE=build"
-    CALL :SubMake
-    EXIT /B
-  )
+FOR %%X IN (%*) DO SET /A INPUT+=1
+SET /A VALID=%ARGB%+%ARGPL%+%ARGBC%
 
-  ECHO.
-  ECHO Unsupported commandline switch!
-  ECHO Run "%~nx0 help" for details about the commandline switches.
-  ENDLOCAL
+IF %VALID% NEQ %INPUT% GOTO UnsupportedSwitch
+
+IF %ARGB%  GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGB% == 0  (SET "BUILDTYPE=Build")
+IF %ARGPL% GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGPL% == 0 (SET "ARCH=Both")
+IF %ARGBC% GTR 1 (GOTO UnsupportedSwitch) ELSE IF %ARGBC% == 0 (SET "DEBUG=")
+
+
+IF /I "%ARCH%" == "Both" (
+  SET "ARCH=x86" & CALL :Main
+  SET "ARCH=x64" & CALL :Main
+  GOTO End
+)
+
+
+:Main
+IF /I "%ARCH%" == "x64" (SET "x64=64BIT=yes") ELSE (SET "x64=")
+
+IF /I "%BUILDTYPE%" == "Rebuild" (
+  SET "BUILDTYPE=Clean" & CALL :SubMake %x64% %DEBUG% clean
+  SET "BUILDTYPE=Build" & CALL :SubMake %x64% %DEBUG%
   EXIT /B
 )
+
+IF /I "%BUILDTYPE%" == "Clean" (CALL :SubMake %x64% %DEBUG% clean & EXIT /B)
+
+CALL :SubMake %x64% %DEBUG%
+EXIT /B
+
+
+:End
+TITLE Compiling FFmpeg [FINISHED]
+POPD
+ENDLOCAL
+EXIT /B
 
 
 :SubMake
-IF "%BUILDTYPE%" == "clean" (
-  SET JOBS=1
-) ELSE (
-  IF DEFINED NUMBER_OF_PROCESSORS (
-    SET JOBS=%NUMBER_OF_PROCESSORS%
-  ) ELSE (
-    REM Default number of jobs
-    SET JOBS=4
-  )
-)
+IF DEFINED NUMBER_OF_PROCESSORS (SET JOBS=%NUMBER_OF_PROCESSORS%) ELSE (SET JOBS=4)
+IF /I "%BUILDTYPE%" == "Clean"  (SET "JOBS=1")
 
-SET "JOBS=-j%JOBS%"
-
-TITLE "make %JOBS% %DEBUG% %*"
-ECHO make %JOBS% %DEBUG% %*
-make.exe %JOBS% %DEBUG% %*
-
-ENDLOCAL
+TITLE "make -j%JOBS% %*"
+ECHO make -j%JOBS% %*
+make.exe -j%JOBS% %*
 EXIT /B
 
 
@@ -86,14 +115,26 @@ ENDLOCAL
 EXIT /B
 
 
-:SHOWHELP
+:UnsupportedSwitch
+ECHO.
+ECHO Unsupported commandline switch!
+ECHO Run "%~nx0 help" for details about the commandline switches.
+ENDLOCAL
+EXIT /B
+
+
+:ShowHelp
 TITLE %~nx0 Help
 ECHO.
-ECHO Usage:   %~nx0 [Clean^|Build^|Rebuild] [Debug]
+ECHO Usage:
+ECHO %~nx0 [Clean^|Build^|Rebuild] [x86^|x64^|Both] [Debug^|Release]
 ECHO.
-ECHO Notes:   The arguments are not case sensitive.
+ECHO Notes: You can also prefix the commands with "-", "--" or "/".
+ECHO        The arguments are not case sensitive and can be ommitted.
 ECHO. & ECHO.
-ECHO Executing %~nx0 will use the defaults: "%~nx0 build"
+ECHO Executing %~nx0 without any arguments will use the default ones:
+ECHO "%~nx0 Build Both Release"
 ECHO.
+POPD
 ENDLOCAL
 EXIT /B
