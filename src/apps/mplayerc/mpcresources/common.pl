40bd3c2248064eee02f1aac4020bd9dc21d2056d
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-10 11:25:38 +0000

    common.pl: skip MfcMaskedEdit
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4673 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/apps/mplayerc/mpcresources/common.pl
+++ src/apps/mplayerc/mpcresources/common.pl
@@ -263,6 +263,7 @@ sub skipNonTranslatedStr {
 					Static|Button|msctls_updown32|SysListView32|msctls_trackbar32					#
 					|msctls_progress32|SysTreeView32|SysTabControl32|SysAnimate32|SysLink			#skip built-in control names
 					|MS\sShell\sDlg|MS\sSans\sSerif|MS\sUI\sGothic									#skip dialog font, but maybe should not because 3 asian languages need change this
+					|MfcMaskedEdit																	#skip MfcMaskedEdit
 					|\\000|(LANGUAGE.+)?\\r\\n|\+\/-												#skip \r\n  \000 +- etc
 					|<a>http.+<\/a>|http:\/\/														#skip http links
 					|Media\sPlayer\sClassic\s-?\sHome\sCinema|mpc-hc|MPC-HC\sTeam					#skip app names
