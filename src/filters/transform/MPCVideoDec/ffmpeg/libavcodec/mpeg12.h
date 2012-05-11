f1618126ff2b6a962bd7ee9bc8b3382131636259
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-11 20:11:42 +0000

    update ffmpeg (9eb99ab25b5ec9af9e575371c9ef84a37c63b3e5)
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4696 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MPCVideoDec/ffmpeg/libavcodec/mpeg12.h
+++ src/filters/transform/MPCVideoDec/ffmpeg/libavcodec/mpeg12.h
@@ -47,6 +47,7 @@ typedef struct Mpeg1Context {
     AVRational frame_rate_ext;       ///< MPEG-2 specific framerate modificator
     int sync;                        ///< Did we reach a sync point like a GOP/SEQ/KEYFrame?
     int tmpgexs;
+    int parsed_extra;
     // ==> Start patch MPC
     DXVA_SliceInfo* pSliceInfo;
     uint8_t* prev_slice;
