d63f55241cf2df95cbe13a9c1fff80b7da111407
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-13 10:59:27 +0000

    array_allocator.h, PODtypes.h: keep one line statements
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4707 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/filters/transform/MPCVideoDec/ffmpeg/array_allocator.h
+++ src/filters/transform/MPCVideoDec/ffmpeg/array_allocator.h
@@ -16,12 +16,8 @@ public:
     typedef T& reference;
     typedef const T& const_reference;
 
-    pointer address(reference r) const {
-        return &r;
-    }
-    const_pointer address(const_reference r) const {
-        return &r;
-    }
+    pointer address(reference r) const {return &r;}
+    const_pointer address(const_reference r) const {return &r;}
 
     array_allocator() throw() {}
     template <class U,size_t sz> array_allocator(const array_allocator<U,sz>& ) throw() {}
@@ -31,33 +27,21 @@ public:
         p = ((T  *)::operator new(size * sizeof (T)));
         return p;
     }
-    void deallocate(pointer p, size_type) {
-        delete p;
-    }
+    void deallocate(pointer p, size_type) {delete p;}
 
     //Use placement new to engage the constructor
-    void construct(pointer p, const T& val) {
-        new((void*)p) T(val);
-    }
-    void destroy(pointer p) {
-        ((T*)p)->~T();
-    }
+    void construct(pointer p, const T& val) {new((void*)p) T(val);}
+    void destroy(pointer p) {((T*)p)->~T();}
 
-    size_type max_size() const throw() {
-        return size;
-    }
-    template<class U> struct rebind {
-        typedef array_allocator<U,size> other;
-    };
+    size_type max_size() const throw() {return size;}
+    template<class U> struct rebind {typedef array_allocator<U,size> other;};
 };
 
 template<class T,size_t size> struct array_vector : std::vector<T, array_allocator<T,size> > {
 };
 
-#if defined(__INTEL_COMPILER) || defined(__GNUC__) || (_MSC_VER>=1300)
-template<class T,size_t a> struct allocator_traits<array_allocator<T,a> > {
-    enum {is_static=true};
-};
+#if defined(__INTEL_COMPILER) || defined(__GNUC__) || (_MSC_VER >= 1300)
+template<class T,size_t a> struct allocator_traits<array_allocator<T,a> > {enum {is_static=true};};
 #endif
 
 #endif
