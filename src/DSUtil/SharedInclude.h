2b3555ef9036edc0df93c29112f20f723688e328
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-14 19:37:12 +0000

    remove _CRT_SECURE_NO_WARNINGS
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4727 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- src/DSUtil/SharedInclude.h
+++ src/DSUtil/SharedInclude.h
@@ -22,14 +22,8 @@
 
 #pragma once
 
-// TODO: check 4731 and 4799
-//#pragma warning(disable:4244 4267)
+// TODO: check 4731 and 4799, search for "pragma warning"
 #pragma warning(disable:4995)
-#ifdef _DEBUG
-// Remove this if you want to see all the "unsafe" functions used
-// For Release builds _CRT_SECURE_NO_WARNINGS is defined
-#pragma warning(disable:4996)
-#endif
 
 #ifdef _DEBUG
 #define _CRTDBG_MAP_ALLOC // include Microsoft memory leak detection procedures
