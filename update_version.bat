45bec579e10a9d78cdbfef4e87047af50bf97a7c
XhmikosR <xhmikosr@users.sourceforge.net>
2012-05-29 19:54:33 +0000

    update_version.bat: add license header
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@4940 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- update_version.bat
+++ update_version.bat
@@ -1,4 +1,24 @@
 @ECHO OFF
+REM $Id$
+REM
+REM (C) 2010-2012 see Authors.txt
+REM
+REM This file is part of MPC-HC.
+REM
+REM MPC-HC is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation; either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM MPC-HC is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 SETLOCAL
 
 PUSHD %~dp0
