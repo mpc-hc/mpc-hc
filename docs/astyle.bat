9a444c24cfea2d07fec73dd84377c36588456902
XhmikosR <xhmikosr@users.sourceforge.net>
2012-06-15 18:09:22 +0000

    move astyle options to astyle.ini
    git-svn-id: https://mpc-hc.svn.sourceforge.net/svnroot/mpc-hc/trunk@5141 10f7b99b-c216-0410-bff0-8a66a9350fd8

--- docs/astyle.bat
+++ docs/astyle.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 SETLOCAL
+TITLE Applying astyle using %~dp0astyle.ini
 
 PUSHD %~dp0
 
@@ -13,24 +14,7 @@ IF %ERRORLEVEL% NEQ 0 (
   GOTO END
 )
 
-rem current command
-astyle.exe^
- --indent=spaces=4 --style=kr --indent-switches --indent-namespaces --indent-col1-comments^
- --pad-header --pad-oper --unpad-paren --align-pointer=type --align-reference=type^
- --add-brackets --lineend=windows --preserve-date --keep-one-line-blocks --keep-one-line-statements^
- --convert-tabs --suffix=none --ignore-exclude-errors --ignore-exclude-errors-x --recursive^
- --exclude=LineNumberEdit.cpp --exclude=LineNumberEdit.h --exclude=Struct.h --exclude=atl^
- --exclude=avisynth --exclude=detours --exclude=dx --exclude=lglcd --exclude=ogg --exclude=qt^
- --exclude=realmedia --exclude=vd2 --exclude=winddk --exclude=inttypes.h --exclude=MediaInfoDLL.h^
- --exclude=unrar.h --exclude=Version_rev.h --exclude=thirdparty --exclude=resource.h^
- --exclude=simd.h --exclude=simd_common.h --exclude=libmpeg2.h --exclude=libmpeg2.cpp^
- --exclude=attributes.h --exclude=MPEG2Dec.h --exclude=MPEG2Dec.cpp --exclude=idctref.cpp^
- --exclude=idctfpu.cpp --exclude=vd_asm.cpp --exclude=vd_asm.h --exclude=vd.cpp --exclude=vd.h^
- --exclude=deinterlace.cpp --exclude=mmintrin64.c --exclude=simd_instructions.h --exclude=DeCSS^
- --exclude=ffImgfmt.cpp --exclude=ffImgfmt.h --exclude=H264RandomAccess.cpp^
- --exclude=H264RandomAccess.h --exclude=array_allocator.h --exclude=char_t.h^
- --exclude=HScrollListBox.cpp --exclude=HScrollListBox.h ..\*.h ..\*.cpp
-
+astyle.exe --options=astyle.ini ..\*.h ..\*.cpp
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO ERROR: Something went wrong!
 
 :END
