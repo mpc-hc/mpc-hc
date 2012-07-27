02107864b873f7853271249d5769d76216a28835
xhmikosr <xhmikosr@yahoo.com>
2012-07-27 22:05:53 +0300

    properly check for git and perl in the resources batch files

--- src/mpc-hc/mpcresources/sync.bat
+++ src/mpc-hc/mpcresources/sync.bat
@@ -1,24 +1,27 @@
 @echo off
+setlocal
 
 rem An all in one script which demonstrates how to sync all locale rc files to the latest mplayerc.rc.
 rem It will try to patch existing local rc files first, then sync them to mplayerc.rc.
 rem Then it will overwrite rc files with new rc ones, and after that it will generate the text files.
 rem This is only an example.
 
+call "common.bat"
+if %errorlevel% neq 0 goto end
+
 echo Get the latest mplayerc.rc from repository first...
-git show HEAD:../mplayerc.rc > $$TEMP$$.old
-if %ERRORLEVEL% neq 0 goto NOGITCLI
+git.exe show HEAD:../mplayerc.rc > $$TEMP$$.old
 echo ----------------------
 
 for %%i in (*.rc) do (
   echo Patching file %%i
-  perl patch.pl -i text\%%i.txt %%i
+  perl.exe patch.pl -i text\%%i.txt %%i
   echo ----------------------
 )
 echo ----------------------
 
 echo Generating new rc files...
-perl rcfile.pl -b $$TEMP$$.old
+perl.exe rcfile.pl -b $$TEMP$$.old
 del $$TEMP$$.old
 echo ----------------------
 
@@ -27,15 +30,11 @@ echo ----------------------
 
 echo Generating new string files...
 copy ..\mplayerc.rc .
-perl rcstrings.pl -a
+perl.exe rcstrings.pl -a
 del mplayerc.rc
 echo ----------------------
-goto END
 
-:NOGITCLI
-echo You'll need git command line tool to use this script.
-echo Or you can just checkout the head revision of mplayerc.rc file by yourself,
-echo put it somewhere and then use the -b option to point to it.
-
-:END
+:end
 pause
+endlocal
+exit /b
