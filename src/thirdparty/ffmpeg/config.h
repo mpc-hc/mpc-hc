9c24289189d17683997812296843c796a0ad24c8
XhmikosR <xhmikosr@users.sourceforge.net>
2012-08-16 23:38:18 +0300

    update ffmpeg/config.asm and ffmpeg/config.h

--- src/thirdparty/ffmpeg/config.h
+++ src/thirdparty/ffmpeg/config.h
@@ -8,7 +8,7 @@
   #define HAVE_AMD3DNOWEXT 1
   #define HAVE_AVX 1
   #define HAVE_MMX 1
-  #define HAVE_MMX2 1
+  #define HAVE_MMX2 HAVE_MMXEXT
   #define HAVE_MMXEXT 1
   #define HAVE_SSE 1
   #define HAVE_SSSE3 1
