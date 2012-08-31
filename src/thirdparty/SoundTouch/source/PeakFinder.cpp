3ec1fe453443270ac50c6b223d493a422b9ad0d4
XhmikosR <xhmikosr@users.sourceforge.net>
2012-08-31 12:37:59 +0300

    update SoundTouch to v1.7.0pre r150

--- src/thirdparty/SoundTouch/source/PeakFinder.cpp
+++ src/thirdparty/SoundTouch/source/PeakFinder.cpp
@@ -64,7 +64,7 @@ int PeakFinder::findTop(const float *data, int peakpos) const
 
     refvalue = data[peakpos];
 
-    // seek within ï¿½10 points
+    // seek within ±10 points
     start = peakpos - 10;
     if (start < minPos) start = minPos;
     end = peakpos + 10;
@@ -103,7 +103,7 @@ int PeakFinder::findGround(const float *data, int peakpos, int direction) const
 
     pos = peakpos;
 
-    while ((pos > minPos) && (pos < maxPos))
+    while ((pos > minPos+1) && (pos < maxPos-1))
     {
         int prevpos;
 
@@ -192,11 +192,8 @@ double PeakFinder::getPeakCenter(const float *data, int peakpos) const
     gp1 = findGround(data, peakpos, -1);
     gp2 = findGround(data, peakpos, 1);
 
-    groundLevel = max(data[gp1], data[gp2]);
+    groundLevel = 0.5 * (data[gp1] + data[gp2]);
     peakLevel = data[peakpos];
-
-    if (groundLevel < 1e-9) return 0;                // ground level too small => detection failed
-    if ((peakLevel / groundLevel) < 1.3) return 0;   // peak less than 30% of the ground level => no good peak detected
 
     // calculate 70%-level of the peak
     cutLevel = 0.70f * peakLevel + 0.30f * groundLevel;
@@ -260,7 +257,7 @@ double PeakFinder::detectPeak(const float *data, int aminPos, int amaxPos)
 
         // accept harmonic peak if 
         // (a) it is found
-        // (b) is within ï¿½4% of the expected harmonic interval
+        // (b) is within ±4% of the expected harmonic interval
         // (c) has at least half x-corr value of the max. peak
 
         double diff = harmonic * peaktmp / highPeak;
@@ -269,7 +266,7 @@ double PeakFinder::detectPeak(const float *data, int aminPos, int amaxPos)
         // now compare to highest detected peak
         i1 = (int)(highPeak + 0.5);
         i2 = (int)(peaktmp + 0.5);
-        if (data[i2] >= 0.5*data[i1])
+        if (data[i2] >= 0.4*data[i1])
         {
             // The harmonic is at least half as high primary peak,
             // thus use the harmonic peak instead
