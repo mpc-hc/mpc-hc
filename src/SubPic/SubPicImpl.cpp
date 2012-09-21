d119e7cf7bc038b144c35c8a2c067e308ebabd69
Underground78 <underground78@users.sourceforge.net>
2012-09-21 13:22:09 +0200

    SubPicImpl: Check that the static resource is big enough.
    This commit fixes some cases where subtitles weren't renderered using the internal subtitles renderer with DX9-based renderers.

--- src/SubPic/SubPicImpl.cpp
+++ src/SubPic/SubPicImpl.cpp
@@ -224,6 +224,12 @@ STDMETHODIMP CSubPicAllocatorImpl::GetStatic(ISubPic** ppSubPic)
         return E_POINTER;
     }
 
+    SIZE maxSize;
+    if (m_pStatic && (FAILED(m_pStatic->GetMaxSize(&maxSize)) || maxSize.cx < m_cursize.cx || maxSize.cy < m_cursize.cy)) {
+        m_pStatic.Release();
+        m_pStatic = NULL;
+    }
+
     if (!m_pStatic) {
         if (!Alloc(true, &m_pStatic) || !m_pStatic) {
             return E_OUTOFMEMORY;
