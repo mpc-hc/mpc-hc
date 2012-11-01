d2c02d892e7dada9dbbcc10037fe3fa278cf4829
Underground78 <underground78@users.sourceforge.net>
2012-11-01 17:54:45 +0100

    Web server: Don't use compression on images.
    Recompressing already compressed files has no or very little effect and
    has a negative impact on performance.

--- src/mpc-hc/WebServer.cpp
+++ src/mpc-hc/WebServer.cpp
@@ -463,7 +463,7 @@ void CWebServer::OnRequest(CWebClientSocket* pClient, CStringA& hdr, CStringA& b
     }
 
     // gzip
-    if (AfxGetAppSettings().fWebServerUseCompression && hdr.Find("Content-Encoding:") < 0)
+    if (AfxGetAppSettings().fWebServerUseCompression && hdr.Find("Content-Encoding:") < 0 && ext != ".png" && ext != ".jpeg" && ext != ".gif")
         do {
             CString accept_encoding;
             pClient->m_hdrlines.Lookup(_T("accept-encoding"), accept_encoding);
