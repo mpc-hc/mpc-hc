8b84c9599891f451819d3dc20432a829508e47ad
m0viefreak <m0viefreak.cm@googlemail.com>
2013-01-07 23:55:57 +0100

    Added scripts to convert between hashes and revision numbers.
    Original script by Armada

---
+++ hash2rev.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# (C) 2013 see Authors.txt
+#
+# This file is part of MPC-HC.
+#
+# MPC-HC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# MPC-HC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+[[ -n "$1" ]] || exit 1
+# This is the last svn changeset, the number and hash can be automatically
+# calculated, but it is slow to do that. So it is better to have it hardcoded.
+# We'll need to update this with the last svn data before committing this script
+svnrev=5597
+svnhash="f669833b77e6515dc5f0a682c5bf665f9a81b2ec"
+
+if [[ ! -d ".git" ]] || ! command -v git >/dev/null 2>&1 ; then
+  # If the folder ".git" doesn't exist or git isn't available exit the script
+  exit 1
+fi
+
+# If the input hash is after the switch to git
+if [[ $(git merge-base $svnhash "$1") == $(git rev-parse --verify "$svnhash") ]] ; then
+  # Count how many changesets we have since the last svn changeset until the input hash
+  ver=$(git rev-list --count "$svnhash..$1")
+  # Now add it with to last svn revision number
+  ver=$((ver+svnrev))
+else
+  # Get the revision by parsing the git-svn-id
+  ver=$(git log -1 --format="%b" "$1")
+  ver=${ver#*@}
+  ver=${ver%% *}
+fi
+
+echo "$ver"
