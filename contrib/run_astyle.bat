ab1a71dfbb4a86de5750def42183ffa64886dde4
Armada <jules.blok@gmail.com>
2013-01-09 01:17:39 +0200

    Allow files to be dropped onto run_astyle.bat for styling

--- contrib/run_astyle.bat
+++ contrib/run_astyle.bat
@@ -3,7 +3,13 @@ SETLOCAL
 
 PUSHD %~dp0
 TITLE Running astyle using astyle.ini
-astyle --options=astyle.ini ..\*.h ..\*.cpp
+
+IF "%~1" == "" (
+  astyle -r --options=astyle.ini ..\*.h ..\*.cpp
+) ELSE (
+  FOR %%G IN (%*) DO astyle --options=astyle.ini %%G
+)
+
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO ERROR: Something went wrong!
 POPD
 
