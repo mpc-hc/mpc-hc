afb62761ff970bf390c9148a185875ef0c04c579
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-09 14:27:03 +0200

    simplify coverity.bat

--- contrib/coverity.bat
+++ contrib/coverity.bat
@@ -4,7 +4,7 @@ SET COVDIR=H:\progs\thirdparty\cov-analysis-win64-6.0.5
 
 CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
-"%COVDIR%\bin\cov-build.exe" --dir cov-int mpc-hc-covbuild.bat
+"%COVDIR%\bin\cov-build.exe" --dir cov-int devenv "..\mpc-hc_vs2012.sln" /Rebuild "Release|Win32"
 "%PROGRAMFILES%\7za.exe" a -ttar MPC-HC.tar cov-int
 "%PROGRAMFILES%\7za.exe" a -tgzip MPC-HC.tgz MPC-HC.tar
 IF EXIST "MPC-HC.tar" DEL "MPC-HC.tar"
--- contrib/mpc-hc-covbuild.bat
+++
@@ -1,1 +0,0 @@
-devenv "..\mpc-hc_vs2012.sln" /project "..\src\mpc-hc\mpc-hc.vcxproj" /Rebuild "Release|Win32"
