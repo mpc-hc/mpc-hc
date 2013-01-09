15c9c253d67ecc618b61b13ab44cd4d4f3bdf298
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-09 10:17:16 +0200

    coverty.bat: use VS2010 for now

--- contrib/coverty.bat
+++ contrib/coverty.bat
@@ -2,7 +2,7 @@
 
 SET COVDIR=H:\progs\thirdparty\cov-analysis-win64-6.0.5
 
-CALL "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
 "%COVDIR%\bin\cov-build.exe" --dir cov-int mpc-hc-covbuild.bat
 "%PROGRAMFILES%\7za.exe" a -ttar MPC-HC.tar cov-int
