cdc542bdd2890e6ff51590c11b4ab13a538c343c
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-11 11:18:57 +0200

    update coverity.bat

--- contrib/coverity.bat
+++ contrib/coverity.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
+SETLOCAL
 
-SET COVDIR=H:\progs\thirdparty\cov-analysis-win64-6.0.5
+PUSHD %~dp0
+
+SET COVDIR=H:\progs\thirdparty\cov-analysis-win64-6.5.1
 
 CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
@@ -8,3 +11,6 @@ CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 "%PROGRAMFILES%\7za.exe" a -ttar MPC-HC.tar cov-int
 "%PROGRAMFILES%\7za.exe" a -tgzip MPC-HC.tgz MPC-HC.tar
 IF EXIST "MPC-HC.tar" DEL "MPC-HC.tar"
+
+ENDLOCAL
+PAUSE
