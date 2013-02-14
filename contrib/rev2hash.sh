a0aa3b9f0b9bd28a4067fe9da5e301b0b7955529
m0viefreak <m0viefreak.cm@googlemail.com>
2013-02-14 10:16:41 +0100

    contrib/rev2hash.sh: use short hash by default and add option to use full hash

--- contrib/rev2hash.sh
+++ contrib/rev2hash.sh
@@ -16,7 +16,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-(( $1 > 0 )) || exit
+OPTIND=1
+fullhash=0
+hashlength=7
+
+show_help() {
+  echo "Syntax: $0 [options] <rev>"
+  echo "Options:"
+  echo "  -f    show full hash instead of abbreviation"
+  echo "  -h    show this help"
+}
+
+while getopts "hf" opt; do
+  case "$opt" in
+    h)
+      show_help
+      exit 0
+      ;;
+    f)
+      fullhash=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      show_help >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+[[ "$1" == "--" ]] && shift
+
+rev="$1"
+(( rev > 0 )) || { show_help >&2; exit 1; }
 # This is the last svn changeset, the number and hash can be automatically
 # calculated, but it is slow to do that. So it is better to have it hardcoded.
 svnrev=5597
@@ -26,16 +57,17 @@ svnhash="f669833b77e6515dc5f0a682c5bf665f9a81b2ec"
 git rev-parse --git-dir > /dev/null 2>&1 || exit 1
 
 # If this revision is after the switch to git
-if (( $1 > svnrev)); then
+if ((rev > svnrev)); then
   # Calculate how many changesets we need to traverse from the svnrev to HEAD
-  n=$(($1 - svnrev))
+  n=$((rev - svnrev))
 
   # List the changes from svnhash..HEAD in reverse order and pick the ${n}th
   hash=$(git rev-list --reverse $svnhash..HEAD | sed -n "${n}p")
 else
   # Get the hash by parsing the git-svn-id
   # --all is needed in case the given ref is not part of trunk/master but in a tag / svn branch
-  hash=$(git log --all -1 --format="%H" --grep="git-svn-id: .*@$1 ")
+  hash=$(git log --all -1 --format="%H" --grep="git-svn-id: .*@${rev} ")
 fi
 
+[[ $fullhash == 0 ]] && hash="${hash:0:7}"
 echo "$hash"
