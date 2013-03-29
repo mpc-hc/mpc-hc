698a49a438b7b7930dec824feff479e7d8757da7
v0lt <v0lt@rambler.ru>
2013-03-29 06:40:16 +0400

    MpaDecFilter->Mixer: fixed FlushBuffers() function

--- src/filters/transform/MpaDecFilter/Mixer.cpp
+++ src/filters/transform/MpaDecFilter/Mixer.cpp
@@ -246,7 +246,7 @@ void CMixer::FlushBuffers()
         }
 
         // Set Matrix on the context
-        ret = avresample_set_matrix(m_pAVRCxt, m_matrix_dbl, m_in_layout);
+        ret = avresample_set_matrix(m_pAVRCxt, m_matrix_dbl, av_popcount(m_in_layout));
         if (ret < 0) {
             TRACE(_T("Mixer: avresample_set_matrix failed\n"));
             return;
