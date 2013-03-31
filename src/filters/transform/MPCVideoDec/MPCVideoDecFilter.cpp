35637b4878e5bff41519dea06cf6690dc7f6607c
v0lt <v0lt@rambler.ru>
2013-03-31 22:57:34 +0400

    MPCVideoDec: fixed playback h264 after "stop" on Intel HD Graphics (second very safe version).

--- src/filters/transform/MPCVideoDec/MPCVideoDecFilter.cpp
+++ src/filters/transform/MPCVideoDec/MPCVideoDecFilter.cpp
@@ -2363,6 +2363,10 @@ HRESULT CMPCVideoDecFilter::CreateDXVA2Decoder(UINT nNumRenderTargets, IDirect3D
             pDecoderRenderTargets, nNumRenderTargets, &pDirectXVideoDec);
 
     if (SUCCEEDED(hr)) {
+        if (m_nPCIVendor == PCIV_Intel) {
+            // need recreate dxva decoder after "stop" on Intel HD Graphics 
+            SAFE_DELETE (m_pDXVADecoder);
+        }
         if (!m_pDXVADecoder) {
             m_pDXVADecoder  = CDXVADecoder::CreateDecoder(this, pDirectXVideoDec, &m_DXVADecoderGUID, GetPicEntryNumber(), &m_DXVA2Config);
             if (m_pDXVADecoder) {
