b2cbfa5ab19d6fa981d74121cc92c7b601fff782
Alex Marsev <alex.marsev@gmail.com>
2013-04-03 17:36:21 +0300

    Shaders: add license headers to resources

--- src/mpc-hc/res/shaders/external/0-255 to 16-235.hlsl
+++ src/mpc-hc/res/shaders/external/0-255 to 16-235.hlsl
@@ -1,3 +1,23 @@
+/*
+ * (C) 2009-2013 see Authors.txt
+ *
+ * This file is part of MPC-HC.
+ *
+ * MPC-HC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-HC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 sampler s0 : register(s0);
 
 #define const_1 ( 16.0 / 255.0)
--- src/mpc-hc/res/shaders/external/BT.601 to BT.709.hlsl
+++ src/mpc-hc/res/shaders/external/BT.601 to BT.709.hlsl
@@ -1,4 +1,23 @@
-// (C) 2011 Jan-Willem Krans (janwillem32 <at> hotmail.com) released under GPL v2; see COPYING.txt
+/*
+ * (C) 2011 Jan-Willem Krans (janwillem32 <at> hotmail.com)
+ * (C) 2011-2013 see Authors.txt
+ *
+ * This file is part of MPC-HC.
+ *
+ * MPC-HC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-HC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 // Correct video colorspace BT.601 [SD] to BT.709 [HD] for HD video input
 // Use this shader only if BT.709 [HD] encoded video is incorrectly matrixed to full range RGB with the BT.601 [SD] colorspace.
