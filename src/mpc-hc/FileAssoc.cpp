ae3db48090dc9837fbb439503075521e356dcfa7
Armada <jules.blok@gmail.com>
2013-05-09 13:13:54 +0200

    CFileAssoc: Prevent non-associable formats from being registered.

--- src/mpc-hc/FileAssoc.cpp
+++ src/mpc-hc/FileAssoc.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2012 see Authors.txt
+ * (C) 2006-2013 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -479,6 +479,11 @@ bool CFileAssoc::AreRegisteredFileContextMenuEntries(CString strExt)
 
 bool CFileAssoc::Register(CMediaFormatCategory& mfc, bool bRegister, bool bRegisterContextMenuEntries, bool bAssociatedWithIcon)
 {
+    if (!mfc.IsAssociable()) {
+        ASSERT(FALSE);
+        return false;
+    }
+
     CAtlList<CString> exts;
     ExplodeMin(mfc.GetExtsWithPeriod(), exts, ' ');
 
