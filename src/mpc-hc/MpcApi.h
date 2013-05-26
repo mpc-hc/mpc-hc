592f49fd84614054d2b5b0f5d7e58c067da16479
XhmikosR <xhmikosr@users.sourceforge.net>
2013-05-26 15:04:02 +0300

    MpcApi.h: fix a few typos in the comments.

--- src/mpc-hc/MpcApi.h
+++ src/mpc-hc/MpcApi.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2006-2012 see Authors.txt
+ * (C) 2006-2013 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -19,29 +19,29 @@
  */
 
 
-// This file define commands used for "MPC-HC" API. To send commands
-// to mpc-hc, and receive playback notifications, first launch process with the /slave command line
-// argument follow by an HWnd handle use to receive notification :
+// This file defines commands used for "MPC-HC" API. To send commands
+// to MPC-HC and receive playback notifications, first launch MPC-HC with the /slave command line
+// argument followed by a HWnd handle used to receive notification:
 //
 // ..\bin\mpc-hc /slave 125421
 //
-// After startup, mpc-hc send a WM_COPYDATA message to host with COPYDATASTRUCT struct filled with :
-//      - dwData    : CMD_CONNECT
-//      - lpData    : Unicode string containing mpc-hc main window Handle
+// After startup, MPC-HC sends a WM_COPYDATA message to host with COPYDATASTRUCT struct filled with:
+//      - dwData :  CMD_CONNECT
+//      - lpData :  Unicode string containing MPC-HC's main window handle
 //
-// To control mpc-hc, send WM_COPYDATA messages to Hwnd provided on connection. All messages should be
+// To control MPC-HC, send WM_COPYDATA messages to Hwnd provided on connection. All messages should be
 // formatted as null-terminated Unicode strings. For commands or notifications with multiple parameters,
-// values are separated by |
-// If a string contains a |, it will be escaped with a \ so a \| is not a separator
+// values are separated by |.
+// If a string contains a |, it will be escaped with a \ so a \| is not a separator.
 //
-// Ex : When a file is opened, mpc-hc send to host the "now playing" notification :
-//      - dwData    : CMD_NOWPLAYING
-//      - lpData    : title|author|description|filename|duration
+// Ex: When a file is opened, MPC-HC sends to host the "now playing" notification:
+//      - dwData :  CMD_NOWPLAYING
+//      - lpData :  title|author|description|filename|duration
 //
-// Ex : When a DVD is playing, use CMD_GETNOWPLAYING to get:
-//      - dwData    : CMD_NOWPLAYING
-//      - lpData    : dvddomain|titlenumber|numberofchapters|currentchapter|titleduration
-//                              dvddomains : DVD - Stopped, DVD - FirstPlay, DVD - RootMenu, DVD - TitleMenu, DVD - Title
+// Ex: When a DVD is playing, use CMD_GETNOWPLAYING to get:
+//      - dwData :  CMD_NOWPLAYING
+//      - lpData :  dvddomain|titlenumber|numberofchapters|currentchapter|titleduration
+//                  dvddomains: DVD - Stopped, DVD - FirstPlay, DVD - RootMenu, DVD - TitleMenu, DVD - Title
 
 #pragma once
 
@@ -68,11 +68,11 @@ struct MPC_OSDDATA {
 };
 
 //// MPC_OSDDATA.nMsgPos constants (for host side programming):
-//typedef enum {
-//    OSD_NOMESSAGE,
-//    OSD_TOPLEFT,
-//    OSD_TOPRIGHT,
-//} OSD_MESSAGEPOS;
+/*typedef enum {
+    OSD_NOMESSAGE,
+    OSD_TOPLEFT,
+    OSD_TOPRIGHT
+} OSD_MESSAGEPOS;*/
 
 
 typedef enum MPCAPI_COMMAND :
@@ -80,69 +80,69 @@ unsigned int {
     // ==== Commands from MPC to host
 
     // Send after connection
-    // Par 1 : MPC window handle (command should be send to this HWnd)
+    // Parameter 1: MPC window handle (command should be sent to this HWnd)
     CMD_CONNECT             = 0x50000000,
 
     // Send when opening or closing file
-    // Par 1 : current state (see MPC_LOADSTATE enum)
+    // Parameter 1: current state (see MPC_LOADSTATE enum)
     CMD_STATE               = 0x50000001,
 
     // Send when playing, pausing or closing file
-    // Par 1 : current play mode (see MPC_PLAYSTATE enum)
+    // Parameter 1: current play mode (see MPC_PLAYSTATE enum)
     CMD_PLAYMODE            = 0x50000002,
 
     // Send after opening a new file
-    // Par 1 : title
-    // Par 2 : author
-    // Par 3 : description
-    // Par 4 : complete filename (path included)
-    // Par 5 : duration in seconds
+    // Parameter 1: title
+    // Parameter 2: author
+    // Parameter 3: description
+    // Parameter 4: complete filename (path included)
+    // Parameter 5: duration in seconds
     CMD_NOWPLAYING          = 0x50000003,
 
     // List of subtitle tracks
-    // Par 1 : Subtitle track name 0
-    // Par 2 : Subtitle track name 1
+    // Parameter 1: Subtitle track name 0
+    // Parameter 2: Subtitle track name 1
     // ...
-    // Par n : Active subtitle track, -1 if subtitles disabled
+    // Parameter n: Active subtitle track, -1 if subtitles are disabled
     //
     // if no subtitle track present, returns -1
     // if no file loaded, returns -2
     CMD_LISTSUBTITLETRACKS  = 0x50000004,
 
     // List of audio tracks
-    // Par 1 : Audio track name 0
-    // Par 2 : Audio track name 1
+    // Parameter 1: Audio track name 0
+    // Parameter 2: Audio track name 1
     // ...
-    // Par n : Active audio track
+    // Parameter n: Active audio track
     //
-    // if no audio track present, returns -1
-    // if no file loaded, returns -2
+    // if no audio track is present, returns -1
+    // if no file is loaded, returns -2
     CMD_LISTAUDIOTRACKS     = 0x50000005,
 
     // Send current playback position in responce
     // of CMD_GETCURRENTPOSITION.
-    // Par 1 : current position in seconds
+    // Parameter 1: current position in seconds
     CMD_CURRENTPOSITION     = 0x50000007,
 
     // Send the current playback position after a jump.
     // (Automatically sent after a seek event).
-    // Par 1 : new playback position (in seconds).
+    // Parameter 1: new playback position (in seconds).
     CMD_NOTIFYSEEK          = 0x50000008,
 
     // Notify the end of current playback
     // (Automatically sent).
-    // Par 1 : none.
+    // Parameter 1: none.
     CMD_NOTIFYENDOFSTREAM   = 0x50000009,
 
     // Send version str
-    // Par 1 : mpc-hc version
+    // Parameter 1: MPC-HC's version
     CMD_VERSION             = 0x5000000A,
 
     // List of files in the playlist
-    // Par 1 : file path 0
-    // Par 2 : file path 1
+    // Parameter 1: file path 0
+    // Parameter 2: file path 1
     // ...
-    // Par n : active file, -1 if no active file
+    // Parameter n: active file, -1 if no active file
     CMD_PLAYLIST            = 0x50000006,
 
     // Send information about mpc closing
@@ -151,7 +151,7 @@ unsigned int {
     // ==== Commands from host to MPC
 
     // Open new file
-    // Par 1 : file path
+    // Parameter 1: file path
     CMD_OPENFILE            = 0xA0000000,
 
     // Stop playback, but keep file / playlist
@@ -170,7 +170,7 @@ unsigned int {
     CMD_PAUSE = 0xA0000005,
 
     // Add a new file to playlist (did not start playing)
-    // Par 1 : file path
+    // Parameter 1: file path
     CMD_ADDTOPLAYLIST       = 0xA0001000,
 
     // Remove all files from playlist
@@ -182,28 +182,28 @@ unsigned int {
     CMD_REMOVEFROMPLAYLIST  = 0xA0001003,   // TODO
 
     // Cue current file to specific position
-    // Par 1 : new position in seconds
+    // Parameter 1: new position in seconds
     CMD_SETPOSITION         = 0xA0002000,
 
     // Set the audio delay
-    // Par 1 : new audio delay in ms
+    // Parameter 1: new audio delay in ms
     CMD_SETAUDIODELAY       = 0xA0002001,
 
     // Set the subtitle delay
-    // Par 1 : new subtitle delay in ms
+    // Parameter 1: new subtitle delay in ms
     CMD_SETSUBTITLEDELAY    = 0xA0002002,
 
     // Set the active file in the playlist
-    // Par 1 : index of the active file, -1 for no file selected
+    // Parameter 1: index of the active file, -1 for no file selected
     // DOESN'T WORK
     CMD_SETINDEXPLAYLIST    = 0xA0002003,
 
     // Set the audio track
-    // Par 1 : index of the audio track
+    // Parameter 1: index of the audio track
     CMD_SETAUDIOTRACK       = 0xA0002004,
 
     // Set the subtitle track
-    // Par 1 : index of the subtitle track, -1 for disabling subtitles
+    // Parameter 1: index of the subtitle track, -1 for disabling subtitles
     CMD_SETSUBTITLETRACK    = 0xA0002005,
 
     // Ask for a list of the subtitles tracks of the file
@@ -212,14 +212,14 @@ unsigned int {
 
     // Ask for the current playback position,
     // see CMD_CURRENTPOSITION.
-    // Par 1 : current position in seconds
+    // Parameter 1: current position in seconds
     CMD_GETCURRENTPOSITION  = 0xA0003004,
 
     // Jump forward/backward of N seconds,
-    // Par 1 : seconds (negative values for backward)
+    // Parameter 1: seconds (negative values for backward)
     CMD_JUMPOFNSECONDS      = 0xA0003005,
 
-    // ask slave for version
+    // Ask slave for version
     CMD_GETVERSION          = 0xA0003006,
 
     // Ask for a list of the audio tracks of the file
@@ -249,7 +249,7 @@ unsigned int {
     // Decrease volume
     CMD_DECREASEVOLUME      = 0xA0004004,
 
-    // Shader toggle
+    // Toggle shader
     CMD_SHADER_TOGGLE       = 0xA0004005,
 
     // Close App
@@ -258,7 +258,7 @@ unsigned int {
     // Set playing rate
     CMD_SETSPEED            = 0xA0004008,
 
-    // show host defined OSD message string
+    // Show host defined OSD message string
     CMD_OSDSHOWMESSAGE      = 0xA0005000
 
 };
