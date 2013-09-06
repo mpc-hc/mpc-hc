0e2bad8e36d1c673cc8e86d97a5228fe4d7cd751
Armada <jules.blok@gmail.com>
2013-09-06 23:14:41 +0200

    MainFrm: Use GetFileName() when saving subtitles.
    Fixes cases where URLs are being used as a suggested subtitle filename.

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -5329,14 +5329,14 @@ void CMainFrame::OnFileSavesubtitle()
             return;
         }
 
-        OpenMediaData* pOMD = m_wndPlaylistBar.GetCurOMD();
-        CString suggestedFileName(_T(""));
-        if (OpenFileData* p = dynamic_cast<OpenFileData*>(pOMD)) {
-            // HACK: get the file name from the current playlist item
-            suggestedFileName = m_wndPlaylistBar.GetCurFileName();
-            suggestedFileName = suggestedFileName.Left(suggestedFileName.ReverseFind('.')); // exclude the extension, it will be auto completed
+        CPath suggestedFileName(GetFileName());
+        suggestedFileName.RemoveExtension(); // exclude the extension, it will be auto-completed
+
+        // Try to detect the directory of the current playlist item, and prepend it if it's valid
+        CPath path(m_wndPlaylistBar.GetCurFileName());
+        if (path.RemoveFileSpec() && path.IsDirectory() && path.Append(suggestedFileName)) {
+            suggestedFileName = path;
         }
-        delete pOMD;
 
         if (clsid == __uuidof(CVobSubFile)) {
             CVobSubFile* pVSF = (CVobSubFile*)(ISubStream*)pSubInput->subStream;
