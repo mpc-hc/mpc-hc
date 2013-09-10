b8bbd97d9cace61694801715b739450ad9762245
Underground78 <underground78@users.sourceforge.net>
2013-09-10 23:05:07 +0200

    Audio Switcher: Fix a crash when the audio resampling and channel remapping were enabled.
    This commit fixes #3425.

--- src/filters/switcher/AudioSwitcher/AudioSwitcher.cpp
+++ src/filters/switcher/AudioSwitcher/AudioSwitcher.cpp
@@ -282,19 +282,33 @@ HRESULT CAudioSwitcherFilter::Transform(IMediaSample* pIn, IMediaSample* pOut)
         return S_OK;
     }
 
+    bool bDownSampleTo441 = (m_fDownSampleTo441
+                             && wfe->nSamplesPerSec > 44100 && wfeout->nSamplesPerSec == 44100
+                             && wfe->wBitsPerSample <= 16 && fPCM);
+
+    BYTE* pTmp = nullptr;
+    BYTE* pDst = nullptr;
+    if (bDownSampleTo441 && m_fCustomChannelMapping && wfe->nChannels <= AS_MAX_CHANNELS) {
+        pDst = pTmp = DEBUG_NEW BYTE[len * bps * wfeout->nChannels];
+    } else {
+        pDst = pDataOut;
+    }
+
     if (m_fCustomChannelMapping && wfe->nChannels <= AS_MAX_CHANNELS) {
         size_t channelsCount = m_chs[wfe->nChannels - 1].GetCount();
         ASSERT(channelsCount == 0 || wfeout->nChannels == channelsCount);
+
+        int srcstep = bps * wfe->nChannels;
+        int dststep = bps * wfeout->nChannels;
+        int channels = wfe->nChannels;
+
         if (channelsCount > 0 && wfeout->nChannels == channelsCount) {
             for (int i = 0; i < wfeout->nChannels; i++) {
                 DWORD mask = m_chs[wfe->nChannels - 1][i].Channel;
 
                 BYTE* src = pDataIn;
-                BYTE* dst = &pDataOut[bps * i];
+                BYTE* dst = &pDst[bps * i];
 
-                int srcstep = bps * wfe->nChannels;
-                int dststep = bps * wfeout->nChannels;
-                int channels = wfe->nChannels;
                 if (fPCM) {
                     if (wfe->wBitsPerSample == 8) {
                         for (int k = 0; k < len; k++, src += srcstep, dst += dststep) {
@@ -341,15 +355,13 @@ HRESULT CAudioSwitcherFilter::Transform(IMediaSample* pIn, IMediaSample* pOut)
         }
     }
 
-    if (m_fDownSampleTo441
-            && wfe->nSamplesPerSec > 44100 && wfeout->nSamplesPerSec == 44100
-            && wfe->wBitsPerSample <= 16 && fPCM) {
+    if (bDownSampleTo441) {
         if (BYTE* buff = DEBUG_NEW BYTE[len * bps]) {
             for (int ch = 0; ch < wfeout->nChannels; ch++) {
                 ZeroMemory(buff, len * bps);
 
                 for (int i = 0; i < len; i++) {
-                    memcpy(buff + i * bps, (char*)pDataOut + (ch + i * wfeout->nChannels)*bps, bps);
+                    memcpy(buff + i * bps, (char*)pDst + (ch + i * wfeout->nChannels)*bps, bps);
                 }
 
                 m_pResamplers[ch]->Downsample(buff, len, buff, lenout);
@@ -361,6 +373,8 @@ HRESULT CAudioSwitcherFilter::Transform(IMediaSample* pIn, IMediaSample* pOut)
 
             delete [] buff;
         }
+
+        delete [] pTmp;
     }
 
     if (m_fNormalize || m_boostFactor > 1) {
