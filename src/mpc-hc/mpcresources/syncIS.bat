deedd03729a291859df480e6d2918a64a64daa40
Underground78 <underground78@users.sourceforge.net>
2013-12-21 18:26:16 +0100

    Add an equivalent to sync.bat for the installer strings.
    Was forgotten in 3b6addad1d6997bc5ff3fbe532a06bdde1710f3b...

---
+++ src/mpc-hc/mpcresources/syncIS.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+REM (C) 2013 see Authors.txt
+REM
+REM This file is part of MPC-HC.
+REM
+REM MPC-HC is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation; either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM MPC-HC is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+SETLOCAL
+PUSHD %~dp0
+
+CALL "common_python.bat"
+IF %ERRORLEVEL% NEQ 0 GOTO END
+
+ECHO Updating the installer POT file
+python.exe UpdateISPOT.py
+ECHO ----------------------
+
+FOR %%i IN (PO\mpc-hc.installer.*.strings.po) DO (
+  ECHO Updating PO file %%i
+  python.exe UpdateISPO.py %%~ni
+  ECHO ----------------------
+)
+
+ECHO Updating IS file
+python.exe UpdateIS.py
+ECHO ----------------------
+
+:END
+PAUSE
+ENDLOCAL
+EXIT /B
