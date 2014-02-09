mpc-hc/mpc-hc

commit aa4a1acb57fc28fd464942d48b5f9987d88df82f

ISR: Vobsub/PGS/DVB subtitles are now rendered relative to the video frame.

src/filters/renderer/VideoRenderers/DX7AllocatorPresenter.cpp
@@ -414,7 +414,7 @@ STDMETHODIMP_(bool) CDX7AllocatorPresenter::Paint(bool fAll)
 
         // paint the text on the backbuffer
 
-        AlphaBltSubPic(rSrcPri.Size());
+        AlphaBltSubPic(rDstPri, rDstVid);
     }
 
     // wait vsync

src/SubPic/ISubPic.h
@@ -49,6 +49,11 @@ struct SubPicDesc {
 };
 #pragma pack(pop)
 
+enum RelativeTo {
+    WINDOW,
+    VIDEO
+};
+
 //
 // ISubPic
 //
@@ -77,8 +82,10 @@ public IUnknown {
     STDMETHOD(Unlock)(RECT* pDirtyRect /*[in]*/) PURE;
 
     STDMETHOD(AlphaBlt)(RECT * pSrc, RECT * pDst, SubPicDesc* pTarget = nullptr /*[in]*/) PURE;
-    STDMETHOD(GetSourceAndDest)(SIZE* pSize /*[in]*/, RECT* pRcSource /*[out]*/, RECT* pRcDest /*[out]*/) PURE;
+    STDMETHOD(GetSourceAndDest)(RECT rcWindow /*[in]*/, RECT rcVideo /*[in]*/, RECT* pRcSource /*[out]*/, RECT* pRcDest /*[out]*/) PURE;
     STDMETHOD(SetVirtualTextureSize)(const SIZE pSize, const POINT pTopLeft) PURE;
+    STDMETHOD(GetRelativeTo)(RelativeTo* pRelativeTo /*[out]*/) PURE;
+    STDMETHOD(SetRelativeTo)(RelativeTo relativeTo /*[in]*/) PURE;
 
     STDMETHOD_(REFERENCE_TIME, GetSegmentStart)() PURE;
     STDMETHOD_(REFERENCE_TIME, GetSegmentStop)() PURE;
@@ -128,6 +135,7 @@ public IUnknown {
 
     STDMETHOD(Render)(SubPicDesc & spd, REFERENCE_TIME rt, double fps, RECT & bbox) PURE;
     STDMETHOD(GetTextureSize)(POSITION pos, SIZE & MaxTextureSize, SIZE & VirtualSize, POINT & VirtualTopLeft) PURE;
+    STDMETHOD(GetRelativeTo)(POSITION pos, RelativeTo & relativeTo) PURE;
 };
 
 //

src/SubPic/SubPicAllocatorPresenterImpl.cpp
@@ -71,33 +71,14 @@ STDMETHODIMP CSubPicAllocatorPresenterImpl::NonDelegatingQueryInterface(REFIID r
         __super::NonDelegatingQueryInterface(riid, ppv);
 }
 
-void CSubPicAllocatorPresenterImpl::AlphaBltSubPic(CSize size, SubPicDesc* pTarget)
+void CSubPicAllocatorPresenterImpl::AlphaBltSubPic(const CRect& windowRect, const CRect& videoRect, SubPicDesc* pTarget)
 {
     CComPtr<ISubPic> pSubPic;
     if (m_pSubPicQueue->LookupSubPic(m_rtNow, pSubPic)) {
         CRect rcSource, rcDest;
-        if (SUCCEEDED(pSubPic->GetSourceAndDest(&size, rcSource, rcDest))) {
+        if (SUCCEEDED(pSubPic->GetSourceAndDest(windowRect, videoRect, rcSource, rcDest))) {
             pSubPic->AlphaBlt(rcSource, rcDest, pTarget);
         }
-        /*SubPicDesc spd;
-          pSubPic->GetDesc(spd);
-
-          if (spd.w > 0 && spd.h > 0)
-          {
-              CRect r;
-              pSubPic->GetDirtyRect(r);
-
-              // FIXME
-              r.DeflateRect(1, 1);
-
-              CRect rDstText(
-                  r.left * size.cx / spd.w,
-                  r.top * size.cy / spd.h,
-                  r.right * size.cx / spd.w,
-                  r.bottom * size.cy / spd.h);
-
-              pSubPic->AlphaBlt(r, rDstText, pTarget);
-          }*/
     }
 }
 

src/SubPic/SubPicAllocatorPresenterImpl.h
@@ -52,7 +52,7 @@ protected:
     bool m_bDeviceResetRequested;
     bool m_bPendingResetDevice;
 
-    void AlphaBltSubPic(CSize size, SubPicDesc* pTarget = nullptr);
+    void AlphaBltSubPic(const CRect& windowRect, const CRect& videoRect, SubPicDesc* pTarget = nullptr);
 
     XForm m_xform;
     void Transform(CRect r, Vector v[4]);

src/SubPic/SubPicImpl.cpp
@@ -40,6 +40,7 @@ CSubPicImpl::CSubPicImpl()
     , m_VirtualTextureSize(0, 0)
     , m_VirtualTextureTopLeft(0, 0)
     , m_invAlpha(false)
+    , m_relativeTo(WINDOW)
 {
 }
 
@@ -115,21 +116,27 @@ STDMETHODIMP CSubPicImpl::GetDirtyRect(RECT* pDirtyRect)
     return pDirtyRect ? *pDirtyRect = m_rcDirty, S_OK : E_POINTER;
 }
 
-STDMETHODIMP CSubPicImpl::GetSourceAndDest(SIZE* pSize, RECT* pRcSource, RECT* pRcDest)
+STDMETHODIMP CSubPicImpl::GetSourceAndDest(RECT rcWindow, RECT rcVideo, RECT* pRcSource, RECT* pRcDest)
 {
     CheckPointer(pRcSource, E_POINTER);
     CheckPointer(pRcDest,   E_POINTER);
 
     if (m_size.cx > 0 && m_size.cy > 0) {
+        CRect rcTarget = (m_relativeTo == WINDOW) ? rcWindow : rcVideo;
+        CSize szTarget = rcTarget.Size();
+
         CRect rcTemp = m_rcDirty;
 
         *pRcSource = rcTemp;
 
         rcTemp.OffsetRect(m_VirtualTextureTopLeft);
-        *pRcDest = CRect(rcTemp.left   * pSize->cx / m_VirtualTextureSize.cx,
-                         rcTemp.top    * pSize->cy / m_VirtualTextureSize.cy,
-                         rcTemp.right  * pSize->cx / m_VirtualTextureSize.cx,
-                         rcTemp.bottom * pSize->cy / m_VirtualTextureSize.cy);
+        rcTemp = CRect(rcTemp.left   * szTarget.cx / m_VirtualTextureSize.cx,
+                       rcTemp.top    * szTarget.cy / m_VirtualTextureSize.cy,
+                       rcTemp.right  * szTarget.cx / m_VirtualTextureSize.cx,
+                       rcTemp.bottom * szTarget.cy / m_VirtualTextureSize.cy);
+        rcTemp.OffsetRect(rcTarget.TopLeft());
+
+        *pRcDest = rcTemp;
 
         return S_OK;
     } else {
@@ -189,6 +196,18 @@ STDMETHODIMP_(bool) CSubPicImpl::GetInverseAlpha()
 STDMETHODIMP_(void) CSubPicImpl::SetInverseAlpha(bool bInverted)
 {
     m_invAlpha = bInverted;
+}
+
+STDMETHODIMP CSubPicImpl::GetRelativeTo(RelativeTo* pRelativeTo)
+{
+    return pRelativeTo ? *pRelativeTo = m_relativeTo, S_OK : E_POINTER;
+}
+
+STDMETHODIMP CSubPicImpl::SetRelativeTo(RelativeTo relativeTo)
+{
+    m_relativeTo = relativeTo;
+
+    return S_OK;
 }
 
 //

src/SubPic/SubPicImpl.h
@@ -35,6 +35,7 @@ protected:
     CSize  m_VirtualTextureSize;
     CPoint m_VirtualTextureTopLeft;
     bool   m_invAlpha;
+    RelativeTo m_relativeTo;
 
     /*
 
@@ -95,13 +96,14 @@ public:
     STDMETHODIMP AlphaBlt(RECT* pSrc, RECT* pDst, SubPicDesc* pTarget) PURE;
 
     STDMETHODIMP SetVirtualTextureSize(const SIZE pSize, const POINT pTopLeft);
-    STDMETHODIMP GetSourceAndDest(SIZE* pSize, RECT* pRcSource, RECT* pRcDest);
+    STDMETHODIMP GetSourceAndDest(RECT rcWindow, RECT rcVideo, RECT* pRcSource, RECT* pRcDest);
+    STDMETHODIMP GetRelativeTo(RelativeTo* pRelativeTo);
+    STDMETHODIMP SetRelativeTo(RelativeTo relativeTo);
 
     STDMETHODIMP_(REFERENCE_TIME) GetSegmentStart();
     STDMETHODIMP_(REFERENCE_TIME) GetSegmentStop();
     STDMETHODIMP_(void) SetSegmentStart(REFERENCE_TIME rtStart);
     STDMETHODIMP_(void) SetSegmentStop(REFERENCE_TIME rtStop);
-
     STDMETHODIMP_(bool) GetInverseAlpha();
     STDMETHODIMP_(void) SetInverseAlpha(bool bInverted);
 };

src/SubPic/SubPicProviderImpl.h
@@ -48,4 +48,5 @@ public:
 
     STDMETHODIMP Render(SubPicDesc& spd, REFERENCE_TIME rt, double fps, RECT& bbox) PURE;
     STDMETHODIMP GetTextureSize(POSITION pos, SIZE& MaxTextureSize, SIZE& VirtualSize, POINT& VirtualTopLeft) { return E_NOTIMPL; };
+    STDMETHODIMP GetRelativeTo(POSITION pos, RelativeTo& relativeTo) { relativeTo = WINDOW; return S_OK; };
 };

src/SubPic/SubPicQueueImpl.cpp
@@ -514,6 +514,11 @@ DWORD CSubPicQueue::ThreadProc()
                             pDynamic->SetVirtualTextureSize(VirtualSize, VirtualTopLeft);
                         }
 
+                        RelativeTo relativeTo;
+                        if (SUCCEEDED(pSubPicProvider->GetRelativeTo(pos, relativeTo))) {
+                            pDynamic->SetRelativeTo(relativeTo);
+                        }
+
                         AppendQueue(pDynamic);
                         bAgain = true;
 
@@ -653,6 +658,11 @@ STDMETHODIMP_(bool) CSubPicQueueNoThread::LookupSubPic(REFERENCE_TIME rtNow, CCo
                     }
                     if (SUCCEEDED(hr2)) {
                         pSubPic->SetVirtualTextureSize(VirtualSize, VirtualTopLeft);
+                    }
+
+                    RelativeTo relativeTo;
+                    if (SUCCEEDED(pSubPicProvider->GetRelativeTo(pos, relativeTo))) {
+                        pSubPic->SetRelativeTo(relativeTo);
                     }
                 }
             }

commit 2d06f4e712b518f4b6f957848a849257d9e27cf8

VMR-7 renderless: subtitles were not displayed except in fullscreen.
This issue was introduced in aa4a1acb57fc28fd464942d48b5f9987d88df82f.

src/filters/renderer/VideoRenderers/DX7AllocatorPresenter.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2013 see Authors.txt
+ * (C) 2006-2014 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -386,7 +386,6 @@ STDMETHODIMP_(bool) CDX7AllocatorPresenter::Paint(bool fAll)
 
     CRect rSrcPri(CPoint(0, 0), m_WindowRect.Size());
     CRect rDstPri(m_WindowRect);
-    MapWindowRect(m_hWnd, HWND_DESKTOP, &rDstPri);
 
     if (fAll) {
         // clear the backbuffer
@@ -424,6 +423,7 @@ STDMETHODIMP_(bool) CDX7AllocatorPresenter::Paint(bool fAll)
 
     // blt to the primary surface
 
+    MapWindowRect(m_hWnd, HWND_DESKTOP, &rDstPri);
     hr = m_pPrimary->Blt(rDstPri, m_pBackBuffer, rSrcPri, DDBLT_WAIT, nullptr);
 
     if (hr == DDERR_SURFACELOST) {