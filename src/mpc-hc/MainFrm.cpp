mpc-hc/mpc-hc

commit 27c1b6218b396b0718dab00ee9296487bd7acdc4

Show tooltip with H/W decoder name in status bar.
Fixes #3873

src/mpc-hc/MainFrm.cpp
@@ -3006,7 +3006,7 @@ void CMainFrame::OnMenuFilters()
 void CMainFrame::OnUpdatePlayerStatus(CCmdUI* pCmdUI)
 {
     if (m_eMediaLoadState == MLS::LOADING) {
-        pCmdUI->SetText(ResStr(IDS_CONTROLS_OPENING));
+        m_wndStatusBar.SetStatusMessage(ResStr(IDS_CONTROLS_OPENING));
         if (AfxGetAppSettings().fUseWin7TaskBar && m_pTaskbarList) {
             m_pTaskbarList->SetProgressState(m_hWnd, TBPF_INDETERMINATE);
         }
@@ -3115,16 +3115,16 @@ void CMainFrame::OnUpdatePlayerStatus(CCmdUI* pCmdUI)
             fs == State_Running ? ResStr(IDS_CONTROLS_PLAYING) :
             _T("");
         if (m_bUsingDXVA && (UI_Text == ResStr(IDS_CONTROLS_PAUSED) || UI_Text == ResStr(IDS_CONTROLS_PLAYING))) {
-            UI_Text += _T(" [DXVA]");
+            UI_Text.AppendFormat(_T(" %s"), ResStr(IDS_HW_INDICATOR));
         }
-        pCmdUI->SetText(UI_Text);
+        m_wndStatusBar.SetStatusMessage(UI_Text);
     } else if (m_eMediaLoadState == MLS::CLOSING) {
-        pCmdUI->SetText(ResStr(IDS_CONTROLS_CLOSING));
+        m_wndStatusBar.SetStatusMessage(ResStr(IDS_CONTROLS_CLOSING));
         if (AfxGetAppSettings().fUseWin7TaskBar && m_pTaskbarList) {
             m_pTaskbarList->SetProgressState(m_hWnd, TBPF_INDETERMINATE);
         }
     } else {
-        pCmdUI->SetText(m_closingmsg);
+        m_wndStatusBar.SetStatusMessage(m_closingmsg);
     }
 }
 
@@ -14041,13 +14041,6 @@ void CMainFrame::StopWebServer()
     }
 }
 
-CString CMainFrame::GetStatusMessage() const
-{
-    CString str;
-    m_wndStatusBar.m_status.GetWindowText(str);
-    return str;
-}
-
 void CMainFrame::SendStatusMessage(CString msg, int nTimeOut)
 {
     KillTimer(TIMER_STATUSERASER);
@@ -16113,16 +16106,18 @@ void CMainFrame::UpdateDXVAStatus()
 {
     CString DXVADecoderDescription = GetDXVADecoderDescription();
     m_bUsingDXVA = (_T("Not using DXVA") != DXVADecoderDescription && _T("Unknown") != DXVADecoderDescription);
+    m_HWAccelType = GetDXVAVersion();
 
     CString DXVAInfo;
-    DXVAInfo.Format(_T("%-13s: %s"), GetDXVAVersion(), GetDXVADecoderDescription());
+    DXVAInfo.Format(_T("%-13s: %s"), m_HWAccelType, DXVADecoderDescription);
 
     // If LAV Video is in the graph, we query it since it's always more reliable than the hook.
     if (CComQIPtr<ILAVVideoStatus> pLAVVideoStatus = FindFilter(GUID_LAVVideo, m_pGB)) {
         CStringW decoderName = pLAVVideoStatus->GetActiveDecoderName();
         if (decoderName.Find(L"dxva") == 0 || decoderName == L"cuvid" || decoderName == L"quicksync") {
+            m_HWAccelType = CFGFilterLAVVideo::GetUserFriendlyDecoderName(decoderName);
             CString LAVDXVAInfo;
-            LAVDXVAInfo.Format(_T("LAV Video Decoder (%s)"), CFGFilterLAVVideo::GetUserFriendlyDecoderName(decoderName));
+            LAVDXVAInfo.Format(_T("LAV Video Decoder (%s)"), m_HWAccelType);
 
             if (!m_bUsingDXVA) { // Don't trust the hook
                 m_bUsingDXVA = true;
@@ -16134,4 +16129,13 @@ void CMainFrame::UpdateDXVAStatus()
     }
     GetRenderersData()->m_strDXVAInfo = DXVAInfo;
     return;
+}
+
+bool CMainFrame::GetDecoderType(CString& type) const
+{
+    if (!m_fAudioOnly) {
+        type = m_bUsingDXVA ? m_HWAccelType : ResStr(IDS_TOOLTIP_SOFTWARE_DECODING);
+        return true;
+    }
+    return false;
 }

src/mpc-hc/PlayerStatusBar.cpp
@@ -52,12 +52,12 @@ BOOL CPlayerStatusBar::Create(CWnd* pParentWnd)
     BOOL ret = CDialogBar::Create(pParentWnd, IDD_PLAYERSTATUSBAR, WS_CHILD | WS_VISIBLE | CBRS_ALIGN_BOTTOM, IDD_PLAYERSTATUSBAR);
 
     m_tooltip.Create(this, TTS_NOPREFIX | TTS_ALWAYSTIP);
-
     m_tooltip.SetDelayTime(TTDT_INITIAL, 0);
     m_tooltip.SetDelayTime(TTDT_AUTOPOP, 2500);
     m_tooltip.SetDelayTime(TTDT_RESHOW, 0);
-
     m_tooltip.AddTool(&m_time, IDS_TOOLTIP_REMAINING_TIME);
+    m_tooltip.AddTool(&m_status);
+
 
     return ret;
 }
@@ -86,7 +86,7 @@ int CPlayerStatusBar::OnCreate(LPCREATESTRUCT lpCreateStruct)
     m_type.Create(_T(""), WS_CHILD | WS_VISIBLE | SS_ICON | SS_CENTERIMAGE,
                   r, this, IDC_STATIC1);
 
-    m_status.Create(_T(""), WS_CHILD | WS_VISIBLE | SS_OWNERDRAW,
+    m_status.Create(_T(""), WS_CHILD | WS_VISIBLE | SS_OWNERDRAW | SS_NOTIFY,
                     r, this, IDC_PLAYERSTATUS);
 
     m_time.Create(_T(""), WS_CHILD | WS_VISIBLE | SS_OWNERDRAW | SS_NOTIFY,
@@ -101,39 +101,47 @@ int CPlayerStatusBar::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 void CPlayerStatusBar::Relayout()
 {
-    BITMAP bm;
-    ZeroMemory(&bm, sizeof(bm));
+    BITMAP bm {};
     if (m_bm.m_hObject) {
         m_bm.GetBitmap(&bm);
     }
 
+    CString str;
     CRect r, r2;
+
     GetClientRect(r);
 
-    r.DeflateRect(11 + m_pMainFrame->m_dpi.ScaleX(16), 5, bm.bmWidth + 8, 4);
-    int div = r.right - (m_time.IsWindowVisible() ? m_pMainFrame->m_dpi.ScaleX(140) : 0);
-
-    CString str;
-    m_time.GetWindowText(str);
-    if (CDC* pDC = m_time.GetDC()) {
-        CFont* pOld = pDC->SelectObject(&m_time.GetFont());
-        div = r.right - pDC->GetTextExtent(str).cx;
-        pDC->SelectObject(pOld);
-        m_time.ReleaseDC(pDC);
+    if (m_type.GetIcon()) {
+        r2.SetRect(6, r.top + 4, 6 + m_pMainFrame->m_dpi.ScaleX(16), r.bottom - 4);
+        m_type.MoveWindow(r2);
     }
 
-    r2 = r;
-    r2.right = div - 2;
-    m_status.MoveWindow(&r2);
+    r.DeflateRect(11 + m_pMainFrame->m_dpi.ScaleX(16), 5, bm.bmWidth + 8, 4);
 
-    r2 = r;
-    r2.left = div;
-    m_time.MoveWindow(&r2);
-    m_time_rect = r2;
+    if (CDC* pDC = m_time.GetDC()) {
+        CFont* pOld = pDC->SelectObject(&m_time.GetFont());
+        m_time.GetWindowText(str);
+        r2 = r;
+        r2.left = r2.right - pDC->GetTextExtent(str).cx;
+        m_time.MoveWindow(&r2);
+        m_time_rect = r2;
+        pDC->SelectObject(pOld);
+        m_time.ReleaseDC(pDC);
+    } else {
+        ASSERT(FALSE);
+    }
 
-    GetClientRect(r);
-    r.SetRect(6, r.top + 4, 6 + m_pMainFrame->m_dpi.ScaleX(16), r.bottom - 4);
-    m_type.MoveWindow(r);
+    if (CDC* pDC = m_status.GetDC()) {
+        CFont* pOld = pDC->SelectObject(&m_status.GetFont());
+        m_status.GetWindowText(str);
+        r2 = r;
+        r2.right = r2.left + pDC->GetTextExtent(str).cx;
+        m_status.MoveWindow(&r2);
+        pDC->SelectObject(pOld);
+        m_status.ReleaseDC(pDC);
+    } else {
+        ASSERT(FALSE);
+    }
 
     Invalidate();
 }
@@ -182,13 +190,26 @@ void CPlayerStatusBar::SetStatusTypeIcon(HICON hIcon)
     Relayout();
 }
 
-void CPlayerStatusBar::SetStatusMessage(CString str)
+CString CPlayerStatusBar::GetStatusMessage() const
 {
-    str.Trim();
-    m_status.SetWindowText(str);
+    CString strResult;
+
+    m_status.GetWindowText(strResult);
+
+    return strResult;
 }
 
-CString CPlayerStatusBar::GetStatusTimer()
+void CPlayerStatusBar::SetStatusMessage(CString str)
+{
+    if (GetStatusMessage() != str) {
+        str.Trim();
+        m_status.SetWindowText(str);
+
+        Relayout();
+    }
+}
+
+CString CPlayerStatusBar::GetStatusTimer() const
 {
     CString strResult;
 
@@ -199,16 +220,12 @@ CString CPlayerStatusBar::GetStatusTimer()
 
 void CPlayerStatusBar::SetStatusTimer(CString str)
 {
-    CString tmp;
-    m_time.GetWindowText(tmp);
-    if (tmp == str) {
-        return;
+    if (GetStatusTimer() != str) {
+        str.Trim();
+        m_time.SetWindowText(str);
+
+        Relayout();
     }
-
-    str.Trim();
-    m_time.SetWindowText(str);
-
-    Relayout();
 }
 
 void CPlayerStatusBar::SetStatusTimer(REFERENCE_TIME rtNow, REFERENCE_TIME rtDur, bool fHighPrecision, const GUID& timeFormat/* = TIME_FORMAT_MEDIA_TIME*/)
@@ -278,6 +295,7 @@ BEGIN_MESSAGE_MAP(CPlayerStatusBar, CDialogBar)
     ON_WM_LBUTTONDOWN()
     ON_WM_SETCURSOR()
     ON_WM_CTLCOLOR()
+    ON_NOTIFY_EX(TTN_NEEDTEXT, 0, OnToolTipNotify)
 END_MESSAGE_MAP()
 
 
@@ -434,4 +452,23 @@ void CPlayerStatusBar::OnTimeDisplayClicked()
     s.fRemainingTime = !s.fRemainingTime;
     // This isn't particularly nice...
     pFrame->OnTimer(2);
+}
+
+BOOL CPlayerStatusBar::OnToolTipNotify(UINT id, NMHDR* pNMHDR, LRESULT* pResult)
+{
+    TOOLTIPTEXT* pTTT = reinterpret_cast<LPTOOLTIPTEXT>(pNMHDR);
+    if (pTTT->uFlags & TTF_IDISHWND) {
+        UINT_PTR nID = pNMHDR->idFrom;
+        if (::GetDlgCtrlID((HWND)nID) == IDC_PLAYERSTATUS) {
+            CString type;
+            const CString msg = GetStatusMessage();
+            if (m_pMainFrame->GetDecoderType(type)
+                    && (msg.Find(ResStr(IDS_CONTROLS_PAUSED)) == 0 || msg.Find(ResStr(IDS_CONTROLS_PLAYING)) == 0)) {
+                _tcscpy_s(pTTT->szText, type);
+                *pResult = 0;
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
 }

src/mpc-hc/PlayerStatusBar.h
@@ -58,7 +58,8 @@ public:
     void SetStatusTimer(REFERENCE_TIME rtNow, REFERENCE_TIME rtDur, bool fHighPrecision,
                         const GUID& timeFormat = TIME_FORMAT_MEDIA_TIME);
 
-    CString GetStatusTimer();
+    CString GetStatusTimer() const;
+    CString GetStatusMessage() const;
     void ShowTimer(bool fShow);
 
     // Overrides
@@ -77,4 +78,5 @@ protected:
     afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
     afx_msg BOOL PreTranslateMessage(MSG* pMsg);
     afx_msg void OnTimeDisplayClicked();
+    afx_msg BOOL OnToolTipNotify(UINT id, NMHDR* pNMHDR, LRESULT* pResult);
 };

commit 5904a2c419179eb3c6686f9a4023ae846b19d611

Fix: The "Save image/thumbnails" path was cut off in the status bar.
This issue was introduced in 27c1b6218b396b0718dab00ee9296487bd7acdc4.

Also reduce the visibility of the status bar member variables.

This commit fixes #4079.

src/mpc-hc/MainFrm.cpp
@@ -4522,14 +4522,7 @@ void CMainFrame::SaveDIB(LPCTSTR fn, BYTE* pData, long size)
 
     path.m_strPath.Replace(_T("\\\\"), _T("\\"));
 
-    if (CDC* pDC = m_wndStatusBar.m_status.GetDC()) {
-        CRect r;
-        m_wndStatusBar.m_status.GetClientRect(r);
-        path.CompactPath(pDC->m_hDC, r.Width());
-        m_wndStatusBar.m_status.ReleaseDC(pDC);
-    }
-
-    SendStatusMessage((LPCTSTR)path, 3000);
+    SendStatusMessage(m_wndStatusBar.PreparePathStatusMessage(path), 3000);
 }
 
 void CMainFrame::SaveImage(LPCTSTR fn)

src/mpc-hc/PlayerStatusBar.cpp
@@ -216,6 +216,20 @@ void CPlayerStatusBar::SetStatusMessage(CString str)
     }
 }
 
+CString CPlayerStatusBar::PreparePathStatusMessage(CPath path)
+{
+    if (CDC* pDC = m_status.GetDC()) {
+        CRect r;
+        m_status.GetClientRect(r);
+        path.CompactPath(pDC->m_hDC, m_time_rect.left - r.left - 1);
+        m_status.ReleaseDC(pDC);
+    } else {
+        ASSERT(FALSE);
+    }
+
+    return path;
+}
+
 CString CPlayerStatusBar::GetStatusTimer() const
 {
     CString strResult;

src/mpc-hc/PlayerStatusBar.h
@@ -31,6 +31,7 @@ class CPlayerStatusBar : public CDialogBar
 {
     DECLARE_DYNAMIC(CPlayerStatusBar)
 
+private:
     CMainFrame* m_pMainFrame;
 
     CStatic m_type;
@@ -60,6 +61,9 @@ public:
 
     CString GetStatusTimer() const;
     CString GetStatusMessage() const;
+
+    CString PreparePathStatusMessage(CPath path);
+
     void ShowTimer(bool fShow);
 
     // Overrides