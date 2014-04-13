mpc-hc/mpc-hc

commit 5b803b86b2b7fe138f76e65712664e466870ce97

EVR-CP: Repaint only when necessary when paused.
Additionally this seems to fix backward frame-stepping.

This commit fixes #4036.

src/filters/renderer/VideoRenderers/EVRAllocatorPresenter.cpp
@@ -2001,7 +2001,7 @@ STDMETHODIMP_(bool) CEVRAllocatorPresenter::DisplayChange()
 void CEVRAllocatorPresenter::RenderThread()
 {
     HANDLE   hEvts[] = { m_hEvtQuit, m_hEvtFlush};
-    bool     bQuit = false;
+    bool     bQuit = false, bForcePaint = false;
     TIMECAPS tc;
     DWORD    dwResolution;
     MFTIME   nsSampleTime;
@@ -2053,6 +2053,7 @@ void CEVRAllocatorPresenter::RenderThread()
                 FlushSamples();
                 m_bEvtFlush = false;
                 ResetEvent(m_hEvtFlush);
+                bForcePaint = true;
                 TRACE_EVR("EVR: Flush done!\n");
                 break;
 
@@ -2326,11 +2327,16 @@ void CEVRAllocatorPresenter::RenderThread()
                                 }
                             }
                         } else if (m_nRenderState == Paused) {
-                            // Ensure that the renderer is properly updated when paused
-                            if (!g_bExternalSubtitleTime) {
-                                __super::SetTime(g_tSegmentStart + nsSampleTime);
+                            if (bForcePaint) {
+                                bForcePaint = false;
+                                bStepForward = true;
+                                // Ensure that the renderer is properly updated after seeking when paused
+                                if (!g_bExternalSubtitleTime) {
+                                    __super::SetTime(g_tSegmentStart + nsSampleTime);
+                                }
+                                Paint(false);
                             }
-                            Paint(false);
+                            NextSleepTime = int(SampleDuration / 10000 - 2);
                         }
 
                         m_pCurrentDisplaydSample = nullptr;

commit 48adf221f2fb12542eb8cde9e1a70e4f49b33c91

EVR-CP: Fix a jump few frames ahead when pausing the video after seeking.
The issue was introduced in 5b803b86b2b7fe138f76e65712664e466870ce97.

A forced repaint was sometimes triggered because of a flush but the renderer was unpaused before the redraw happened and it was delayed until next time the renderer was paused.

This commit fixes #4182.

src/filters/renderer/VideoRenderers/EVRAllocatorPresenter.cpp
@@ -2328,7 +2328,6 @@ void CEVRAllocatorPresenter::RenderThread()
                             }
                         } else if (m_nRenderState == Paused) {
                             if (bForcePaint) {
-                                bForcePaint = false;
                                 bStepForward = true;
                                 // Ensure that the renderer is properly updated after seeking when paused
                                 if (!g_bExternalSubtitleTime) {
@@ -2347,6 +2346,8 @@ void CEVRAllocatorPresenter::RenderThread()
                         } else {
                             MoveToScheduledList(pMFSample, true);
                         }
+
+                        bForcePaint = false;
                     } else if (m_bLastSampleOffsetValid && m_LastSampleOffset < -10000000) { // Only starve if we are 1 seconds behind
                         if (m_nRenderState == Started && !g_bNoDuration) {
                             m_pSink->Notify(EC_STARVATION, 0, 0);