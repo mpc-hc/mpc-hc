mpc-hc/mpc-hc

commit 8ae02c6203ea3296406b88910bfdefbf80a8614e

Use internal filters for gif format.
Microsoft's Generate Still Video filter has problems with animation rate and store every
frame (uncompressed) in memory, which for gifs with longer animation can be a problem.

Fixes #4678, #4856

src/mpc-hc/PPageInternalFilters.cpp
@@ -362,6 +362,11 @@ void CPPageInternalFilters::InitFiltersList()
         m_filters.Add(filter_t(_T("FLV"), SOURCE_FILTER, SRC_FLV, IDS_INTERNAL_LAVF));
     }
 #endif
+#if INTERNAL_SOURCEFILTER_GIF
+    if (bLAVSplitterIsAvailable) {
+        m_filters.Add(filter_t(_T("GIF"), SOURCE_FILTER, SRC_RTMP, IDS_INTERNAL_LAVF));
+    }
+#endif
 #if INTERNAL_SOURCEFILTER_MATROSKA
     if (bLAVSplitterIsAvailable) {
         m_filters.Add(filter_t(_T("Matroska"), SOURCE_FILTER, SRC_MATROSKA, IDS_INTERNAL_LAVF));

src/mpc-hc/AppSettings.cpp
@@ -262,6 +262,9 @@ CAppSettings::CAppSettings()
 #if INTERNAL_SOURCEFILTER_FLV
     SrcFiltersKeys[SRC_FLV] = FilterKey(_T("SRC_FLV"), true);
 #endif
+#if INTERNAL_SOURCEFILTER_GIF
+    SrcFiltersKeys[SRC_GIF] = FilterKey(_T("SRC_GIF"), true);
+#endif
 #if INTERNAL_SOURCEFILTER_ASF
     SrcFiltersKeys[SRC_ASF] = FilterKey(_T("SRC_ASF"), false);
 #endif

src/mpc-hc/FGManager.cpp
@@ -1455,6 +1455,13 @@ CFGManagerCustom::CFGManagerCustom(LPCTSTR pName, LPUNKNOWN pUnk)
     }
 #endif
 
+#if INTERNAL_SOURCEFILTER_GIF
+    if (src[SRC_GIF]) {
+        pFGLAVSplitterSource->m_extensions.AddTail(_T(".gif"));
+        pFGLAVSplitterSource->AddEnabledFormat("gif");
+    }
+#endif
+
 #if INTERNAL_SOURCEFILTER_ASF
     if (src[SRC_ASF]) {
         pFGLAVSplitterSource->m_extensions.AddTail(_T(".wmv"));

src/mpc-hc/FilterEnum.h
@@ -95,6 +95,9 @@ enum SOURCE_FILTER {
 #if INTERNAL_SOURCEFILTER_FLV
     SRC_FLV,
 #endif
+#if INTERNAL_SOURCEFILTER_GIF
+    SRC_GIF,
+#endif
 #if INTERNAL_SOURCEFILTER_ASF
     SRC_ASF,
 #endif

src/mpc-hc/InternalFiltersConfig.h
@@ -42,6 +42,7 @@
 #define INTERNAL_SOURCEFILTER_FLIC      INTERNAL_FILTERS_ENABLED
 #define INTERNAL_SOURCEFILTER_FLAC      INTERNAL_FILTERS_ENABLED
 #define INTERNAL_SOURCEFILTER_FLV       INTERNAL_FILTERS_ENABLED
+#define INTERNAL_SOURCEFILTER_GIF       INTERNAL_FILTERS_ENABLED
 #define INTERNAL_SOURCEFILTER_ASF       INTERNAL_FILTERS_ENABLED
 #define INTERNAL_SOURCEFILTER_MATROSKA  INTERNAL_FILTERS_ENABLED
 #define INTERNAL_SOURCEFILTER_MP4       INTERNAL_FILTERS_ENABLED
@@ -108,7 +109,7 @@
     INTERNAL_SOURCEFILTER_MATROSKA || INTERNAL_SOURCEFILTER_MP4 || INTERNAL_SOURCEFILTER_OGG || INTERNAL_SOURCEFILTER_MPEGAUDIO || INTERNAL_SOURCEFILTER_MPEG || \
     INTERNAL_SOURCEFILTER_REALMEDIA || INTERNAL_SOURCEFILTER_HTTP || INTERNAL_SOURCEFILTER_UDP ||INTERNAL_SOURCEFILTER_RTP || INTERNAL_SOURCEFILTER_RTSP ||      \
     INTERNAL_SOURCEFILTER_AVI2AC3 || INTERNAL_SOURCEFILTER_RFS || INTERNAL_SOURCEFILTER_RTSP || INTERNAL_SOURCEFILTER_MMS || INTERNAL_SOURCEFILTER_RTMP ||       \
-    INTERNAL_SOURCEFILTER_AVS)
+    INTERNAL_SOURCEFILTER_AVS || INTERNAL_SOURCEFILTER_GIF)
 
 #define HAS_AUDIO_DECODERS \
     (INTERNAL_DECODER_LPCM || INTERNAL_DECODER_PS2AUDIO || INTERNAL_DECODER_REALAUDIO || INTERNAL_DECODER_PCM || INTERNAL_DECODER_AC3 || \

commit 6076f796729dd9a95bbac6fa318de8c5032e3baf

Fix typo introduced in 8ae02c6203ea3296406b88910bfdefbf80a8614e.

src/mpc-hc/PPageInternalFilters.cpp
@@ -364,7 +364,7 @@ void CPPageInternalFilters::InitFiltersList()
 #endif
 #if INTERNAL_SOURCEFILTER_GIF
     if (bLAVSplitterIsAvailable) {
-        m_filters.Add(filter_t(_T("GIF"), SOURCE_FILTER, SRC_RTMP, IDS_INTERNAL_LAVF));
+        m_filters.Add(filter_t(_T("GIF"), SOURCE_FILTER, SRC_GIF, IDS_INTERNAL_LAVF));
     }
 #endif
 #if INTERNAL_SOURCEFILTER_MATROSKA