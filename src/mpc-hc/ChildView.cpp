mpc-hc/mpc-hc

commit 82f8656202b16e867be41f265412dc9e2c6fc93b

Update taskbar status only where it makes sense

src/mpc-hc/MainFrm.cpp
@@ -2242,8 +2242,6 @@ LRESULT CMainFrame::OnGraphNotify(WPARAM wParam, LPARAM lParam)
     CAppSettings& s = AfxGetAppSettings();
     HRESULT hr = S_OK;
 
-    UpdateThumbarButton();
-
     LONG evCode = 0;
     LONG_PTR evParam1, evParam2;
     while (m_pME && SUCCEEDED(m_pME->GetEvent(&evCode, &evParam1, &evParam2, 0))) {
@@ -9590,8 +9588,6 @@ void CMainFrame::MoveVideoWindow(bool fShowStats/* = false*/, bool bSetStoppedVi
     } else {
         m_wndView.SetVideoRect();
     }
-
-    UpdateThumbarButton();
 }
 
 void CMainFrame::HideVideoWindow(bool fHide)
@@ -14444,6 +14440,8 @@ void CMainFrame::SetPlayState(MPC_PLAYSTATE iState)
     } else {
         SetThreadExecutionState(iState == PS_PLAY ? ES_CONTINUOUS | ES_SYSTEM_REQUIRED : ES_CONTINUOUS);
     }
+
+    UpdateThumbarButton(iState);
 }
 
 bool CMainFrame::CreateFullScreenWindow()
@@ -15448,6 +15446,22 @@ HRESULT CMainFrame::CreateThumbnailToolbar()
 
 HRESULT CMainFrame::UpdateThumbarButton()
 {
+    MPC_PLAYSTATE state = PS_STOP;
+    if (GetLoadState() == MLS::LOADED) {
+        switch (GetMediaState()) {
+            case State_Running:
+                state = PS_PLAY;
+                break;
+            case State_Paused:
+                state = PS_PAUSE;
+                break;
+        }
+    }
+    return UpdateThumbarButton(state);
+}
+
+HRESULT CMainFrame::UpdateThumbarButton(MPC_PLAYSTATE iPlayState)
+{
     if (!m_pTaskbarList) {
         return E_FAIL;
     }
@@ -15516,22 +15530,21 @@ HRESULT CMainFrame::UpdateThumbarButton()
 
     if (m_eMediaLoadState == MLS::LOADED) {
         HICON hIcon = nullptr;
-        OAFilterState fs = GetMediaState();
-        if (fs == State_Running) {
+        if (iPlayState == PS_PLAY) {
             buttons[1].dwFlags = THBF_ENABLED;
             buttons[2].dwFlags = THBF_ENABLED;
             buttons[2].iBitmap = 2;
 
             hIcon = (HICON)LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_TB_PLAY), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE);
             m_pTaskbarList->SetProgressState(m_hWnd, m_wndSeekBar.HasDuration() ? TBPF_NORMAL : TBPF_NOPROGRESS);
-        } else if (fs == State_Stopped) {
+        } else if (iPlayState == PS_STOP) {
             buttons[1].dwFlags = THBF_DISABLED;
             buttons[2].dwFlags = THBF_ENABLED;
             buttons[2].iBitmap = 3;
 
             hIcon = (HICON)LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_TB_STOP), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE);
             m_pTaskbarList->SetProgressState(m_hWnd, TBPF_NOPROGRESS);
-        } else if (fs == State_Paused) {
+        } else if (iPlayState == PS_PAUSE) {
             buttons[1].dwFlags = THBF_ENABLED;
             buttons[2].dwFlags = THBF_ENABLED;
             buttons[2].iBitmap = 3;

src/mpc-hc/MainFrm.h
@@ -961,6 +961,7 @@ public:
     ITaskbarList3* m_pTaskbarList;
     HRESULT CreateThumbnailToolbar();
     HRESULT UpdateThumbarButton();
+    HRESULT UpdateThumbarButton(MPC_PLAYSTATE iPlayState);
     HRESULT UpdateThumbnailClip();
 
 protected:

src/mpc-hc/PlayerSeekBar.cpp
@@ -134,19 +134,16 @@ REFERENCE_TIME CPlayerSeekBar::PositionFromClientPoint(const CPoint& point) cons
 
 void CPlayerSeekBar::SyncThumbToVideo(REFERENCE_TIME rtPos)
 {
-    if (m_rtPos == rtPos) {
-        return;
-    }
-
     m_rtPos = rtPos;
-
     if (m_bHasDuration) {
         CRect newThumbRect(GetThumbRect());
+        bool bSetTaskbar = (rtPos <= 0);
         if (newThumbRect != m_lastThumbRect) {
+            bSetTaskbar = true;
             InvalidateRect(newThumbRect | m_lastThumbRect);
-            if (AfxGetAppSettings().fUseWin7TaskBar && m_pMainFrame->m_pTaskbarList) {
-                m_pMainFrame->m_pTaskbarList->SetProgressValue(m_pMainFrame->m_hWnd, m_rtPos, m_rtStop);
-            }
+        }
+        if (bSetTaskbar && AfxGetAppSettings().fUseWin7TaskBar && m_pMainFrame->m_pTaskbarList) {
+            VERIFY(S_OK == m_pMainFrame->m_pTaskbarList->SetProgressValue(m_pMainFrame->m_hWnd, max(m_rtPos, 1), m_rtStop));
         }
     }
 }

commit 4a8c4266745e08eebaa1f4cb6ce02207dfd8f614

Fix taskbar preview scaling.
This commit fixes regression introduced in
82f8656202b16e867be41f265412dc9e2c6fc93b

Fixes #4207

src/mpc-hc/ChildView.cpp
@@ -224,6 +224,7 @@ void CChildView::OnSize(UINT nType, int cx, int cy)
     if (!m_bSwitchingFullscreen) {
         m_pMainFrame->MoveVideoWindow();
     }
+    m_pMainFrame->UpdateThumbnailClip();
 }
 
 LRESULT CChildView::OnNcHitTest(CPoint point)

src/mpc-hc/MainFrm.cpp
@@ -15869,20 +15869,17 @@ HRESULT CMainFrame::UpdateThumbnailClip()
 
     const CAppSettings& s = AfxGetAppSettings();
 
-    if (!s.fUseWin7TaskBar || (GetLoadState() != MLS::LOADED) || m_fAudioOnly || m_fFullScreen) {
+    CRect r;
+    m_wndView.GetClientRect(&r);
+    if (s.eCaptionMenuMode == MODE_SHOWCAPTIONMENU) {
+        r.OffsetRect(0, GetSystemMetrics(SM_CYMENU));
+    }
+
+    if (!s.fUseWin7TaskBar || (GetLoadState() != MLS::LOADED) || m_fFullScreen || IsD3DFullScreenMode() || r.Width() <= 0 || r.Height() <= 0) {
         return m_pTaskbarList->SetThumbnailClip(m_hWnd, nullptr);
     }
 
-    RECT vid_rect, result_rect;
-    m_wndView.GetClientRect(&vid_rect);
-
-    // Remove the menu from thumbnail clip preview if it displayed
-    result_rect.left = 2;
-    result_rect.right = result_rect.left + (vid_rect.right - vid_rect.left) - 4;
-    result_rect.top = (s.eCaptionMenuMode == MODE_SHOWCAPTIONMENU) ? 22 : 2;
-    result_rect.bottom = result_rect.top + (vid_rect.bottom - vid_rect.top) - 4;
-
-    return m_pTaskbarList->SetThumbnailClip(m_hWnd, &result_rect);
+    return m_pTaskbarList->SetThumbnailClip(m_hWnd, &r);
 }
 
 LRESULT CMainFrame::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)