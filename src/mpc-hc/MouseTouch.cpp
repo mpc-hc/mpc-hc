0b40dd82edcf358d4f9c4c46e8b899e00be259de
Kacper Michajłow <kasper93@gmail.com>
2014-10-19 14:15:57 +0200

    Use system metric values for dragging threshold to avoid unintentionally starting a drag operation.
    Fixes #4956

--- src/mpc-hc/MouseTouch.cpp
+++ src/mpc-hc/MouseTouch.cpp
@@ -473,7 +473,8 @@ bool CMouse::TestDrag(const CPoint& screenPoint)
         ASSERT(!IsOnFullscreenWindow());
         bool bUpAssigned = !!AssignedToCmd(wmcmd::LUP, false);
         if ((!bUpAssigned && screenPoint != m_beginDragPoint) ||
-                (bUpAssigned && !PointEqualsImprecise(screenPoint, m_beginDragPoint))) {
+                (bUpAssigned && !PointEqualsImprecise(screenPoint, m_beginDragPoint,
+                        GetSystemMetrics(SM_CXDRAG), GetSystemMetrics(SM_CYDRAG)))) {
             VERIFY(ReleaseCapture());
             m_pMainFrame->PostMessage(WM_NCLBUTTONDOWN, HTCAPTION, MAKELPARAM(m_beginDragPoint.x, m_beginDragPoint.y));
             m_drag = Drag::DRAGGED;
