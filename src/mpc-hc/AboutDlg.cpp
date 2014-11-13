3c447503e74532d9fd8b7166f3ec745b17bc03fe
XhmikosR <xhmikosr@gmail.com>
2014-11-13 09:37:12 +0200

    Add MSVC 2013 Update 4 version number.

--- src/mpc-hc/AboutDlg.cpp
+++ src/mpc-hc/AboutDlg.cpp
@@ -96,7 +96,9 @@ BOOL CAboutDlg::OnInitDialog()
 #endif
 #elif defined(_MSC_VER)
 #if (_MSC_VER == 1800)              // 2013
-#if (_MSC_FULL_VER == 180030723)
+#if (_MSC_FULL_VER == 180031101)
+    m_MPCCompiler = _T("MSVC 2013 Update 4");
+#elif (_MSC_FULL_VER == 180030723)
     m_MPCCompiler = _T("MSVC 2013 Update 3");
 #elif (_MSC_FULL_VER == 180030501)
     m_MPCCompiler = _T("MSVC 2013 Update 2");
