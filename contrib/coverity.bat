503a158404b77703d40f1825d1b15f29090f104f
Kacper Michajłow <kasper93@gmail.com>
2015-01-21 22:56:18 +0100

    Update coverity.bat:
    - Add some checks for curl parameters
    - Redirect curl output to cov_upload.log to enable upload stats view
    - Remove redundant call vcvarsall.bat
    - Update curl upload command
    - Use more aggressive compression LZMA2 using xz.
    - Use SubMsg for messages and improve error checking.

--- contrib/coverity.bat
+++ contrib/coverity.bat
@@ -23,26 +23,15 @@ PUSHD %~dp0
 
 IF EXIST "..\build.user.bat" CALL "..\build.user.bat"
 
-IF NOT DEFINED COV_PATH SET "COV_PATH=H:\progs\thirdparty\cov-analysis-win64"
-IF DEFINED COV_PATH IF NOT EXIST "%COV_PATH%" (
-  ECHO.
-  ECHO ERROR: Coverity not found in "%COV_PATH%"
-  GOTO End
-)
-
-
-CALL "%VS120COMNTOOLS%\vsvars32.bat"
-IF %ERRORLEVEL% NEQ 0 (
-  ECHO vsvars32.bat call failed.
-  GOTO End
-)
+IF NOT EXIST "%COV_PATH%" (CALL :SubMsg "ERROR" "Coverity not found in '%COV_PATH%'" & EXIT /B)
 
 
 :Cleanup
 IF EXIST "cov-int" RD /q /s "cov-int"
-IF EXIST "MPC-HC.lzma" DEL "MPC-HC.lzma"
-IF EXIST "MPC-HC.tar"  DEL "MPC-HC.tar"
-IF EXIST "MPC-HC.tgz"  DEL "MPC-HC.tgz"
+IF EXIST "MPC-HC.tar.xz"  DEL "MPC-HC.tar.xz"
+IF EXIST "MPC-HC.tar"     DEL "MPC-HC.tar"
+IF EXIST "MPC-HC.tgz"     DEL "MPC-HC.tgz"
+IF EXIST "cov_upload.log" DEL "cov_upload.log"
 
 
 :Main
@@ -54,32 +43,56 @@ CALL "..\build.bat" clean Api Both Release silent
 "%COV_PATH%\bin\cov-build.exe" --dir cov-int "..\build.bat" Build Filters Both Release silent
 "%COV_PATH%\bin\cov-build.exe" --dir cov-int "..\build.bat" Build IconLib Both Release silent
 "%COV_PATH%\bin\cov-build.exe" --dir cov-int "..\build.bat" Build Api Both Release silent
+IF %ERRORLEVEL% NEQ 0 (CALL :SubMsg "ERROR" "Build failed." & EXIT /B)
 
 
 :tar
-tar --version 1>&2 2>NUL || (ECHO. & ECHO ERROR: tar not found & GOTO SevenZip)
-tar caf "MPC-HC.lzma" "cov-int"
+CALL :SubDetectTar
+IF NOT EXIST "%TAR%" (CALL :SubMsg "WARNING" "tar not found. Trying 7-zip..." & GOTO SevenZip)
+SET "XZ_OPT=-9e"
+REM You can add -T{N} where {N} stands for count of threads to use. 0 will use all available threads.
+REM Pay attention to memory usage, -9eT4 uses over 5GB of RAM, but -9eT1 uses 700MB in my test.
+REM Lowering compression preset will also decrease memory usage significantly.
+
+%TAR% cJf MPC-HC.tar.xz cov-int
+IF %ERRORLEVEL% NEQ 0 (
+  REM Fallback for 32-bit version of xz.
+  SET "XZ_OPT=-7e"
+  %TAR% cJf MPC-HC.tar.xz cov-int
+)
+
+IF %ERRORLEVEL% NEQ 0 (CALL :SubMsg "WARNING" "tar failed. Trying 7-zip..." & GOTO SevenZip)
 GOTO Upload
 
 
 :SevenZip
 CALL :SubDetectSevenzipPath
-
-rem Coverity is totally bogus with lzma...
-rem And since I cannot replicate the arguments with 7-Zip, just use tar/gzip.
-IF EXIST "%SEVENZIP%" (
-  "%SEVENZIP%" a -ttar "MPC-HC.tar" "cov-int"
-  "%SEVENZIP%" a -tgzip "MPC-HC.tgz" "MPC-HC.tar"
-  IF EXIST "MPC-HC.tar" DEL "MPC-HC.tar"
-  GOTO Upload
-)
+IF NOT EXIST "%SEVENZIP%" (CALL :SubMsg "ERROR" "7-zip not found." & EXIT /B)
+REM 7-Zip doesn't support tarball compliant LZMA2 archives, just use tar/gzip.
+"%SEVENZIP%" a -ttar "MPC-HC.tar" "cov-int"
+IF %ERRORLEVEL% NEQ 0 (CALL :SubMsg "ERROR" "7-zip failed." & EXIT /B)
+"%SEVENZIP%" a -tgzip "MPC-HC.tgz" "MPC-HC.tar"
+IF %ERRORLEVEL% NEQ 0 (CALL :SubMsg "ERROR" "7-zip failed." & EXIT /B)
+IF EXIST "MPC-HC.tar" DEL "MPC-HC.tar"
+GOTO Upload
 
 
 :Upload
 CALL "..\build.bat" GetVersion
 CALL :SubDetectCurl
-%CURL% --form project=MPC-HC --form token=%COV_TOKEN% --form email=%COV_EMAIL% --form file=@MPC-HC.lzma --form version=%MPCHC_HASH% http://scan5.coverity.com/cgi-bin/upload.py
+IF NOT EXIST "%CURL%" (CALL :SubMsg "WARNING" "curl not found. Upload aborted." & GOTO End)
+IF NOT DEFINED COV_TOKEN (CALL :SubMsg "WARNING" "COV_TOKEN not defined. Upload aborted." & GOTO End)
+IF NOT DEFINED COV_EMAIL (CALL :SubMsg "WARNING" "COV_EMAIL not defined. Upload aborted." & GOTO End)
+%CURL% --form token=%COV_TOKEN% --form email=%COV_EMAIL% --form file=@MPC-HC.tar.xz --form version=%MPCHC_HASH% https://scan.coverity.com/builds?project=MPC-HC -o cov_upload.log
 GOTO End
+
+
+:End
+POPD
+CALL :SubMsg "INFO" "Done. Press any key to exit..."
+PAUSE >NUL
+ENDLOCAL
+EXIT /B
 
 
 :SubDetectSevenzipPath
@@ -103,9 +116,42 @@ IF EXIST "%CURL_PATH%" (SET "CURL=%CURL_PATH%" & EXIT /B)
 EXIT /B
 
 
-:End
-POPD
-ECHO. & ECHO Press any key to close this window...
-PAUSE >NUL
-ENDLOCAL
+:SubDetectTar
+IF EXIST tar.exe (SET "TAR=tar.exe" & EXIT /B)
+IF EXIST "%TAR_PATH%\tar.exe" (SET "TAR=%TAR_PATH%\tar.exe" & EXIT /B)
+FOR %%G IN (tar.exe) DO (SET "TAR_PATH=%%~$PATH:G")
+IF EXIST "%TAR_PATH%" (SET "TAR=%TAR_PATH%" & EXIT /B)
+EXIT /B
+
+
+:SubMsg
+ECHO. & ECHO ------------------------------
+IF /I "%~1" == "ERROR" (
+  CALL :SubColorText "0C" "[%~1]" "%~2"
+) ELSE IF /I "%~1" == "INFO" (
+  CALL :SubColorText "0A" "[%~1]" "%~2"
+) ELSE IF /I "%~1" == "WARNING" (
+  CALL :SubColorText "0E" "[%~1]" "%~2"
+)
+ECHO ------------------------------ & ECHO.
+IF /I "%~1" == "ERROR" (
+  ECHO Press any key to exit...
+  PAUSE >NUL
+  POPD
+  ENDLOCAL
+  EXIT /B 1
+) ELSE (
+  EXIT /B
+)
+
+
+:SubColorText
+FOR /F "tokens=1,2 delims=#" %%G IN (
+  '"PROMPT #$H#$E# & ECHO ON & FOR %%H IN (1) DO REM"') DO (
+  SET "DEL=%%G")
+<NUL SET /p ".=%DEL%" > "%~2"
+FINDSTR /v /a:%1 /R ".18" "%~2" NUL
+DEL "%~2" > NUL 2>&1
+REM The space in the following ECHO is intentional
+ECHO  %~3
 EXIT /B
