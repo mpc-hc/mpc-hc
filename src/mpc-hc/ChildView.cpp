9f3f36ddf701233a33340b186f06a1e8447316a2
Underground78 <underground78@users.sourceforge.net>
2015-02-10 21:43:18 +0100

    ChildView: Don't try to draw the logo when its size would be 0.
    It did not crash but it caused a failed assert when using debug builds.

--- src/mpc-hc/ChildView.cpp
+++ src/mpc-hc/ChildView.cpp
@@ -224,18 +224,20 @@ BOOL CChildView::OnEraseBkgnd(CDC* pDC)
 
         r = CRect(CPoint(x, y), CSize(std::lround(dImgWidth), std::lround(dImgHeight)));
 
-        if (m_resizedImg.IsNull() || r.Width() != m_resizedImg.GetWidth() || r.Height() != m_resizedImg.GetHeight() || img.GetBPP() != m_resizedImg.GetBPP()) {
-            m_resizedImg.Destroy();
-            m_resizedImg.Create(r.Width(), r.Height(), img.GetBPP());
+        if (!r.IsRectEmpty()) {
+            if (m_resizedImg.IsNull() || r.Width() != m_resizedImg.GetWidth() || r.Height() != m_resizedImg.GetHeight() || img.GetBPP() != m_resizedImg.GetBPP()) {
+                m_resizedImg.Destroy();
+                m_resizedImg.Create(r.Width(), r.Height(), img.GetBPP());
 
-            HDC hDC = m_resizedImg.GetDC();
-            SetStretchBltMode(hDC, STRETCH_HALFTONE);
-            img.StretchBlt(hDC, 0, 0, r.Width(), r.Height(), SRCCOPY);
-            m_resizedImg.ReleaseDC();
+                HDC hDC = m_resizedImg.GetDC();
+                SetStretchBltMode(hDC, STRETCH_HALFTONE);
+                img.StretchBlt(hDC, 0, 0, r.Width(), r.Height(), SRCCOPY);
+                m_resizedImg.ReleaseDC();
+            }
+
+            m_resizedImg.BitBlt(*pDC, r.TopLeft());
+            pDC->ExcludeClipRect(r);
         }
-
-        m_resizedImg.BitBlt(*pDC, r.TopLeft());
-        pDC->ExcludeClipRect(r);
     }
     img.Detach();
 
