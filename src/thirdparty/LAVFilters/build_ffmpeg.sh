858971930f431613eef0ea3ba023a74757a7a1e1
Kacper Michajłow <kasper93@gmail.com>
2015-03-02 13:25:17 +0100

    build_ffmpeg.sh: Use exact number of threads available.
    It isn't faster from my test and there are reports that it is slower with
    one more thread.

--- src/thirdparty/LAVFilters/build_ffmpeg.sh
+++ src/thirdparty/LAVFilters/build_ffmpeg.sh
@@ -101,7 +101,7 @@ configure() {
 
 build() {
   echo Building...
-  make -j$(($NUMBER_OF_PROCESSORS+1)) 2>&1 | tee make.log
+  make -j$(($NUMBER_OF_PROCESSORS)) 2>&1 | tee make.log
   ## Check the return status and the log to detect possible errors
   [ ${PIPESTATUS[0]} -eq 0 ] && ! grep -q -F "rerun configure" make.log
 }
