mpc-hc/mpc-hc

commit ec3fd94af5a6b9a666ab2bc0f7dafb3c2ac2f4ae

Vobsub: Support animated subtitles (with fade in/out).
This commit fixes #1814.

src/Subtitles/VobSubFile.cpp
@@ -348,6 +348,7 @@ bool CVobSubFile::Open(CString fn)
 
                 sp[j].stop = sp[j].start + m_img.delay;
                 sp[j].fForced = m_img.fForced;
+                sp[j].bAnimated = m_img.bAnimated;
 
                 if (j > 0 && sp[j - 1].stop > sp[j].start) {
                     sp[j - 1].stop = sp[j].start;
@@ -1198,7 +1199,7 @@ const CVobSubFile::SubPos* CVobSubFile::GetFrameInfo(int idx, int iLang /*= -1*/
     return &sp[idx];
 }
 
-bool CVobSubFile::GetFrame(int idx, int iLang)
+bool CVobSubFile::GetFrame(int idx, int iLang /*= -1*/, REFERENCE_TIME rt /*= -1*/)
 {
     if (iLang < 0 || iLang >= 32) {
         iLang = m_iLang;
@@ -1209,7 +1210,8 @@ bool CVobSubFile::GetFrame(int idx, int iLang)
         return false;
     }
 
-    if (m_img.iLang != iLang || m_img.iIdx != idx) {
+    if (m_img.iLang != iLang || m_img.iIdx != idx
+            || (sp[idx].bAnimated && sp[idx].start + m_img.tCurrent <= rt)) {
         int packetsize = 0, datasize = 0;
         CAutoVectorPtr<BYTE> buff;
         buff.Attach(GetPacket(idx, packetsize, datasize, iLang));
@@ -1220,7 +1222,8 @@ bool CVobSubFile::GetFrame(int idx, int iLang)
         m_img.start = sp[idx].start;
         m_img.delay = (size_t)idx < (sp.GetCount() - 1) ? sp[idx + 1].start - sp[idx].start : 3000;
 
-        bool ret = m_img.Decode(buff, packetsize, datasize, m_fCustomPal, m_tridx, m_orgpal, m_cuspal, true);
+        bool ret = m_img.Decode(buff, packetsize, datasize, rt >= 0 ? int(rt - sp[idx].start) : INT_MAX,
+                                m_fCustomPal, m_tridx, m_orgpal, m_cuspal, true);
 
         if ((size_t)idx < (sp.GetCount() - 1)) {
             m_img.delay = std::min(m_img.delay, sp[idx + 1].start - m_img.start);
@@ -1297,8 +1300,6 @@ STDMETHODIMP CVobSubFile::NonDelegatingQueryInterface(REFIID riid, void** ppv)
 
 // ISubPicProvider
 
-// TODO: return segments for the fade-in/out time (with animated set to "true" of course)
-
 STDMETHODIMP_(POSITION) CVobSubFile::GetStartPosition(REFERENCE_TIME rt, double fps)
 {
     rt /= 10000;
@@ -1341,7 +1342,9 @@ STDMETHODIMP_(REFERENCE_TIME) CVobSubFile::GetStop(POSITION pos, double fps)
 
 STDMETHODIMP_(bool) CVobSubFile::IsAnimated(POSITION pos)
 {
-    return false;
+    int i = (int)pos - 1;
+    const SubPos* sp = GetFrameInfo(i);
+    return (sp ? sp->bAnimated : false);
 }
 
 STDMETHODIMP CVobSubFile::Render(SubPicDesc& spd, REFERENCE_TIME rt, double fps, RECT& bbox)
@@ -1352,7 +1355,7 @@ STDMETHODIMP CVobSubFile::Render(SubPicDesc& spd, REFERENCE_TIME rt, double fps,
 
     rt /= 10000;
 
-    if (!GetFrame(GetFrameIdxByTimeStamp(rt))) {
+    if (!GetFrame(GetFrameIdxByTimeStamp(rt), -1, rt)) {
         return E_FAIL;
     }
 
@@ -2446,6 +2449,7 @@ void CVobSubStream::Add(REFERENCE_TIME tStart, REFERENCE_TIME tStop, BYTE* pData
     CAutoPtr<SubPic> p(DEBUG_NEW SubPic());
     p->tStart = tStart;
     p->tStop = vsi.delay > 0 ? (tStart + 10000i64 * vsi.delay) : tStop;
+    p->bAnimated = vsi.bAnimated;
     p->pData.SetCount(len);
     memcpy(p->pData.GetData(), pData, p->pData.GetCount());
 
@@ -2525,7 +2529,8 @@ STDMETHODIMP_(REFERENCE_TIME) CVobSubStream::GetStop(POSITION pos, double fps)
 
 STDMETHODIMP_(bool) CVobSubStream::IsAnimated(POSITION pos)
 {
-    return false;
+    CAutoLock cAutoLock(&m_csSubPics);
+    return m_subpics.GetAt(pos)->bAnimated;
 }
 
 STDMETHODIMP CVobSubStream::Render(SubPicDesc& spd, REFERENCE_TIME rt, double fps, RECT& bbox)
@@ -2538,10 +2543,10 @@ STDMETHODIMP CVobSubStream::Render(SubPicDesc& spd, REFERENCE_TIME rt, double fp
     for (; pos; m_subpics.GetPrev(pos)) {
         SubPic* sp = m_subpics.GetAt(pos);
         if (sp->tStart <= rt && rt < sp->tStop) {
-            if (m_img.iIdx != (int)pos) {
+            if (m_img.iIdx != (int)pos || (sp->bAnimated && sp->tStart + m_img.tCurrent * 10000i64 <= rt)) {
                 BYTE* pData = sp->pData.GetData();
                 m_img.Decode(
-                    pData, (pData[0] << 8) | pData[1], (pData[2] << 8) | pData[3],
+                    pData, (pData[0] << 8) | pData[1], (pData[2] << 8) | pData[3], int((rt - sp->tStart) / 10000i64),
                     m_fCustomPal, m_tridx, m_orgpal, m_cuspal, true);
                 m_img.iIdx = (int)pos;
             }

src/Subtitles/VobSubFile.h
@@ -84,7 +84,7 @@ public:
     struct SubPos {
         __int64 filepos;
         __int64 start, stop;
-        bool fForced;
+        bool fForced, bAnimated;
         char vobid, cellid;
         __int64 celltimestamp;
         bool fValid;
@@ -107,7 +107,7 @@ protected:
 
     BYTE* GetPacket(int idx, int& packetsize, int& datasize, int iLang = -1);
     const SubPos* GetFrameInfo(int idx, int iLang = -1) const;
-    bool GetFrame(int idx, int iLang = -1);
+    bool GetFrame(int idx, int iLang = -1, REFERENCE_TIME rt = -1);
     bool GetFrameByTimeStamp(__int64 time);
     int GetFrameIdxByTimeStamp(__int64 time);
 
@@ -170,6 +170,7 @@ class __declspec(uuid("D7FBFB45-2D13-494F-9B3D-FFC9557D5C45"))
     CCritSec m_csSubPics;
     struct SubPic {
         REFERENCE_TIME tStart, tStop;
+        bool bAnimated;
         CAtlArray<BYTE> pData;
     };
     CAutoPtrList<SubPic> m_subpics;

src/Subtitles/VobSubImage.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2013 see Authors.txt
+ * (C) 2006-2014 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -29,6 +29,7 @@ CVobSubImage::CVobSubImage()
     : iLang(-1)
     , iIdx(-1)
     , fForced(false)
+    , bAnimated(false)
     , start(0)
     , delay(0)
     , rect(CRect(0, 0, 0, 0))
@@ -89,13 +90,13 @@ void CVobSubImage::Free()
     lpPixels = nullptr;
 }
 
-bool CVobSubImage::Decode(BYTE* lpData, int packetsize, int datasize,
+bool CVobSubImage::Decode(BYTE* lpData, int packetsize, int datasize, int t,
                           bool fCustomPal,
                           int tridx,
                           RGBQUAD* orgpal /*[16]*/, RGBQUAD* cuspal /*[4]*/,
                           bool fTrim)
 {
-    GetPacketInfo(lpData, packetsize, datasize);
+    GetPacketInfo(lpData, packetsize, datasize, t);
 
     if (!Alloc(rect.Width(), rect.Height())) {
         return false;
@@ -149,17 +150,18 @@ bool CVobSubImage::Decode(BYTE* lpData, int packetsize, int datasize,
     return true;
 }
 
-void CVobSubImage::GetPacketInfo(const BYTE* lpData, int packetsize, int datasize)
+void CVobSubImage::GetPacketInfo(const BYTE* lpData, int packetsize, int datasize, int t /*= INT_MAX*/)
 {
     //  delay = 0;
 
     int i, nextctrlblk = datasize;
     WORD pal = 0, tr = 0;
+    WORD nPal = 0, nTr = 0;
 
     do {
         i = nextctrlblk;
 
-        int t = (lpData[i] << 8) | lpData[i + 1];
+        tCurrent = 1024 * ((lpData[i] << 8) | lpData[i + 1]) / 90;
         i += 2;
         nextctrlblk = (lpData[i] << 8) | lpData[i + 1];
         i += 2;
@@ -167,6 +169,10 @@ void CVobSubImage::GetPacketInfo(const BYTE* lpData, int packetsize, int datasiz
         if (nextctrlblk > packetsize || nextctrlblk < datasize) {
             ASSERT(0);
             return;
+        }
+
+        if (tCurrent > t) {
+            break;
         }
 
         bool fBreak = false;
@@ -214,15 +220,17 @@ void CVobSubImage::GetPacketInfo(const BYTE* lpData, int packetsize, int datasiz
                     fForced = false;
                     break;
                 case 0x02: // stop displaying
-                    delay = 1024 * t / 90;
+                    delay = tCurrent;
                     break;
                 case 0x03:
                     pal = (lpData[i] << 8) | lpData[i + 1];
                     i += 2;
+                    nPal++;
                     break;
                 case 0x04:
                     tr = (lpData[i] << 8) | lpData[i + 1];
                     i += 2;
+                    nTr++;
                     //tr &= 0x00f0;
                     break;
                 case 0x05:
@@ -252,6 +260,8 @@ void CVobSubImage::GetPacketInfo(const BYTE* lpData, int packetsize, int datasiz
         this->pal[i].pal = (pal >> (i << 2)) & 0xf;
         this->pal[i].tr = (tr >> (i << 2)) & 0xf;
     }
+
+    bAnimated = (nPal > 1 || nTr > 1);
 }
 
 BYTE CVobSubImage::GetNibble(const BYTE* lpData)

src/Subtitles/VobSubImage.h
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2013 see Authors.txt
+ * (C) 2006-2014 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -60,7 +60,8 @@ private:
 
 public:
     int iLang, iIdx;
-    bool fForced;
+    bool fForced, bAnimated;
+    int tCurrent;
     __int64 start, delay;
     CRect rect;
     struct SubPal {
@@ -74,8 +75,8 @@ public:
 
     void Invalidate() { iLang = iIdx = -1; }
 
-    void GetPacketInfo(const BYTE* lpData, int packetsize, int datasize);
-    bool Decode(BYTE* lpData, int packetsize, int datasize,
+    void GetPacketInfo(const BYTE* lpData, int packetsize, int datasize, int t = INT_MAX);
+    bool Decode(BYTE* lpData, int packetsize, int datasize, int t,
                 bool fCustomPal,
                 int tridx,
                 RGBQUAD* orgpal /*[16]*/, RGBQUAD* cuspal /*[4]*/,

commit 25d39108b065cb5045172fc433f7b0b9701ee8f5

Vobsub: The latest subtitle entry was sometimes not rendered.
This was broken by ec3fd94af5a6b9a666ab2bc0f7dafb3c2ac2f4ae.

Fixes #5348

src/Subtitles/VobSubFile.cpp
@@ -1227,14 +1227,11 @@ bool CVobSubFile::GetFrame(size_t idx, size_t nLang /*= SIZE_T_ERROR*/, REFERENC
         }
 
         m_img.start = sp[idx].start;
-        m_img.delay = idx + 1 < sp.GetCount() ? sp[idx + 1].start - sp[idx].start : 3000;
 
         bool ret = m_img.Decode(buff, packetSize, dataSize, rt >= 0 ? int(rt - sp[idx].start) : INT_MAX,
                                 m_bCustomPal, m_tridx, m_orgpal, m_cuspal, true);
 
-        if (idx + 1 < sp.GetCount()) {
-            m_img.delay = std::min(m_img.delay, sp[idx + 1].start - m_img.start);
-        }
+        m_img.delay = sp[idx].stop - sp[idx].start;
 
         if (!ret) {
             return false;