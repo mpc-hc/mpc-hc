mpc-hc/mpc-hc

commit c1c4bc50d34c14a5865d24ab1f4ace4c3115a895

Crash reporter: Add a dialog to gather extra info from the user.
The dialog is pre-created in its own thread when starting MPC-HC to avoid allocating memory after a crash as much as possible.

src/mpc-hc/CrashReporterDialog.cpp
@@ -1,0 +1,142 @@
+/*
+ * (C) 2015 see Authors.txt
+ *
+ * This file is part of MPC-HC.
+ *
+ * MPC-HC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-HC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "stdafx.h"
+#include "CrashReporterDialog.h"
+#include "resource.h"
+#include "mplayerc.h"
+#include "DSUtil.h"
+
+CCrashReporterDialog::CCrashReporterDialog()
+    : CDialog()
+{
+    // Listen for language changes
+    EventRouter::EventSelection receives;
+    receives.insert(MpcEvent::CHANGING_UI_LANGUAGE);
+    GetEventd().Connect(m_eventc, receives, std::bind(&CCrashReporterDialog::EventCallback, this, std::placeholders::_1));
+}
+
+BOOL CCrashReporterDialog::Create()
+{
+    // Try to preallocate memory for the result strings
+    m_email.Preallocate(128);
+    m_description.Preallocate(4096);
+
+    bool res = __super::Create(IDD, CWnd::GetDesktopWindow());
+
+    // Because we set LR_SHARED, there is no need to explicitly destroy the icon
+    VERIFY(SetIcon((HICON)LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_MAINFRAME), IMAGE_ICON, 48, 48, LR_SHARED), true) == nullptr);
+
+    return res;
+}
+
+void CCrashReporterDialog::LoadTranslatableResources()
+{
+    // Load the template in a temporary dialog and get the strings from there
+    CDialog tmpDlg;
+    if (tmpDlg.Create(IDD)) {
+        auto setTextFromDlg = [&](int nID) {
+            CString text;
+            if (tmpDlg.GetDlgItemText(nID, text)) {
+                GetDlgItem(nID)->SetWindowText(text);
+            }
+        };
+
+        setTextFromDlg(IDC_STATIC1);
+        setTextFromDlg(IDC_STATIC2);
+        setTextFromDlg(IDC_STATIC3);
+        setTextFromDlg(IDC_STATIC4);
+        setTextFromDlg(IDOK);
+    }
+}
+
+void CCrashReporterDialog::EventCallback(MpcEvent ev)
+{
+    switch (ev) {
+        case MpcEvent::CHANGING_UI_LANGUAGE:
+            LoadTranslatableResources();
+            break;
+        default:
+            ASSERT(FALSE);
+    }
+}
+
+bool CCrashReporterDialog::WaitForUserInput(CString& email, CString& description)
+{
+    m_eventDataAvailable.Wait();
+
+    if (m_bHasData) {
+        email = m_email;
+        description = m_description;
+    }
+
+    return m_bHasData;
+}
+
+void CCrashReporterDialog::OnQuit()
+{
+    if (m_bHasData) {
+        GetDlgItem(IDC_EDIT1)->GetWindowText(m_email);
+        GetDlgItem(IDC_EDIT2)->GetWindowText(m_description);
+    }
+    m_eventDataAvailable.Set();
+    m_eventDataRead.Wait();
+    DestroyWindow();
+}
+
+BEGIN_MESSAGE_MAP(CCrashReporterDialog, CDialog)
+END_MESSAGE_MAP()
+
+IMPLEMENT_DYNCREATE(CCrashReporterUIThread, CWinThread)
+
+CCrashReporterUIThread::CCrashReporterUIThread()
+    : CWinThread()
+    , m_bThreadReady(false)
+    , m_eventThreadReady(TRUE)
+{}
+
+BOOL CCrashReporterUIThread::InitInstance()
+{
+    __super::InitInstance();
+
+    SetThreadName(DWORD(-1), "Crash Reporter UI Thread");
+
+    m_dlg.Create();
+    m_pMainWnd = &m_dlg;
+    return TRUE;
+}
+
+int CCrashReporterUIThread::ExitInstance()
+{
+    m_dlg.DestroyWindow();
+    return __super::ExitInstance();
+}
+
+BOOL CCrashReporterUIThread::OnIdle(LONG lCount)
+{
+    if (!m_bThreadReady) {
+        m_bThreadReady = true;
+        m_eventThreadReady.Set();
+    }
+    return __super::OnIdle(lCount);
+}
+
+BEGIN_MESSAGE_MAP(CCrashReporterUIThread, CWinThread)
+END_MESSAGE_MAP()

src/mpc-hc/AppSettings.cpp
@@ -25,6 +25,7 @@
 #include "FGFilter.h"
 #include "FileAssoc.h"
 #include "CrashReporter.h"
+#include "CrashReporterDialog.h"
 #include "VersionInfo.h"
 #include "SysVersion.h"
 #include "WinAPIUtils.h"
@@ -1206,6 +1207,13 @@ void CAppSettings::LoadSettings()
             language = 0;
         }
     }
+#ifndef DEBUG
+    if (language) {
+        auto pCrashReporterUIThread = CCrashReporterUIThread::GetInstance();
+        pCrashReporterUIThread->WaitThreadReady();
+        pCrashReporterUIThread->GetCrashDialog().LoadTranslatableResources();
+    }
+#endif
 
     CreateCommands();
 

src/mpc-hc/CrashReporter.cpp
@@ -21,6 +21,7 @@
 #include "stdafx.h"
 #include <DbgHelp.h>
 #include "CrashReporter.h"
+#include "CrashReporterDialog.h"
 #include "VersionInfo.h"
 #include "mpc-hc_config.h"
 #include "DoctorDump/CrashRpt.h"
@@ -92,6 +93,8 @@ void CrashReporter::Enable()
 
     if (!g_crashReporter.IsCrashHandlingEnabled()) {
         g_bEnabled = g_crashReporter.InitCrashRpt(&appInfo, &handlerSettings);
+        // Ensure the crash reporter UI thread is running
+        VERIFY(CCrashReporterUIThread::GetInstance() != nullptr);
     } else {
         g_bEnabled = true;
     }
@@ -110,6 +113,34 @@ CrashProcessingCallbackResult CALLBACK CrashReporter::CrashProcessingCallback(Cr
         ExceptionInfo* pExceptionInfo,
         LPVOID pUserData)
 {
-    return g_bEnabled ? DoDefaultActions : SkipSendReportReturnDefaultResult;
+    if (!g_bEnabled) {
+        return SkipSendReportReturnDefaultResult;
+    }
+
+    // All variables are allocated statically to reduce allocations after crashing
+    if (stage == BeforeSendReport) {
+        // We need to make sure the message pump is ready
+        static CCrashReporterUIThread* pCrashReporterUIThread = CCrashReporterUIThread::GetInstance();
+        pCrashReporterUIThread->WaitThreadReady();
+        // before actually showing the dialog
+        static CCrashReporterDialog& crashDlg = pCrashReporterUIThread->GetCrashDialog();
+        crashDlg.ShowWindow(SW_SHOWNORMAL);
+        crashDlg.SetWindowPos(&CWnd::wndTopMost, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+        crashDlg.SetForegroundWindow();
+
+        static CString email, description;
+        if (crashDlg.WaitForUserInput(email, description)) {
+            if (!email.IsEmpty()) {
+                g_crashReporter.AddUserInfoToReport(L"email", email);
+            }
+            if (!description.IsEmpty()) {
+                g_crashReporter.AddUserInfoToReport(L"description", description);
+            }
+        }
+        crashDlg.SignalDataRead();
+        WaitForSingleObject(CCrashReporterUIThread::GetInstance()->m_hThread, INFINITE);
+    }
+
+    return DoDefaultActions;
 };
 #endif

src/mpc-hc/CrashReporterDialog.h
@@ -1,0 +1,87 @@
+/*
+ * (C) 2015 see Authors.txt
+ *
+ * This file is part of MPC-HC.
+ *
+ * MPC-HC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPC-HC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <afxwin.h>
+#include "BaseClasses/wxutil.h"
+#include "resource.h"
+
+class CCrashReporterDialog : public CDialog
+{
+private:
+    CAMEvent m_eventDataAvailable, m_eventDataRead;
+    bool m_bHasData = false;
+    CString m_email, m_description;
+
+    EventClient m_eventc;
+
+public:
+    CCrashReporterDialog();
+
+    BOOL Create();
+    void LoadTranslatableResources();
+
+    bool WaitForUserInput(CString& email, CString& description);
+    void SignalDataRead() { m_eventDataRead.Set(); };
+
+    // Dialog Data
+    enum { IDD = IDD_CRASH_REPORTER };
+
+protected:
+    DECLARE_MESSAGE_MAP()
+
+    virtual void OnOK() { m_bHasData = true; OnQuit(); };
+    virtual void OnCancel() { m_bHasData = false; OnQuit(); };
+
+private:
+    void EventCallback(MpcEvent ev);
+    void OnQuit();
+};
+
+class CCrashReporterUIThread : public CWinThread
+{
+private:
+    CAMEvent m_eventThreadReady;
+    bool m_bThreadReady;
+    CCrashReporterDialog m_dlg;
+
+    DECLARE_DYNCREATE(CCrashReporterUIThread)
+
+protected:
+    CCrashReporterUIThread();
+
+public:
+    static CCrashReporterUIThread* GetInstance() {
+        static CCrashReporterUIThread* pUIThread = (CCrashReporterUIThread*)AfxBeginThread(RUNTIME_CLASS(CCrashReporterUIThread));
+        return pUIThread;
+    };
+
+    virtual BOOL InitInstance();
+    virtual int ExitInstance();
+
+    virtual BOOL OnIdle(LONG lCount);
+
+    void WaitThreadReady() { m_eventThreadReady.Wait(); };
+
+    CCrashReporterDialog& GetCrashDialog() { return m_dlg; };
+
+    DECLARE_MESSAGE_MAP()
+};

src/mpc-hc/mpc-hc.rc
@@ -1104,6 +1104,20 @@ BEGIN
     DEFPUSHBUTTON   "OK",IDOK,252,156,50,14
 END
 
+IDD_CRASH_REPORTER DIALOGEX 0, 0, 300, 240
+STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU
+CAPTION "Crash reporter"
+FONT 8, "MS Shell Dlg", 400, 0, 0x1
+BEGIN
+    LTEXT           "We are sorry, it seems MPC-HC just crashed. :(\n\nTo help us diagnose and fix the problem, a bug report will be sent. You can provide additional information if you want.",IDC_STATIC1,5,5,290,35
+    LTEXT           "Email:",IDC_STATIC2,5,45,290,8
+    EDITTEXT        IDC_EDIT1,5,57,290,13,ES_AUTOHSCROLL
+    LTEXT           "Your email address is optional and will only be used if the developers need to contact you for more information.",IDC_STATIC3,5,72,290,16
+    LTEXT           "Problem description (use English only):",IDC_STATIC4,5,92,290,8
+    EDITTEXT        IDC_EDIT2,5,105,290,112,ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL | ES_WANTRETURN
+    DEFPUSHBUTTON   "OK",IDOK,245,220,50,14
+END
+
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -1277,6 +1291,11 @@ BEGIN
         RIGHTMARGIN, 302
         TOPMARGIN, 7
         BOTTOMMARGIN, 170
+    END
+
+    IDD_CRASH_REPORTER, DIALOG
+    BEGIN
+        BOTTOMMARGIN, 200
     END
 END
 #endif    // APSTUDIO_INVOKED

src/mpc-hc/mpc-hc.vcxproj
@@ -322,6 +322,7 @@
     <ClCompile Include="ComPropertySheet.cpp" />
     <ClCompile Include="CoverArt.cpp" />
     <ClCompile Include="CrashReporter.cpp" />
+    <ClCompile Include="CrashReporterDialog.cpp" />
     <ClCompile Include="CShockwaveFlash.cpp" />
     <ClCompile Include="DebugShadersDlg.cpp" />
     <ClCompile Include="DeinterlacerFilter.cpp" />
@@ -450,6 +451,7 @@
     <ClInclude Include="ComPropertySheet.h" />
     <ClInclude Include="CoverArt.h" />
     <ClInclude Include="CrashReporter.h" />
+    <ClInclude Include="CrashReporterDialog.h" />
     <ClInclude Include="CShockwaveFlash.h" />
     <ClInclude Include="DebugShadersDlg.h" />
     <ClInclude Include="DeinterlacerFilter.h" />

src/mpc-hc/mpc-hc.vcxproj.filters
@@ -83,6 +83,9 @@
     <Filter Include="Main View">
       <UniqueIdentifier>{0453474a-2716-4c52-b5f4-cf6bd9e31ed0}</UniqueIdentifier>
     </Filter>
+    <Filter Include="Crash Reporter">
+      <UniqueIdentifier>{1d73f7db-64e8-4bc8-b0ca-72847253f467}</UniqueIdentifier>
+    </Filter>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="AppSettings.cpp">
@@ -434,7 +437,12 @@
     <ClCompile Include="DropTarget.cpp">
       <Filter>Helpers</Filter>
     </ClCompile>
-    <ClCompile Include="CrashReporter.cpp" />
+    <ClCompile Include="CrashReporter.cpp">
+      <Filter>Crash Reporter</Filter>
+    </ClCompile>
+    <ClCompile Include="CrashReporterDialog.cpp">
+      <Filter>Crash Reporter</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="AppSettings.h">
@@ -814,7 +822,12 @@
     <ClInclude Include="Logger.h">
       <Filter>Helpers</Filter>
     </ClInclude>
-    <ClInclude Include="CrashReporter.h" />
+    <ClInclude Include="CrashReporter.h">
+      <Filter>Crash Reporter</Filter>
+    </ClInclude>
+    <ClInclude Include="CrashReporterDialog.h">
+      <Filter>Crash Reporter</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <None Include="res\ani.avi">

src/mpc-hc/resource.h
@@ -590,6 +590,7 @@
 #define IDD_PPAGEADVANCED               20014
 #define IDD_SAVEIMAGEDIALOGTEMPL        20015
 #define IDD_CMD_LINE_HELP               20016
+#define IDD_CRASH_REPORTER              20017
 #define IDC_FULLSCREEN_MONITOR_CHECK    22002
 #define IDC_SLI_CONTRAST                22003
 #define IDC_RESET                       22004
@@ -1476,7 +1477,7 @@
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        20017
+#define _APS_NEXT_RESOURCE_VALUE        20018
 #define _APS_NEXT_COMMAND_VALUE         33449
 #define _APS_NEXT_CONTROL_VALUE         22081
 #define _APS_NEXT_SYMED_VALUE           24044

commit 64ab5d7e1e539ff16f8d11ef99549d3f0c91c190

Fix: MPC-HC did not get focus at start-up when using a translation.
The problem was introduced by c1c4bc50d34c14a5865d24ab1f4ace4c3115a895.

src/mpc-hc/CrashReporterDialog.cpp
@@ -49,13 +49,18 @@ BOOL CCrashReporterDialog::Create()
 
 void CCrashReporterDialog::LoadTranslatableResources()
 {
-    // Load the template in a temporary dialog and get the strings from there
-    CDialog tmpDlg;
-    if (tmpDlg.Create(IDD)) {
+    // Load the template in a temporary dialog and get the strings from there.
+    // We don't use MFC to avoid side effects caused by creating another dialog.
+    HWND hDlg = ::CreateDialog(AfxGetResourceHandle(), MAKEINTRESOURCE(IDD), nullptr, nullptr);
+    if (hDlg) {
         auto setTextFromDlg = [&](int nID) {
-            CString text;
-            if (tmpDlg.GetDlgItemText(nID, text)) {
-                GetDlgItem(nID)->SetWindowText(text);
+            CWnd* pItem = CWnd::FromHandle(::GetDlgItem(hDlg, nID));
+            if (pItem && IsWindow(pItem->m_hWnd)) {
+                CString text;
+                pItem->GetWindowText(text);
+                if (!text.IsEmpty()) {
+                    GetDlgItem(nID)->SetWindowText(text);
+                }
             }
         };
 
@@ -64,6 +69,8 @@ void CCrashReporterDialog::LoadTranslatableResources()
         setTextFromDlg(IDC_STATIC3);
         setTextFromDlg(IDC_STATIC4);
         setTextFromDlg(IDOK);
+
+        ::DestroyWindow(hDlg);
     }
 }
 