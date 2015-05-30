26ea109802b50f91b45589736d7200f2632b553c
Underground78 <underground78@users.sourceforge.net>
2015-05-30 22:01:51 +0200

    Fix: Give "Display Current Time" its ID back to avoid messing the keys.
    Refs #5416.

--- src/mpc-hc/resource.h
+++ src/mpc-hc/resource.h
@@ -685,8 +685,8 @@
 #define ID_VIEW_TEARING_TEST            32769
 #define ID_FILE_OPENDISC                32774
 #define ID_SHADERS                      32775
-#define ID_VIEW_OSD_DISPLAY_TIME        32777
-#define ID_VIEW_OSD_SHOW_FILENAME       32778
+#define ID_VIEW_OSD_SHOW_FILENAME       32777
+#define ID_VIEW_OSD_DISPLAY_TIME        32778
 #define ID_D3DFULLSCREEN_TOGGLE         32779
 #define ID_GOTO_PREV_SUB                32780
 #define ID_GOTO_NEXT_SUB                32781
