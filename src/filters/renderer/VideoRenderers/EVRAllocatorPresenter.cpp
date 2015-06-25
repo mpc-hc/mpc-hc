mpc-hc/mpc-hc

commit 58abe6ec8b5fffe8196f0fe5b73aab28bea3c834

EVR-CP/Sync: Properly handle SetMediaType calls and release appropriate resources.

src/filters/renderer/VideoRenderers/EVRAllocatorPresenter.cpp
@@ -784,12 +784,27 @@ HRESULT CEVRAllocatorPresenter::CreateOptimalOutputType(IMFMediaType* pMixerProp
 
 HRESULT CEVRAllocatorPresenter::SetMediaType(IMFMediaType* pType)
 {
-    HRESULT hr;
+    HRESULT hr = S_OK;
     AM_MEDIA_TYPE* pAMMedia = nullptr;
     CString strTemp, strTemp1;
 
     CHECK_HR(CheckShutdown());
-    CheckPointer(pType, E_POINTER);
+
+    if (!pType) {
+        // Release
+        RemoveAllSamples();
+        DeleteSurfaces();
+        CAutoLock lock(&m_MediaTypeLock);
+        m_pMediaType = nullptr;
+        return hr;
+    }
+
+    DWORD dwFlags = 0;
+    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags)) {
+        // Nothing to do
+        return hr;
+    }
+
     CHECK_HR(pType->GetRepresentation(FORMAT_VideoInfo2, (void**)&pAMMedia));
 
     hr = InitializeDevice(pType);

src/filters/renderer/VideoRenderers/SyncRenderer.cpp
@@ -3022,12 +3022,27 @@ HRESULT CSyncAP::CreateOptimalOutputType(IMFMediaType* pMixerProposedType, IMFMe
 
 HRESULT CSyncAP::SetMediaType(IMFMediaType* pType)
 {
-    HRESULT hr;
+    HRESULT hr = S_OK;
     AM_MEDIA_TYPE* pAMMedia = nullptr;
     CString strTemp;
 
     CHECK_HR(CheckShutdown());
-    CheckPointer(pType, E_POINTER);
+
+    if (pType == nullptr) {
+        // Release
+        RemoveAllSamples();
+        DeleteSurfaces();
+        CAutoLock lock(this);
+        m_pMediaType = nullptr;
+        return hr;
+    }
+
+    DWORD dwFlags = 0;
+    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags)) {
+        // Nothing to do
+        return hr;
+    }
+
     CHECK_HR(pType->GetRepresentation(FORMAT_VideoInfo2, (void**)&pAMMedia));
 
     hr = InitializeDevice(pAMMedia);

commit cac691864e9b3c7ceba86b282d8280f92ca78229

Fix a typo introduced in 58abe6ec8b5fffe8196f0fe5b73aab28bea3c834 which result in not working playback in certain cases.

src/filters/renderer/VideoRenderers/EVRAllocatorPresenter.cpp
@@ -799,7 +799,7 @@ HRESULT CEVRAllocatorPresenter::SetMediaType(IMFMediaType* pType)
     }
 
     DWORD dwFlags = 0;
-    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags)) {
+    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags) == S_OK) {
         // Nothing to do
         return hr;
     }

src/filters/renderer/VideoRenderers/SyncRenderer.cpp
@@ -3038,7 +3038,7 @@ HRESULT CSyncAP::SetMediaType(IMFMediaType* pType)
     }
 
     DWORD dwFlags = 0;
-    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags)) {
+    if (m_pMediaType && m_pMediaType->IsEqual(pType, &dwFlags) == S_OK) {
         // Nothing to do
         return hr;
     }