mpc-hc/mpc-hc

commit 35d040bd2a22e136d6d000086f8458a3219914dd

AboutDlg.cpp: add VS2013 Update 5 version number.

src/mpc-hc/AboutDlg.cpp
@@ -95,6 +95,8 @@ BOOL CAboutDlg::OnInitDialog()
 #endif
 #elif defined(_MSC_VER)
 #if (_MSC_VER == 1800)              // 2013
+#if (_MSC_FULL_VER == 180040629)
+    m_MPCCompiler = _T("MSVC 2013 Update 5");
 #if (_MSC_FULL_VER == 180031101)
     m_MPCCompiler = _T("MSVC 2013 Update 4");
 #elif (_MSC_FULL_VER == 180030723)

commit 2ed911efe1a433bdeab5d8d2e109948884df1522

Fix stupid typo introduced in 35d040b.

src/mpc-hc/AboutDlg.cpp
@@ -97,7 +97,7 @@ BOOL CAboutDlg::OnInitDialog()
 #if (_MSC_VER == 1800)              // 2013
 #if (_MSC_FULL_VER == 180040629)
     m_MPCCompiler = _T("MSVC 2013 Update 5");
-#if (_MSC_FULL_VER == 180031101)
+#elif (_MSC_FULL_VER == 180031101)
     m_MPCCompiler = _T("MSVC 2013 Update 4");
 #elif (_MSC_FULL_VER == 180030723)
     m_MPCCompiler = _T("MSVC 2013 Update 3");