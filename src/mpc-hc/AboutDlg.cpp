103f70ab52a105cf7b8b46e85968a8c97eb10285
XhmikosR <xhmikosr@gmail.com>
2015-11-30 23:55:47 +0200

    AboutDlg.cpp: Add MSVC 2015 Update 1 version number.

--- src/mpc-hc/AboutDlg.cpp
+++ src/mpc-hc/AboutDlg.cpp
@@ -95,7 +95,9 @@ BOOL CAboutDlg::OnInitDialog()
 #endif
 #elif defined(_MSC_VER)
 #if (_MSC_VER == 1900)                // 2015
-#if (_MSC_FULL_VER == 190023026)
+#if (_MSC_FULL_VER == 190023506)
+    m_MPCCompiler = _T("MSVC 2015 Update 1");
+#else
     m_MPCCompiler = _T("MSVC 2015");
 #endif
 #elif (_MSC_VER == 1800)              // 2013
