mpc-hc/mpc-hc

commit 90d51fd54ea7131340dcefdc0224d6687c17f56f

Do not use singleton for SubtitlesProviders class.

src/mpc-hc/PPageSubMisc.cpp
@@ -20,6 +20,7 @@
 
 #include "stdafx.h"
 #include "mplayerc.h"
+#include "MainFrm.h"
 #include "AuthDlg.h"
 #include "PPageSubMisc.h"
 #include "SubtitlesProviders.h"
@@ -30,6 +31,8 @@ IMPLEMENT_DYNAMIC(CPPageSubMisc, CPPageBase)
 
 CPPageSubMisc::CPPageSubMisc()
     : CPPageBase(CPPageSubMisc::IDD, CPPageSubMisc::IDD)
+      // Do not check dynamic_cast, because if it fails we cannot recover from the error anyway.
+    , m_pSubtitlesProviders(AfxGetMainFrame()->m_pSubtitlesProviders.get())
     , m_fPreferDefaultForcedSubtitles(TRUE)
     , m_fPrioritizeExternalSubtitles(TRUE)
     , m_fDisableInternalSubtitles(FALSE)
@@ -40,8 +43,8 @@ CPPageSubMisc::CPPageSubMisc()
     , m_strSubtitlesProviders()
     , m_strSubtitlesLanguageOrder()
     , m_strAutoloadPaths()
-    , m_subtitlesProviders(SubtitlesProviders::Instance())
 {
+    ASSERT(m_pSubtitlesProviders);
 }
 
 CPPageSubMisc::~CPPageSubMisc()
@@ -88,7 +91,7 @@ BOOL CPPageSubMisc::OnInitDialog()
                             | LVS_EX_DOUBLEBUFFER | LVS_EX_FULLROWSELECT
                             | LVS_EX_CHECKBOXES | LVS_EX_LABELTIP);
 
-    m_list.SetImageList(&m_subtitlesProviders.GetImageList(), LVSIL_SMALL);
+    m_list.SetImageList(&m_pSubtitlesProviders->GetImageList(), LVSIL_SMALL);
 
     CArray<int> columnWidth;
     if (columnWidth.GetCount() != COL_TOTAL_COLUMNS) {
@@ -107,7 +110,7 @@ BOOL CPPageSubMisc::OnInitDialog()
     m_list.DeleteAllItems();
 
     int i = 0;
-    for (const auto& iter : m_subtitlesProviders.Providers()) {
+    for (const auto& iter : m_pSubtitlesProviders->Providers()) {
         int iItem = m_list.InsertItem(i++, CString(iter->Name().c_str()), iter->GetIconIndex());
         m_list.SetItemText(iItem, COL_USERNAME, UTF8To16(iter->UserName().c_str()));
         m_list.SetItemText(iItem, COL_LANGUAGES, ResStr(IDS_SUBPP_DLG_FETCHING_LANGUAGES));
@@ -120,7 +123,7 @@ BOOL CPPageSubMisc::OnInitDialog()
     m_list.UpdateWindow();
 
     m_threadFetchSupportedLanguages = std::thread([this]() {
-        for (const auto& iter : m_subtitlesProviders.Providers()) {
+        for (const auto& iter : m_pSubtitlesProviders->Providers()) {
             iter->Languages();
         }
         PostMessage(WM_SUPPORTED_LANGUAGES_READY); // Notify the window that languages have been fetched
@@ -161,7 +164,7 @@ BOOL CPPageSubMisc::OnApply()
         provider->Enabled(SPF_SEARCH, m_list.GetCheck(i));
     }
 
-    s.strSubtitlesProviders = m_subtitlesProviders.WriteSettings().c_str();
+    s.strSubtitlesProviders = m_pSubtitlesProviders->WriteSettings().c_str();
 
     return __super::OnApply();
 }
@@ -179,7 +182,7 @@ END_MESSAGE_MAP()
 void CPPageSubMisc::OnSupportedLanguagesReady()
 {
     int i = 0;
-    for (const auto& iter : m_subtitlesProviders.Providers()) {
+    for (const auto& iter : m_pSubtitlesProviders->Providers()) {
         CString languages(SubtitlesProvidersUtils::JoinContainer(iter->Languages(), ",").c_str());
         m_list.SetItemText(i++, COL_LANGUAGES, languages.IsEmpty() ? ResStr(IDS_SUBPP_DLG_LANGUAGES_ERROR) : languages);
     }
@@ -247,12 +250,12 @@ void CPPageSubMisc::OnRightClick(NMHDR* pNMHDR, LRESULT* pResult)
                 SetModified();
                 break;
             case MOVE_UP:
-                m_subtitlesProviders.MoveUp(lpnmlv->iItem);
+                m_pSubtitlesProviders->MoveUp(lpnmlv->iItem);
                 ListView_SortItemsEx(m_list.GetSafeHwnd(), SortCompare, m_list.GetSafeHwnd());
                 SetModified();
                 break;
             case MOVE_DOWN:
-                m_subtitlesProviders.MoveDown(lpnmlv->iItem);
+                m_pSubtitlesProviders->MoveDown(lpnmlv->iItem);
                 ListView_SortItemsEx(m_list.GetSafeHwnd(), SortCompare, m_list.GetSafeHwnd());
                 SetModified();
                 break;

src/mpc-hc/MainFrm.cpp
@@ -929,7 +929,7 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
         m_pGraphThread->SetMainFrame(this);
     }
 
-    m_pSubtitlesProviders = &SubtitlesProviders::Instance();
+    m_pSubtitlesProviders = std::make_unique<SubtitlesProviders>(this);
     m_wndSubtitlesDownloadDialog.Create(m_wndSubtitlesDownloadDialog.IDD, this);
     m_wndSubtitlesUploadDialog.Create(m_wndSubtitlesUploadDialog.IDD, this);
 

src/mpc-hc/MainFrm.h
@@ -483,7 +483,7 @@ protected:
     friend class CPPageSubMisc;
 
     friend class SubtitlesProviders;
-    SubtitlesProviders* m_pSubtitlesProviders;
+    std::unique_ptr<SubtitlesProviders> m_pSubtitlesProviders;
     friend struct SubtitlesInfo;
     friend class SubtitlesTask;
     friend class SubtitlesThread;

src/mpc-hc/PPageSubMisc.h
@@ -38,7 +38,7 @@ private:
         COL_TOTAL_COLUMNS
     };
 
-    SubtitlesProviders& m_subtitlesProviders;
+    SubtitlesProviders* m_pSubtitlesProviders;
 
     std::thread m_threadFetchSupportedLanguages;
 

src/mpc-hc/SubtitlesProviders.cpp
@@ -510,8 +510,8 @@ bool SubtitlesProvider::CheckInternetConnection()
 ** SubtitlesProviders
 ******************************************************************************/
 
-SubtitlesProviders::SubtitlesProviders()
-    : m_MainFrame(*(CMainFrame*)(AfxGetMyApp()->GetMainWnd()))
+SubtitlesProviders::SubtitlesProviders(CMainFrame* pMainFrame)
+    : m_pMainFrame(pMainFrame)
 {
     m_himl.Create(16, 16, ILC_COLOR32, 0, 0);
     RegisterProviders();
@@ -532,19 +532,19 @@ BOOL SubtitlesProviders::CheckInternetConnection()
 void SubtitlesProviders::Search(bool bAutoDownload)
 {
     Abort(SubtitlesThreadType(STT_SEARCH | STT_DOWNLOAD));
-    m_MainFrame.m_wndSubtitlesDownloadDialog.DoClear();
+    m_pMainFrame->m_wndSubtitlesDownloadDialog.DoClear();
 
     if (CheckInternetConnection()) {
-        InsertTask(DEBUG_NEW SubtitlesTask(&m_MainFrame, bAutoDownload, LanguagesISO6391()));
+        InsertTask(DEBUG_NEW SubtitlesTask(m_pMainFrame, bAutoDownload, LanguagesISO6391()));
     } else if (bAutoDownload == FALSE) {
-        m_MainFrame.m_wndSubtitlesDownloadDialog.DoFailed();
+        m_pMainFrame->m_wndSubtitlesDownloadDialog.DoFailed();
     }
 }
 
 void SubtitlesProviders::Download(SubtitlesInfo& pSubtitlesInfo, bool bActivate)
 {
     if (CheckInternetConnection()) {
-        InsertTask(DEBUG_NEW SubtitlesTask(&m_MainFrame, pSubtitlesInfo, bActivate));
+        InsertTask(DEBUG_NEW SubtitlesTask(m_pMainFrame, pSubtitlesInfo, bActivate));
     }
 }
 
@@ -554,15 +554,15 @@ void SubtitlesProviders::Upload(bool bShowConfirm)
         // We get all the information we need within the main thread, to delay closing the file
         // until we have everything we need.
         SubtitlesInfo pSubtitlesInfo;
-        m_MainFrame.SendMessage(WM_GETSUBTITLES, 0, (LPARAM)&pSubtitlesInfo);
+        m_pMainFrame->SendMessage(WM_GETSUBTITLES, 0, (LPARAM)&pSubtitlesInfo);
         //pSubtitlesInfo.GetCurrentSubtitles();
 
         if (!pSubtitlesInfo.fileContents.empty()) {
             CString msg;
             msg.Format(IDS_SUBUL_DLG_CONFIRM, UTF8To16(pSubtitlesInfo.fileName.c_str()));
             if (!bShowConfirm
-                    || IDYES == MessageBox(m_MainFrame.m_wndSubtitlesUploadDialog, msg, ResStr(IDS_SUBUL_DLG_TITLE), MB_YESNO)) {
-                InsertTask(DEBUG_NEW SubtitlesTask(&m_MainFrame, pSubtitlesInfo));
+                    || IDYES == MessageBox(m_pMainFrame->m_wndSubtitlesUploadDialog, msg, ResStr(IDS_SUBUL_DLG_TITLE), MB_YESNO)) {
+                InsertTask(DEBUG_NEW SubtitlesTask(m_pMainFrame, pSubtitlesInfo));
             }
         }
     }

src/mpc-hc/SubtitlesProviders.h
@@ -380,17 +380,11 @@ protected:
 
 class SubtitlesProviders final
 {
-    SubtitlesProviders();
-    ~SubtitlesProviders();
 public:
+    explicit SubtitlesProviders(CMainFrame* pMainFrame);
+    ~SubtitlesProviders();
     SubtitlesProviders(SubtitlesProviders const&) = delete;
     SubtitlesProviders& operator=(SubtitlesProviders const&) = delete;
-
-    // Instantiated on first use and guaranteed to be destroyed.
-    static SubtitlesProviders& Instance() {
-        static SubtitlesProviders that;
-        return that;
-    }
 
 private:
     void RegisterProviders();
@@ -439,7 +433,7 @@ public:
     CImageList& GetImageList() { return m_himl; }
 
 private:
-    CMainFrame& m_MainFrame;
+    CMainFrame* m_pMainFrame;
 
     std::vector<std::shared_ptr<SubtitlesProvider>> m_pProviders;
 

commit 95f697345b26d13e1309b8ff7fae9aabf1a1c882

Fix crash when trying to open formats options page as administrator.
Fixes regression introduced in 90d51fd54ea7131340dcefdc0224d6687c17f56f

Fixes #5853 and DrDump issue #212927

src/mpc-hc/PPageSubMisc.cpp
@@ -31,8 +31,7 @@ IMPLEMENT_DYNAMIC(CPPageSubMisc, CPPageBase)
 
 CPPageSubMisc::CPPageSubMisc()
     : CPPageBase(CPPageSubMisc::IDD, CPPageSubMisc::IDD)
-      // Do not check dynamic_cast, because if it fails we cannot recover from the error anyway.
-    , m_pSubtitlesProviders(AfxGetMainFrame()->m_pSubtitlesProviders.get())
+    , m_pSubtitlesProviders(nullptr)
     , m_fPreferDefaultForcedSubtitles(TRUE)
     , m_fPrioritizeExternalSubtitles(TRUE)
     , m_fDisableInternalSubtitles(FALSE)
@@ -44,7 +43,6 @@ CPPageSubMisc::CPPageSubMisc()
     , m_strSubtitlesLanguageOrder()
     , m_strAutoloadPaths()
 {
-    ASSERT(m_pSubtitlesProviders);
 }
 
 CPPageSubMisc::~CPPageSubMisc()
@@ -90,6 +88,12 @@ BOOL CPPageSubMisc::OnInitDialog()
     m_list.SetExtendedStyle(m_list.GetExtendedStyle()
                             | LVS_EX_DOUBLEBUFFER | LVS_EX_FULLROWSELECT
                             | LVS_EX_CHECKBOXES | LVS_EX_LABELTIP);
+
+    // Do not check dynamic_cast, because if it fails we cannot recover from the error anyway.
+    const CMainFrame* pMainFrame = AfxGetMainFrame();
+    ASSERT(pMainFrame);
+    m_pSubtitlesProviders = pMainFrame->m_pSubtitlesProviders.get();
+    ASSERT(m_pSubtitlesProviders);
 
     m_list.SetImageList(&m_pSubtitlesProviders->GetImageList(), LVSIL_SMALL);
 