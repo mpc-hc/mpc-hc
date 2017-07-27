57e7ee54ffeb60ee0699401be4b56b5e2a7b3958
clsid2 <clsid2@users.sourceforge.net>
2017-07-27 17:47:51 +0200

    Improved subtitle selection when no LCID value is available.
    Fixes ticket #4112
    
    Signed-off-by: clsid2 <clsid2@users.sourceforge.net>

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -11724,7 +11724,8 @@ int CMainFrame::SetupSubtitleStreams()
                             auto findCode = [](const CString & name, const CString & code) {
                                 int nPos = code.IsEmpty() ? -1 : name.Find(code);
                                 return ((nPos == 0 && name.GetLength() == code.GetLength())
-                                        || (nPos > 0 && (name[nPos - 1] == _T('[') || name[nPos - 1] == _T('\t'))));
+                                        || (nPos > 0 && (name[nPos - 1] == _T('[') || name[nPos - 1] == _T('\t'))))
+                                       || (nPos > 0 && name[nPos - 1] == _T('.') && (name.GetLength() >= (nPos + code.GetLength() + 1)) && name[nPos + code.GetLength()] == _T('.'));
                             };
                             // match anything that starts with the language name or that seems to use a code that matches
                             if (name.Find(l.name) != 0 && !findCode(name, l.name) && !findCode(name, l.iso6392) && !findCode(name, l.iso6391)) {
