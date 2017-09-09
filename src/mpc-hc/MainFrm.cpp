2bf8c62786615e8a7bfba6c4c039794a57f4a233
phineas12 <31747739+phineas12@users.noreply.github.com>
2017-09-09 16:23:48 -0400

    Screenshot without extension overwrites bugfix
    https://trac.mpc-hc.org/ticket/5877#comment:5

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -4909,7 +4909,7 @@ void CMainFrame::OnFileSaveImage()
     }
     psrc.Combine(s.strSnapshotPath, MakeSnapshotFileName(prefix));
 
-    CSaveImageDialog fd(s.nJpegQuality, nullptr, (LPCTSTR)psrc,
+    CSaveImageDialog fd(s.nJpegQuality, _T("jpg"), (LPCTSTR)psrc,
                         _T("BMP - Windows Bitmap (*.bmp)|*.bmp|JPG - JPEG Image (*.jpg)|*.jpg|PNG - Portable Network Graphics (*.png)|*.png||"), GetModalParent());
 
     if (s.strSnapshotExt == _T(".bmp")) {
